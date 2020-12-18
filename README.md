112a6ea2092bc3569592f77a8ea9c6dde66f32ea
lzh <zhihao.lao@vistel.cn>
2020-12-18 10:24:24 +0800

    initialize

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # myopia-pac-backend
 
-近视防控项目后端：myopia-pac-backend
+近视防控项目后端：myopia prevention and control
---
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/App.java
@@ -0,0 +1,8 @@
+package com.wupol.myopia.business.management;
+
+/**
+ * @Author HaoHao
+ * @Date 2020/12/17
+ **/
+public class App {
+}
