47e3c27f1fc51f1f6cee914fe869dedbad6a4fe6
Simple4H <crescentcxm@gmail.com>
2020-12-31 14:52:30 +0800

    update

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/constant/Const.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/constant/Const.java
@@ -9,9 +9,10 @@ public class Const {
 
     public static final Integer STATUS_IS_DELETED = 2;
 
-    public static final Integer CREATE_USER_ID = 1;
+    public static final String GENERATE_ORG_SN = "org:generate.org.sn:integer";
+    public static final String LOCK_ORG_REDIS = "org:lock.org:string";
 
-    public static final Integer STAFF_USER_ID = 1;
+    public static final String GENERATE_SCHOOL_SN = "org:generate.school.sn:integer";
 
-    public static final String STAFF_ID_CARD = "aaaaaa";
+    public static final String GENERATE_HOSPITAL_SN = "org:generate.hospital.sn:integer";
 }
--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/service/ScreeningOrganizationService.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/service/ScreeningOrganizationService.java
@@ -1,6 +1,7 @@
 package com.wupol.myopia.business.management.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.wupol.myopia.base.cache.RedisUtil;
 import com.wupol.myopia.base.exception.BusinessException;
 import com.wupol.myopia.base.service.BaseService;
 import com.wupol.myopia.business.management.constant.Const;
@@ -9,17 +10,22 @@ import com.wupol.myopia.business.management.domain.mapper.ScreeningOrganizationM
 import com.wupol.myopia.business.management.domain.model.ScreeningOrganization;
 import com.wupol.myopia.business.management.domain.query.PageRequest;
 import com.wupol.myopia.business.management.domain.query.ScreeningOrganizationQuery;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author HaoHao
  * @Date 2020-12-22
  */
 @Service
+@Log4j2
 public class ScreeningOrganizationService extends BaseService<ScreeningOrganizationMapper, ScreeningOrganization> {
 
     @Resource
@@ -28,6 +34,12 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
     @Resource
     private ScreeningOrganizationMapper screeningOrganizationMapper;
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    @Resource
+    private RedissonClient redissonClient;
+
     /**
      * 保存筛查机构
      *
@@ -35,12 +47,32 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
      * @return Integer 插入个数
      */
     @Transactional(rollbackFor = Exception.class)
-    public synchronized Integer saveScreeningOrganization(ScreeningOrganization screeningOrganization) {
-        if (null == screeningOrganization.getTownCode()) {
+    public Integer saveScreeningOrganization(ScreeningOrganization screeningOrganization) {
+
+        Integer townCode = screeningOrganization.getTownCode();
+        Integer result = null;
+
+        if (null == townCode) {
             throw new BusinessException("数据异常");
         }
-        screeningOrganization.setOrgNo(generateOrgNo(screeningOrganization.getTownCode()));
-        return baseMapper.insert(screeningOrganization);
+        String lockKey = Const.LOCK_ORG_REDIS + townCode;
+        RLock rLock = redissonClient.getLock(lockKey);
+        try {
+            boolean tryLock = rLock.tryLock(4, TimeUnit.SECONDS);
+            if (tryLock) {
+                log.info("用户:{}获取锁成功,code:{}", screeningOrganization.getCreateUserId(), townCode);
+                screeningOrganization.setOrgNo(generateOrgNoByRedis(townCode));
+                result = baseMapper.insert(screeningOrganization);
+            }
+        } catch (InterruptedException e) {
+            log.info("用户id:{}获取锁异常", screeningOrganization.getCreateUserId());
+            throw new BusinessException("业务异常");
+        } finally {
+            if (rLock.isLocked()) {
+                rLock.unlock();
+            }
+        }
+        return result;
     }
 
     /**
@@ -96,11 +128,45 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
         return baseMapper.updateById(org);
     }
 
+    /**
+     * 生成编号
+     *
+     * @param code 镇代码
+     * @return 代码
+     */
     private String generateOrgNo(Integer code) {
         ScreeningOrganization org = screeningOrganizationMapper.getLastOrgByNo(code);
         if (null == org) {
             return StringUtils.join(code, "201");
         }
         return String.valueOf(Long.parseLong(org.getOrgNo()) + 1);
+    }
+
+    /**
+     * 生成编号通过Redis
+     *
+     * @param code 镇代码
+     * @return 编号
+     */
+    private String generateOrgNoByRedis(Integer code) {
+        // 查询redis是否存在
+        String key = Const.GENERATE_ORG_SN + code;
+        Integer queueCount = (Integer) redisUtil.get(key);
+        if (null == queueCount) {
+            ScreeningOrganization org = screeningOrganizationMapper.getLastOrgByNo(code);
+            // 数据库不存在
+            if (null == org) {
+                // 数据库不存在，初始化
+                redisUtil.set(key, 201);
+                return StringUtils.join(code, "201");
+            }
+            // 获取当前数据库中最新的编号并且加一
+            String resultCode = String.valueOf(Long.parseLong(org.getOrgNo()) + 1);
+            // 获取后三位,并缓存到redis中
+            redisUtil.set(key, Integer.valueOf(StringUtils.right(resultCode, 3)));
+            return resultCode;
+        }
+        // 自增一,并且返回
+        return StringUtils.join(code, redisUtil.incr(key, 1));
     }
 }
