2a73477de293488c1e5624a355a12455911d7132
Simple4H <crescentcxm@gmail.com>
2021-01-22 11:13:07 +0800

    update

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/constant/CommonConst.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/constant/CommonConst.java
@@ -5,40 +5,40 @@ package com.wupol.myopia.business.management.constant;
  *
  * @author Simple4H
  */
-public class CommonConst {
+public interface CommonConst {
 
     /**
      * 状态-删除
      */
-    public static final Integer STATUS_IS_DELETED = 2;
+    Integer STATUS_IS_DELETED = 2;
 
     /**
      * 状态-启用
      */
-    public static final Integer STATUS_NOT_DELETED = 0;
+    Integer STATUS_NOT_DELETED = 0;
 
     /**
      * 筛查次数
      */
-    public static final Integer SCREENING_TIME = 0;
+    Integer SCREENING_TIME = 0;
 
     /**
      * 通知中心 已读状态
      */
-    public static final Integer READ_NOTICE = 1;
+    Integer READ_NOTICE = 1;
 
     /**
      * 通知中心 删除状态
      */
-    public static final Integer DELETED_NOTICE = 2;
+    Integer DELETED_NOTICE = 2;
 
     /**
      * 系统中心 类型 档案卡模版
      */
-    public static final Integer STUDENT_ARCHIVES = 1;
+    Integer STUDENT_ARCHIVES = 1;
 
     /**
      * 系统中心 类型 筛查报告模版
      */
-    public static final Integer SCREENING_REPORT = 2;
+    Integer SCREENING_REPORT = 2;
 }
