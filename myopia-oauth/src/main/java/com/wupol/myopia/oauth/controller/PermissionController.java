09bba7a0529d08477c5cbb97f21ff5394a775fc3
lzh <zhihao.lao@vistel.cn>
2021-01-30 10:45:40 +0800

    [update] role & user & permission

--- myopia-oauth/src/main/java/com/wupol/myopia/oauth/controller/PermissionController.java
+++ myopia-oauth/src/main/java/com/wupol/myopia/oauth/controller/PermissionController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @Author HaoHao
@@ -24,21 +26,29 @@ public class PermissionController {
     /**
      * 获取全量权限资源
      *
-     * @return java.lang.Object
+     * @return java.util.List<com.wupol.myopia.oauth.domain.model.Permission>
      **/
     @GetMapping("/list")
-    public Object getPermissionList() {
+    public List<Permission> getPermissionList() {
         return permissionService.getAllPermissionTree();
     }
 
+    /**
+     * 新增
+     *
+     * @param param 权限资源数据
+     * @return com.wupol.myopia.oauth.domain.model.Permission
+     **/
     @PostMapping()
-    public Permission addPermission(@RequestBody @Valid Permission param) {
+    public Permission addPermission(@RequestBody @Valid Permission param) throws IOException {
+        permissionService.validateParam(param);
         permissionService.save(param);
         return param;
     }
 
     @PutMapping()
-    public Permission modifyPermission(@RequestBody @Valid Permission param) {
+    public Permission modifyPermission(@RequestBody @Valid Permission param) throws IOException {
+        permissionService.validateParam(param);
         permissionService.updateById(param);
         return param;
     }
