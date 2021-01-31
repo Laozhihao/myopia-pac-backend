ebf4bcbca3ad2cb6cd8632b4cbeae185a1476ca6
deepx <wtful00kin@fuck.off>
2021-02-01 00:15:28 +0800

    stat model update

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/dto/stat/ScreenCompareData.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/dto/stat/ScreenCompareData.java
@@ -1,10 +1,15 @@
 package com.wupol.myopia.business.management.domain.dto.stat;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ScreenCompareData {
     /** 筛查学生数 */
     private Integer screeningNum;
 
-    /** 实际筛查数量 */
+    /** 实际筛查学生数 */
     private Integer ActualScreeningNum;
 
     /** 左眼平均视力 */
@@ -28,18 +33,48 @@ public class ScreenCompareData {
     /** 近视率 */
     private Float myopiaRate;
 
-    /** 重点实例对象数量 */
+    /** 重点视力对象数量 */
     private Integer focusTargetsNum;
 
-    private Integer warningLevelZeroNum;
-    private Integer warningLevelOneNum;
-    private Integer warningLevelTwoNum;
-    private Integer warningLevelThreeNum;
+    /** 0级预警率 */
+    private Float warningLevelZeroRate;
 
-    private Integer recommendNum;
-    private Integer visionDoneNum;
+    /** 1级预警率 */
+    private Float warningLevelOneRate;
+
+    /** 2级预警率 */
+    private Float warningLevelTwoRate;
+
+    /** 3级预警率 */
+    private Float warningLevelThreeRate;
+
+    /** 建议就诊数 */
+    private Integer recommendVisitNum;
+
+    /** 视力筛查完成率 */
+    private Float screeningFinishedRate;
+
+    /** 复测人数 */
     private Integer rescreenNum;
+
+    /** 戴镜复测人数 */
     private Integer wearingGlassesRescreenNum;
 
-    private Float wearingGlassesRescreenRate;
+    /** 戴镜复测指标数 */
+    private Integer wearingGlassesRescreenIndexNum;
+
+    /** 非戴镜复测人数 */
+    private Integer withoutGlassesRescreenNum;
+
+    /** 非戴镜复测指标数 */
+    private Integer withoutGlassesRescreenIndexNum;
+
+    /** 复测项次 */
+    private Integer rescreenItemNum;
+
+    /** 错误项次数 */
+    private Integer incorrectItemNum;
+
+    /** 错误率/发生率 */
+    private Float incorrectRate;
 }
--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/dto/stat/WarningInfo.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/dto/stat/WarningInfo.java
@@ -13,57 +13,37 @@ import lombok.experimental.Accessors;
 @Builder
 @Accessors(chain = true)
 public class WarningInfo {
-    /**
-     * 当前统计时间
-     */
+    /** 当前统计时间 */
     private Long statTime;
 
-    /**
-     * 总重点视力对象数
-     */
+    /** 总重点视力对象数 */
     private Integer focusTargetsNum;
 
-    /**
-     *  总视力对象数在总学生数中的占比
-     */
+    /** 总视力对象数在总学生数中的占比 */
     private Float focusTargetsPercentage;
 
-    /**
-     * 去年今日的时间
-     */
+    /** 去年今日的时间 */
     private Long lastStatTime;
 
-    /**
-     * 去年今日的总重点视力对象数
-     */
+    /** 去年今日的总重点视力对象数 */
     private Integer lastFocusTargetsNum;
 
-    /**
-     *  去年今日的总视力对象数在总学生数中的占比
-     */
+    /** 去年今日的总视力对象数在总学生数中的占比 */
     private Float lastFocusTargetsPercentage;
 
-    /**
-     * 分级预警信息
-     */
+    /** 分级预警信息 */
     private List<WarningLevelInfo> warningLevelInfoList;
 
     @Data
     @Accessors(chain = true)
     public static class WarningLevelInfo {
-        /**
-         * 预警级别
-         */
+        /** 预警级别 */
         private Integer warningLevel;
 
-        /**
-         * 人数
-         */
+        /** 人数 */
         private Integer num;
 
-        /**
-         * 人数比例
-         */
+        /** 人数比例 */
         private Float percentage;
 
         public WarningLevelInfo(Integer warningLevel, Integer num, Float percentage) {
