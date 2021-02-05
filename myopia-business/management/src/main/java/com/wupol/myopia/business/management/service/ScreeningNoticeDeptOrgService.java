885fb4baec098483d152a28accb8b6ed8bad04a9
huangpm <huangpm@wupol.com>
2021-02-05 15:14:04 +0800

    feat(screeningManage): 修复发布筛查任务通知时，筛查机构ID设置错误

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/service/ScreeningNoticeDeptOrgService.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/service/ScreeningNoticeDeptOrgService.java
@@ -5,24 +5,24 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wupol.myopia.base.domain.CurrentUser;
 import com.wupol.myopia.base.exception.BusinessException;
+import com.wupol.myopia.base.service.BaseService;
 import com.wupol.myopia.business.management.constant.CommonConst;
-import com.wupol.myopia.business.management.domain.dto.UserDTO;
+import com.wupol.myopia.business.management.domain.mapper.ScreeningNoticeDeptOrgMapper;
 import com.wupol.myopia.business.management.domain.model.District;
 import com.wupol.myopia.business.management.domain.model.GovDept;
 import com.wupol.myopia.business.management.domain.model.ScreeningNotice;
 import com.wupol.myopia.business.management.domain.model.ScreeningNoticeDeptOrg;
-import com.wupol.myopia.business.management.domain.mapper.ScreeningNoticeDeptOrgMapper;
-import com.wupol.myopia.base.service.BaseService;
 import com.wupol.myopia.business.management.domain.query.PageRequest;
 import com.wupol.myopia.business.management.domain.query.ScreeningNoticeQuery;
-import com.wupol.myopia.business.management.domain.query.UserDTOQuery;
 import com.wupol.myopia.business.management.domain.vo.ScreeningNoticeVo;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -119,10 +119,11 @@ public class ScreeningNoticeDeptOrgService extends BaseService<ScreeningNoticeDe
      *
      * @param noticeId
      * @param acceptOrgId
+     * @param genTaskOrPlanId 生成的任务或计划Id
      * @param user
      */
-    public void statusReadAndCreate(Integer noticeId, Integer acceptOrgId, CurrentUser user) {
-        //1. 更新状态
+    public void statusReadAndCreate(Integer noticeId, Integer acceptOrgId, Integer genTaskOrPlanId, CurrentUser user) {
+        //1. 更新状态与任务/计划ID
         baseMapper.updateStatusByNoticeIdAndAcceptOrgId(noticeId, acceptOrgId, user.getId(), CommonConst.STATUS_NOTICE_CREATED);
     }
 }
