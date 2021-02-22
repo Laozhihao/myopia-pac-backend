e501661ab7b787a1c26384c1bf34cd0eea285255
jacob <maxxma@sina.cn>
2021-02-22 10:02:14 +0800

    feat($statistic_conclusion): 增加统计结论相关实体

---
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/mapper/StatConclusionMapper.java
@@ -0,0 +1,14 @@
+package com.wupol.myopia.business.management.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wupol.myopia.business.management.domain.model.StatConclusion;
+
+/**
+ * 筛查数据结论Mapper接口
+ *
+ * @Author Jacob
+ * @Date 2021-02-22
+ */
+public interface StatConclusionMapper extends BaseMapper<StatConclusion> {
+
+}
--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/model/StatConclusion.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/model/StatConclusion.java
@@ -1,19 +1,29 @@
 package com.wupol.myopia.business.management.domain.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * 筛查数据结论
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("m_stat_conclusion")
 @Builder
-public class StatConclusion {
+public class StatConclusion implements Serializable {
     /** id */
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /** 源筛查数据id */
