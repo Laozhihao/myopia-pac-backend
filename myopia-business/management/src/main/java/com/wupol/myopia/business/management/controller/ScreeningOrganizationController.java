e6b543c1b305672fa9f7298e331bf452f1d93091
Simple4H <crescentcxm@gmail.com>
2021-02-24 14:38:59 +0800

    feat(management): 消息推送通知 bug fix

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/ScreeningOrganizationController.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/ScreeningOrganizationController.java
@@ -7,10 +7,12 @@ import com.wupol.myopia.base.handler.ResponseResultBody;
 import com.wupol.myopia.base.util.CurrentUserUtil;
 import com.wupol.myopia.business.management.domain.dto.ResetPasswordRequest;
 import com.wupol.myopia.business.management.domain.dto.StatusRequest;
+import com.wupol.myopia.business.management.domain.model.GovDept;
 import com.wupol.myopia.business.management.domain.model.ScreeningOrganization;
 import com.wupol.myopia.business.management.domain.query.PageRequest;
 import com.wupol.myopia.business.management.domain.query.ScreeningOrganizationQuery;
 import com.wupol.myopia.business.management.facade.ExcelFacade;
+import com.wupol.myopia.business.management.service.GovDeptService;
 import com.wupol.myopia.business.management.service.ScreeningOrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +34,9 @@ public class ScreeningOrganizationController {
     private ScreeningOrganizationService saveScreeningOrganization;
     @Autowired
     private ExcelFacade excelFacade;
+
+    @Autowired
+    private GovDeptService govDeptService;
 
 
     @PostMapping()
@@ -99,5 +104,14 @@ public class ScreeningOrganizationController {
     public Object getScreeningOrganizationListByGovDeptId(ScreeningOrganizationQuery query) {
 
         return saveScreeningOrganization.getScreeningOrganizationListByGovDeptId(query);
+    }
+
+    @GetMapping("/getDistrictId")
+    public GovDept getDistrictId() {
+        CurrentUser currentUser = CurrentUserUtil.getCurrentUser();
+        if (currentUser.isGovDeptUser()) {
+            return govDeptService.getById(currentUser.getOrgId());
+        }
+        return null;
     }
 }
