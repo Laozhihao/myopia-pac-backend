d9124656d08c371a440cd8d2ce8de400da924718
jacob <maxxma@sina.cn>
2021-02-25 00:02:37 +0800

    feat($SchoolMonitorStatistic): 学校视力监控统计

---
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/service/SchoolMonitorStatisticService.java
@@ -0,0 +1,15 @@
+package com.wupol.myopia.business.management.service;
+
+import com.wupol.myopia.base.service.BaseService;
+import com.wupol.myopia.business.management.domain.mapper.SchoolMonitorStatisticMapper;
+import com.wupol.myopia.business.management.domain.model.SchoolMonitorStatistic;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author HaoHao
+ * @Date 2021-02-24
+ */
+@Service
+public class SchoolMonitorStatisticService extends BaseService<SchoolMonitorStatisticMapper, SchoolMonitorStatistic> {
+
+}
