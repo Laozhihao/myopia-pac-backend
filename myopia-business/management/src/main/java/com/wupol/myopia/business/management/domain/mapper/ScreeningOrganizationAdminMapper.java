835c005108d1f25e0bd6738c165f8a7eab5090d7
Simple4H <crescentcxm@gmail.com>
2021-02-24 16:58:17 +0800

    feat(management): 通知-筛查机构

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/mapper/ScreeningOrganizationAdminMapper.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/mapper/ScreeningOrganizationAdminMapper.java
@@ -2,6 +2,9 @@ package com.wupol.myopia.business.management.domain.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wupol.myopia.business.management.domain.model.ScreeningOrganizationAdmin;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 筛查机构-管理员Mapper接口
@@ -9,4 +12,6 @@ import com.wupol.myopia.business.management.domain.model.ScreeningOrganizationAd
  * @author Simple4H
  */
 public interface ScreeningOrganizationAdminMapper extends BaseMapper<ScreeningOrganizationAdmin> {
+
+    List<ScreeningOrganizationAdmin> getByOrgIds(@Param("orgIds") List<Integer> orgIds);
 }
