f983523aa4957b01e6824476b1bf817dfb79e981
lzh <zhihao.lao@vistel.cn>
2021-02-26 13:25:35 +0800

    initialize parent module

---
+++ myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/domain/mapper/ParentStudentMapper.java
@@ -0,0 +1,14 @@
+package com.wupol.myopia.business.parent.domain.mapper;
+
+import com.wupol.myopia.business.parent.domain.model.ParentStudent;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 家长学生关系表Mapper接口
+ *
+ * @Author HaoHao
+ * @Date 2021-02-26
+ */
+public interface ParentStudentMapper extends BaseMapper<ParentStudent> {
+
+}
---
+++ myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/service/ParentService.java
@@ -0,0 +1,15 @@
+package com.wupol.myopia.business.parent.service;
+
+import com.wupol.myopia.business.parent.domain.model.Parent;
+import com.wupol.myopia.business.parent.domain.mapper.ParentMapper;
+import com.wupol.myopia.base.service.BaseService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author HaoHao
+ * @Date 2021-02-26
+ */
+@Service
+public class ParentService extends BaseService<ParentMapper, Parent> {
+
+}
