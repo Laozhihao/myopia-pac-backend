8470b131f83f703725435934d0868682c98f3a78
Simple4H <crescentcxm@gmail.com>
2021-03-04 19:06:42 +0800

    feat(parent): 添加注释

--- myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/service/ParentStudentService.java
+++ myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/service/ParentStudentService.java
@@ -620,26 +620,31 @@ public class ParentStudentService extends BaseService<ParentStudentMapper, Paren
             } else {
                 // 没有佩戴眼镜
                 boolean checkCyl = cyl.abs().compareTo(new BigDecimal("1.5")) < 0;
+                // 小学生 && 0<=SE<2 && Cyl <1.5
                 if ((SchoolAge.PRIMARY.code.equals(schoolAge)
                         && isBetween(se, new BigDecimal("0.00"), new BigDecimal("2.00"))
                         && checkCyl)
+                        // 初中生 && -0.5<=SE<3 && Cyl <1.5
                         || (SchoolAge.JUNIOR.code.equals(schoolAge)
                         && isBetween(se, new BigDecimal("-0.50"), new BigDecimal("3.00"))
                         && checkCyl)
                 ) {
                     return "裸眼远视力下降，视功能可能异常。建议：请到医疗机构接受检查，明确诊断并及时采取措施。";
-
+                    // 小学生 && !(0 <= SE < 2)
                 } else if ((SchoolAge.PRIMARY.code.equals(schoolAge)
                         && !isBetween(se, new BigDecimal("0.00"), new BigDecimal("2.00")))
+                        // 初中生 && (Cyl >= 1.5 || !(-0.5 <= SE < 3))
                         || (SchoolAge.JUNIOR.code.equals(schoolAge)
-                        && !isBetween(se, new BigDecimal("-0.50"), new BigDecimal("3.00")))) {
+                        && (!isBetween(se, new BigDecimal("-0.50"), new BigDecimal("3.00")) || !checkCyl))) {
                     return "裸眼远视力下降，屈光不正筛查阳性。建议：请到医疗机构接受检查，明确诊断并及时采取措施。";
                 }
                 return "暂无建议";
             }
         }
+        // SE >= 0
         if (se.compareTo(new BigDecimal("0.00")) >= 0) {
             return "裸眼远视力≥4.9，目前尚无近视高危因素。建议：1、6-12个月复查。2、6岁儿童SE≥+2.00D，请到医疗机构接受检查。";
+            // SE < 0
         } else {
             return "裸眼远视力≥4.9，可能存在近视高危因素。建议：1、严格注意用眼卫生。2、到医疗机构检查了解是否可能发展未近视。";
         }
@@ -668,7 +673,7 @@ public class ParentStudentService extends BaseService<ParentStudentMapper, Paren
      */
     private BigDecimal calculationSE(BigDecimal sph, BigDecimal cyl) {
         return sph.add(cyl.multiply(new BigDecimal("0.5")))
-                .setScale(2,BigDecimal.ROUND_HALF_UP);
+                .setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
     /**
