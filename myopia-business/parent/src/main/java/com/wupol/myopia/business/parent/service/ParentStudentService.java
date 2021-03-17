993252ab5a1d3f5da0fbd1a5aa94bf5c4071d3b1
Simple4H <crescentcxm@gmail.com>
2021-03-17 15:33:56 +0800

    feat(parent): 柱镜*100

--- myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/service/ParentStudentService.java
+++ myopia-business/parent/src/main/java/com/wupol/myopia/business/parent/service/ParentStudentService.java
@@ -824,9 +824,9 @@ public class ParentStudentService extends BaseService<ParentStudentMapper, Paren
             ComputerOptometryDO computerOptometry = result.getComputerOptometry();
             if (Objects.nonNull(computerOptometry)) {
                 // 左眼
-                left.setVision(computerOptometry.getLeftEyeData().getCyl());
+                left.setVision(computerOptometry.getLeftEyeData().getCyl().multiply(new BigDecimal("100")));
                 // 右眼
-                right.setVision(computerOptometry.getRightEyeData().getCyl());
+                right.setVision(computerOptometry.getRightEyeData().getCyl().multiply(new BigDecimal("100"));
             }
             details.setItem(Lists.newArrayList(left, right));
             return details;
