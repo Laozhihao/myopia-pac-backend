89159c7f7da45ed95a67bbb25ab9a18acf4010c6
huangpm <huangpm@wupol.com>
2021-03-18 10:45:23 +0800

    feat($schedule): 1. 完成筛查数据导出

---
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/domain/vo/StatConclusionExportVo.java
@@ -0,0 +1,100 @@
+package com.wupol.myopia.business.management.domain.vo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wupol.myopia.business.management.domain.dos.ComputerOptometryDO;
+import com.wupol.myopia.business.management.domain.dos.VisionDataDO;
+import com.wupol.myopia.business.management.domain.model.StatConclusion;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * 处理后筛查数据（包括学校ID）
+ * @author Alix
+ * @Date 2021/3/5
+ **/
+
+@Data
+@Accessors(chain = true)
+public class StatConclusionExportVo extends StatConclusion {
+
+    /**
+     * 筛查计划--参与筛查的学生名字
+     */
+    private String studentName;
+
+    /**
+     * 性别 0-男 1-女
+     */
+    private Integer gender;
+
+    /**
+     * 出生日期
+     */
+    private Date birthday;
+
+    /**
+     * 民族 0-汉族
+     */
+    private Integer nation;
+
+    /**
+     * 学校Id
+     */
+    private Integer schoolId;
+
+    /**
+     * 筛查计划--执行的学校编号
+     */
+    private String schoolNo;
+
+    /**
+     * 筛查计划--执行的学校名字
+     */
+    private String schoolName;
+
+    /**
+     * 筛查计划--年级名称
+     */
+    private String gradeName;
+
+    /**
+     * 筛查计划--年级名称
+     */
+    private String className;
+
+    /**
+     * 筛查计划--参与筛查的学生编号
+     */
+    private String studentNo;
+
+    /**
+     * 筛查计划--参与筛查的学生身份证号码
+     */
+    private String idCard;
+
+    /**
+     * 家长手机号码
+     */
+    private String parentPhone;
+
+    /**
+     * 详细地址
+     */
+    private String address;
+
+    /**
+     * 筛查结果--视力检查结果
+     */
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private VisionDataDO visionData;
+
+    /**
+     * 筛查结果--电脑验光
+     */
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private ComputerOptometryDO computerOptometry;
+}
