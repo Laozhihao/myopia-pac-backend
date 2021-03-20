a4e9dc1862fb223dd39e603892877296d212f30d
lzh <zhihao.lao@vistel.cn>
2021-03-20 20:19:50 +0800

    feat: export report

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/ReportController.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/ReportController.java
@@ -2,24 +2,33 @@ package com.wupol.myopia.business.management.controller;
 
 import com.wupol.myopia.base.domain.ApiResult;
 import com.wupol.myopia.base.handler.ResponseResultBody;
+import com.wupol.myopia.base.util.CurrentUserUtil;
+import com.wupol.myopia.business.management.service.ReportService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 
 /**
  * @Author HaoHao
  * @Date 2021/3/16
  **/
+@Validated
 @ResponseResultBody
 @CrossOrigin
 @RestController
 @RequestMapping("/management/report")
 @Log4j2
 public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
 
     /**
      * 导出区域筛查报告
@@ -29,8 +38,9 @@ public class ReportController {
      * @return com.wupol.myopia.base.domain.ApiResult
      **/
     @GetMapping("/district/export")
-    public ApiResult exportDistrictReport(@NotNull(message = "筛查通知ID不能为空") Integer notificationId, @NotNull(message = "行政区域ID不能为空") Integer districtId) {
-
+    public ApiResult exportDistrictReport(@NotNull(message = "筛查通知ID不能为空") Integer notificationId, @NotNull(message = "行政区域ID不能为空") Integer districtId) throws IOException {
+        // TODO: 权限校验、导出次数限制
+        reportService.exportDistrictReport(notificationId, districtId, CurrentUserUtil.getCurrentUser());
         return ApiResult.success();
     }
 
