52bd1984d875d5dfaa2a5bfd1d6e1af99f56c01b
Simple4H <crescentcxm@gmail.com>
2021-03-22 17:25:58 +0800

    feat(management): update

--- myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/SchoolController.java
+++ myopia-business/management/src/main/java/com/wupol/myopia/business/management/controller/SchoolController.java
@@ -8,6 +8,7 @@ import com.wupol.myopia.base.util.CurrentUserUtil;
 import com.wupol.myopia.business.management.constant.SchoolAge;
 import com.wupol.myopia.business.management.domain.dto.ResetPasswordRequest;
 import com.wupol.myopia.business.management.domain.dto.StatusRequest;
+import com.wupol.myopia.business.management.domain.dto.UsernameAndPasswordDTO;
 import com.wupol.myopia.business.management.domain.model.School;
 import com.wupol.myopia.business.management.domain.query.PageRequest;
 import com.wupol.myopia.business.management.domain.query.SchoolQuery;
@@ -21,8 +22,9 @@ import javax.validation.ValidationException;
 import java.io.IOException;
 
 /**
- * @Author HaoHao
- * @Date 2020-12-22
+ * 学校Controller
+ *
+ * @author Simple4H
  */
 @ResponseResultBody
 @CrossOrigin
@@ -36,14 +38,26 @@ public class SchoolController {
     @Autowired
     private ExcelFacade excelFacade;
 
+    /**
+     * 新增学校
+     *
+     * @param school 学校实体
+     * @return 账号密码 {@link UsernameAndPasswordDTO}
+     */
     @PostMapping()
-    public Object saveSchool(@RequestBody @Valid School school) {
+    public Object getSchoolDetail(@RequestBody @Valid School school) {
         CurrentUser user = CurrentUserUtil.getCurrentUser();
         school.setCreateUserId(user.getId());
         school.setGovDeptId(user.getOrgId());
         return schoolService.saveSchool(school);
     }
 
+    /**
+     * 更新学校
+     *
+     * @param school 学校实体
+     * @return 学校实体
+     */
     @PutMapping()
     public Object updateSchool(@RequestBody @Valid School school) {
         CurrentUser user = CurrentUserUtil.getCurrentUser();
@@ -52,34 +66,73 @@ public class SchoolController {
         return schoolService.updateSchool(school, user);
     }
 
+    /**
+     * 通过ID获取学校详情
+     *
+     * @param id 学校ID
+     * @return 学校实体
+     */
     @GetMapping("{id}")
-    public Object saveSchool(@PathVariable("id") Integer id) {
+    public Object getSchoolDetail(@PathVariable("id") Integer id) {
         return schoolService.getBySchoolId(id);
     }
 
+    /**
+     * 删除学校
+     *
+     * @param id 学校ID
+     * @return 删除数量
+     */
     @DeleteMapping("{id}")
     public Object deletedSchool(@PathVariable("id") Integer id) {
         return schoolService.deletedSchool(id);
     }
 
+    /**
+     * 学校列表
+     *
+     * @param pageRequest 分页请求
+     * @param schoolQuery 请求条件
+     * @return 学校列表
+     */
     @GetMapping("list")
     public Object getSchoolList(PageRequest pageRequest, SchoolQuery schoolQuery) {
         CurrentUser user = CurrentUserUtil.getCurrentUser();
         return schoolService.getSchoolList(pageRequest, schoolQuery, user);
     }
 
+    /**
+     * 更新学校状态
+     *
+     * @param statusRequest 请求入参
+     * @return 更新个数
+     */
     @PutMapping("status")
     public Object updateStatus(@RequestBody @Valid StatusRequest statusRequest) {
         CurrentUserUtil.getCurrentUser();
         return schoolService.updateStatus(statusRequest);
     }
 
+    /**
+     * 重置密码
+     *
+     * @param request 请求入参
+     * @return 账号密码 {@link UsernameAndPasswordDTO}
+     */
     @PostMapping("reset")
     public Object resetPassword(@RequestBody @Valid ResetPasswordRequest request) {
         CurrentUserUtil.getCurrentUser();
         return schoolService.resetPassword(request.getId());
     }
 
+    /**
+     * 导出学校
+     *
+     * @param districtId 行政区域
+     * @return 是否成功
+     * @throws IOException   io异常
+     * @throws UtilException 工具异常
+     */
     @GetMapping("/export")
     public Object getSchoolExportData(Integer districtId) throws IOException, UtilException {
         CurrentUser currentUser = CurrentUserUtil.getCurrentUser();
@@ -87,21 +140,47 @@ public class SchoolController {
         return ApiResult.success();
     }
 
+    /**
+     * 获取学校的筛查记录列表
+     *
+     * @param pageRequest 分页请求
+     * @param schoolId    学校ID
+     * @return 筛查记录列表
+     */
     @GetMapping("screening/record/lists/{schoolId}")
     public Object getScreeningRecordLists(PageRequest pageRequest, @PathVariable("schoolId") Integer schoolId) {
         return schoolService.getScreeningRecordLists(pageRequest, schoolId);
     }
 
+    /**
+     * 学校编号是否被使用
+     *
+     * @param schoolId 学校ID
+     * @param schoolNo 学校编号
+     * @return 是否被使用
+     */
     @GetMapping("/checkSchoolNo/{schoolId}/{schoolNo}")
     public Object checkSchoolNo(@PathVariable("schoolId") Integer schoolId, @PathVariable("schoolNo") String schoolNo) {
         return schoolService.checkSchoolNo(schoolId, schoolNo);
     }
 
+    /**
+     * 通过名字获取学校列表
+     *
+     * @param schoolName 学校名称
+     * @return 学校列表
+     */
     @GetMapping("/getSchools/{schoolName}")
     public Object getSchoolByName(@PathVariable("schoolName") String schoolName) {
         return schoolService.getBySchoolName(schoolName);
     }
 
+    /**
+     * 通过districtId获取学校列表
+     *
+     * @param districtId 行政区域
+     * @return 学校列表
+     */
     @GetMapping("/getSchoolsByDistrictId/{districtId}")
     public Object getSchoolsByDistrictId(@PathVariable("districtId") Integer districtId) {
         return schoolService.getByDistrictId(districtId);
@@ -109,8 +188,9 @@ public class SchoolController {
 
     /**
      * 筛查计划新增学校：机构所在省份全省学校
-     * @param schoolQuery
-     * @return
+     *
+     * @param schoolQuery 查询条件
+     * @return 学校列表
      */
     @GetMapping("/listByDistrict")
     public Object getSchoolListByDistctId(SchoolQuery schoolQuery) {
@@ -122,6 +202,11 @@ public class SchoolController {
         return schoolService.getSchoolListByDistrictId(schoolQuery);
     }
 
+    /**
+     * 获取学龄段列表
+     *
+     * @return 学龄段列表
+     */
     @GetMapping("/schoolAge/list")
     public Object getSchoolAge() {
         return SchoolAge.getSchoolAgeList();
