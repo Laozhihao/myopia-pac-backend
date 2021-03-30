357bbddf68a2408e7768845ca03bc4b82963bef7
Simple4H <crescentcxm@gmail.com>
2021-03-30 11:26:21 +0800

    feat(management): update

--- base-service/src/main/java/com/wupol/myopia/base/service/BaseService.java
+++ base-service/src/main/java/com/wupol/myopia/base/service/BaseService.java
@@ -7,13 +7,11 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -47,20 +45,6 @@ public abstract class BaseService<M extends BaseMapper<T>, T> extends ServiceImp
      **/
     public List<T> findByList(T entity) throws IOException {
         return list(getQueryWrapper(entity));
-    }
-
-    /**
-     * 查询List集合
-     *
-     * @param entity 查询实体参数
-     * @return java.util.List<T>
-     **/
-    public List<T> findByList(T entity, String column, Boolean isDesc) throws IOException {
-        if (StringUtils.isEmpty(column)){
-            return list(getQueryWrapper(entity));
-        }
-        if (isDesc) return list(getQueryWrapper(entity).orderByDesc(column));
-        return list(getQueryWrapper(entity).orderByAsc(column));
     }
 
     /**
@@ -127,5 +111,15 @@ public abstract class BaseService<M extends BaseMapper<T>, T> extends ServiceImp
      **/
     public int count(T entity) throws IOException {
         return count(getQueryWrapper(entity));
+    }
+
+    /**
+     * 更新或新增
+     *
+     * @param entity 实体
+     * @return boolean
+     */
+    public boolean updateOrSave(T entity) {
+        return saveOrUpdate(entity);
     }
 }
