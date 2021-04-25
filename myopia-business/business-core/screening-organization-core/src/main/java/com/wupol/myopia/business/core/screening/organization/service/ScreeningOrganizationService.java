6e956d759e80ecac3086e036cb21a4232be22077
Simple4H <crescentcxm@gmail.com>
2021-04-25 17:10:25 +0800

    feat(all): 筛查机构

--- myopia-business/business-core/screening-organization-core/src/main/java/com/wupol/myopia/business/core/screening/organization/service/ScreeningOrganizationService.java
+++ myopia-business/business-core/screening-organization-core/src/main/java/com/wupol/myopia/business/core/screening/organization/service/ScreeningOrganizationService.java
@@ -1,10 +1,8 @@
 package com.wupol.myopia.business.core.screening.organization.service;
 
-import cn.hutool.core.lang.Assert;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wupol.myopia.base.constant.SystemCode;
-import com.wupol.myopia.base.domain.CurrentUser;
 import com.wupol.myopia.base.exception.BusinessException;
 import com.wupol.myopia.base.service.BaseService;
 import com.wupol.myopia.base.util.PasswordGenerator;
@@ -17,20 +15,17 @@ import com.wupol.myopia.business.core.screening.organization.domain.dto.Screenin
 import com.wupol.myopia.business.core.screening.organization.domain.mapper.ScreeningOrganizationMapper;
 import com.wupol.myopia.business.core.screening.organization.domain.model.ScreeningOrganization;
 import com.wupol.myopia.business.core.screening.organization.domain.model.ScreeningOrganizationAdmin;
-import com.wupol.myopia.business.core.screening.organization.domain.model.ScreeningOrganizationStaff;
 import com.wupol.myopia.oauth.sdk.client.OauthServiceClient;
 import com.wupol.myopia.oauth.sdk.domain.request.UserDTO;
 import com.wupol.myopia.oauth.sdk.domain.response.User;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Date;
+import java.util.List;
 
 /**
  * 筛查机构
@@ -42,32 +37,10 @@ import java.util.stream.Collectors;
 public class ScreeningOrganizationService extends BaseService<ScreeningOrganizationMapper, ScreeningOrganization> {
 
     @Resource
-    private ScreeningOrganizationStaffService screeningOrganizationStaffService;
-
-    @Resource
     private ScreeningOrganizationAdminService screeningOrganizationAdminService;
 
     @Resource
-    private DistrictService districtService;
-
-    @Resource
     private OauthServiceClient oauthServiceClient;
-
-    @Resource
-    private ScreeningTaskOrgService screeningTaskOrgService;
-
-    @Resource
-    private VisionScreeningResultService visionScreeningResultService;
-
-    @Resource
-    private SchoolService schoolService;
-
-    @Resource
-    private ScreeningPlanService screeningPlanService;
-
-    @Resource
-    private ScreeningPlanSchoolService screeningPlanSchoolService;
-
 
     /**
      * 保存筛查机构
@@ -117,59 +90,6 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
         return new UsernameAndPasswordDTO(username, password);
     }
 
-    /**
-     * 更新筛查机构
-     *
-     * @param currentUser           当前登录用户
-     * @param screeningOrganization 筛查机构实体咧
-     * @return 筛查机构
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public ScreeningOrgResponseDTO updateScreeningOrganization(CurrentUser currentUser, ScreeningOrganization screeningOrganization) {
-
-        if (checkScreeningOrgName(screeningOrganization.getName(), screeningOrganization.getId())) {
-            throw new BusinessException("筛查机构名称不能重复");
-        }
-        ScreeningOrgResponseDTO response = new ScreeningOrgResponseDTO();
-        ScreeningOrganization checkOrg = baseMapper.selectById(screeningOrganization.getId());
-
-        // 机构管理员
-        ScreeningOrganizationAdmin admin = screeningOrganizationAdminService.getByOrgId(screeningOrganization.getId());
-        // 更新OAuth账号
-        UserDTO userDTO = new UserDTO();
-        userDTO.setId(admin.getUserId())
-                .setPhone(screeningOrganization.getPhone())
-                .setRealName(screeningOrganization.getName())
-                .setUsername(screeningOrganization.getName());
-        oauthServiceClient.modifyUser(userDTO);
-
-        // 名字更新重置密码
-        if (!StringUtils.equals(checkOrg.getName(), screeningOrganization.getName())) {
-            response.setUpdatePassword(Boolean.TRUE);
-            response.setUsername(screeningOrganization.getName());
-            // 重置密码
-            String password = PasswordGenerator.getScreeningAdminPwd();
-            oauthServiceClient.resetPwd(admin.getUserId(), password);
-            response.setPassword(password);
-        }
-
-        baseMapper.updateById(screeningOrganization);
-        ScreeningOrganization organization = baseMapper.selectById(screeningOrganization.getId());
-        BeanUtils.copyProperties(organization, response);
-        response.setDistrictName(districtService.getDistrictName(organization.getDistrictDetail()));
-        // 详细地址
-        response.setAddressDetail(districtService.getAddressDetails(
-                organization.getProvinceCode(), organization.getCityCode(), organization.getAreaCode(), organization.getTownCode(), organization.getAddress()));
-        response.setScreeningTime(screeningOrganization.getScreeningTime())
-                .setStaffCount(screeningOrganization.getStaffCount());
-        // 是否能更新
-        if (currentUser.isPlatformAdminUser()) {
-            response.setCanUpdate(true);
-        } else if (response.getCreateUserId().equals(currentUser.getId())) {
-            response.setCanUpdate(true);
-        }
-        return response;
-    }
 
     /**
      * 删除筛查机构
@@ -185,111 +105,6 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
         return baseMapper.updateById(screeningOrganization);
     }
 
-    /**
-     * 获取筛查机构列表
-     *
-     * @param pageRequest 分页
-     * @param query       筛查机构列表请求体
-     * @param currentUser 当前登录用户
-     * @return IPage<ScreeningOrgResponse> {@link IPage}
-     */
-    public IPage<ScreeningOrgResponseDTO> getScreeningOrganizationList(PageRequest pageRequest,
-                                                                       ScreeningOrganizationQueryDTO query,
-                                                                       CurrentUser currentUser) {
-        Integer districtId = districtService.filterQueryDistrictId(currentUser, query.getDistrictId());
-
-        // 查询
-        IPage<ScreeningOrgResponseDTO> orgLists = baseMapper.getScreeningOrganizationListByCondition(
-                pageRequest.toPage(), query.getName(), query.getType(), query.getConfigType(), districtId,
-                query.getGovDeptId(), query.getPhone(), query.getStatus());
-
-        // 为空直接返回
-        List<ScreeningOrgResponseDTO> orgListsRecords = orgLists.getRecords();
-        if (CollectionUtils.isEmpty(orgListsRecords)) {
-            return orgLists;
-        }
-
-        // 获取筛查人员信息
-        Map<Integer, List<ScreeningOrganizationStaff>> staffMaps = screeningOrganizationStaffService
-                .getOrgStaffMapByIds(orgListsRecords.stream().map(ScreeningOrganization::getId)
-                        .collect(Collectors.toList()));
-        // 获取已有任务的机构ID列表
-        List<Integer> haveTaskOrgIds = getHaveTaskOrgIds(query);
-
-        // 筛查次数
-        List<ScreeningPlan> planLists = screeningPlanService
-                .getByOrgIds(orgListsRecords.stream().map(ScreeningOrganization::getId)
-                        .collect(Collectors.toList()));
-        Map<Integer, Long> orgPlanMaps = planLists.stream().collect(Collectors
-                .groupingBy(ScreeningPlan::getScreeningOrgId, Collectors.counting()));
-        // 封装DTO
-        orgListsRecords.forEach(orgResponseDTO -> {
-            // 同一部门才能更新
-            if (currentUser.isPlatformAdminUser()) {
-                orgResponseDTO.setCanUpdate(true);
-            } else if (orgResponseDTO.getCreateUserId().equals(currentUser.getId())) {
-                orgResponseDTO.setCanUpdate(true);
-            }
-
-            // 筛查人员
-            List<ScreeningOrganizationStaff> staffLists = staffMaps.get(orgResponseDTO.getId());
-            if (!CollectionUtils.isEmpty(staffLists)) {
-                orgResponseDTO.setStaffCount(staffLists.size());
-            } else {
-                orgResponseDTO.setStaffCount(0);
-            }
-            // 区域名字
-            orgResponseDTO.setDistrictName(districtService.getDistrictName(orgResponseDTO.getDistrictDetail()));
-
-            // 筛查次数
-            orgResponseDTO.setScreeningTime(orgPlanMaps.getOrDefault(orgResponseDTO.getId(),0L));
-            orgResponseDTO.setAlreadyHaveTask(haveTaskOrgIds.contains(orgResponseDTO.getId()));
-
-            // 详细地址
-            orgResponseDTO.setAddressDetail(districtService.getAddressDetails(
-                    orgResponseDTO.getProvinceCode(), orgResponseDTO.getCityCode(), orgResponseDTO.getAreaCode(), orgResponseDTO.getTownCode(), orgResponseDTO.getAddress()));
-        });
-        return orgLists;
-    }
-
-    /**
-     * 根据部门ID获取筛查机构列表（带是否已有任务）
-     *
-     * @param query 筛查机构列表请求体
-     * @return List<ScreeningOrgResponse>
-     */
-    public List<ScreeningOrgResponseDTO> getScreeningOrganizationListByGovDeptId(ScreeningOrganizationQueryDTO query) {
-        Assert.notNull(query.getGovDeptId(), "部门id不能为空");
-        query.setStatus(CommonConst.STATUS_NOT_DELETED);
-        // 查询
-        List<ScreeningOrganization> screeningOrganizationList = baseMapper.getBy(query);
-        // 为空直接返回
-        if (CollectionUtils.isEmpty(screeningOrganizationList)) {
-            return Collections.emptyList();
-        }
-        // 获取已有任务的机构ID列表
-        List<Integer> haveTaskOrgIds = getHaveTaskOrgIds(query);
-        // 封装DTO
-        return screeningOrganizationList.stream().map(organization -> {
-            ScreeningOrgResponseDTO orgResponseDTO = new ScreeningOrgResponseDTO();
-            BeanUtils.copyProperties(organization, orgResponseDTO);
-            orgResponseDTO.setAlreadyHaveTask(haveTaskOrgIds.contains(organization.getId()));
-            return orgResponseDTO;
-        }).collect(Collectors.toList());
-    }
-
-    /**
-     * 根据是否需要查询机构是否已有任务，返回时间段内已有任务的机构id
-     *
-     * @param query 条件
-     * @return List<Integer>
-     */
-    private List<Integer> getHaveTaskOrgIds(ScreeningOrganizationQueryDTO query) {
-        if (Objects.nonNull(query.getNeedCheckHaveTask()) && query.getNeedCheckHaveTask()) {
-            return screeningTaskOrgService.getHaveTaskOrgIds(query.getGovDeptId(), query.getStartTime(), query.getEndTime());
-        }
-        return Collections.emptyList();
-    }
 
     /**
      * 获取导出数据
@@ -412,5 +227,19 @@ public class ScreeningOrganizationService extends BaseService<ScreeningOrganizat
      */
     public Boolean checkScreeningOrgName(String name, Integer id) {
         return baseMapper.getByNameAndNeId(name, id).size() > 0;
+    }
+
+    /**
+     * 获取筛查机构列表
+     *
+     * @param pageRequest 分页请求
+     * @param query       查询条件
+     * @param districtId  行政区域
+     * @return 筛查机构列表
+     */
+    public IPage<ScreeningOrgResponseDTO> getByCondition(PageRequest pageRequest, ScreeningOrganizationQueryDTO query, Integer districtId) {
+        return baseMapper.getScreeningOrganizationListByCondition(
+                pageRequest.toPage(), query.getName(), query.getType(), query.getConfigType(), districtId,
+                query.getGovDeptId(), query.getPhone(), query.getStatus());
     }
 }
