6fd9d51aae14956f044e6fd0bb1f8c39d861e30f
lzh <zhihao.lao@vistel.cn>
2021-04-29 14:48:14 +0800

    add config

--- myopia-oauth-backup/src/main/java/com/wupol/myopia/oauth/MyopiaOauthBackupApplication.java
+++ myopia-oauth-backup/src/main/java/com/wupol/myopia/oauth/MyopiaOauthBackupApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan("com.wupol.myopia.oauth.**.domain.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = { "com.wupol.myopia" })
-public class MyopiaOauthApplication {
+public class MyopiaOauthBackupApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyopiaOauthApplication.class, args);
+        SpringApplication.run(MyopiaOauthBackupApplication.class, args);
     }
 
 }
