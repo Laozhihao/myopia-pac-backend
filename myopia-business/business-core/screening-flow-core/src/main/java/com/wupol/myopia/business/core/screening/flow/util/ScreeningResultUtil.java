0f67a70a0357625c09d9d99f1d885bae4efade5b
Simple4H <crescentcxm@gmail.com>
2021-05-10 14:28:13 +0800

    feat(parent): code review

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/util/ScreeningResultUtil.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/util/ScreeningResultUtil.java
@@ -621,7 +621,7 @@ public class ScreeningResultUtil {
      * @param end   结束值
      * @return 是否在区间内
      */
-    public static Boolean isBetweenAll(BigDecimal val, BigDecimal start, BigDecimal end) {
+    public static boolean isBetweenAll(BigDecimal val, BigDecimal start, BigDecimal end) {
         return val.compareTo(start) >= 0 && val.compareTo(end) <= 0;
     }
 
@@ -847,11 +847,9 @@ public class ScreeningResultUtil {
         BigDecimal visionVal = getResultVision(leftCorrectedVision, rightCorrectedVision,
                 leftNakedVision, rightNakedVision, nakedVisionResult);
         if (visionVal.compareTo(new BigDecimal("4.9")) < 0) {
-            // 矫正视力小于4.9
-            return "裸眼远视力下降，戴镜远视力下降。建议：请及时到医疗机构复查。";
+            return DoctorConclusion.CorrectedVisionLessThan_49;
         } else {
-            // 矫正视力大于4.9
-            return "裸眼远视力下降，戴镜远视力≥4.9。建议：请3个月或半年1次检查裸眼视力和戴镜视力。";
+            return DoctorConclusion.CorrectedVisionGreaterThan_49;
         }
     }
 
@@ -897,12 +895,12 @@ public class ScreeningResultUtil {
                 ||
                 (SchoolAge.isMiddleSchool(schoolAge) && isBetweenLeft(se, "-0.50", "3.00") && checkCyl)
         ) {
-            return new TwoTuple<>(1, "裸眼远视力下降，视功能可能异常。建议：请到医疗机构接受检查，明确诊断并及时采取措施。");
+            return new TwoTuple<>(1, DoctorConclusion.VisualFunctionAbnormal);
             // (小学生 && !(0 <= SE < 2)) || (初中生、高中、职业高中 && (Cyl >= 1.5 || !(-0.5 <= SE < 3)))
         } else if ((SchoolAge.PRIMARY.code.equals(schoolAge) && !isBetweenLeft(se, "0.00", "2.00"))
                 ||
                 (SchoolAge.isMiddleSchool(schoolAge) && (!isBetweenLeft(se, "-0.50", "3.00") || !checkCyl))) {
-            return new TwoTuple<>(2, "裸眼远视力下降，屈光不正筛查阳性。建议：请到医疗机构接受检查，明确诊断并及时采取措施。");
+            return new TwoTuple<>(2, DoctorConclusion.RefractiveErrorScreeningPositive);
         }
         return new TwoTuple<>(0, "");
     }
@@ -921,12 +919,10 @@ public class ScreeningResultUtil {
                                            BigDecimal leftSe, BigDecimal rightSe,
                                            TwoTuple<BigDecimal, Integer> nakedVisionResult) {
         BigDecimal se = getSE(leftNakedVision, rightNakedVision, leftSe, rightSe, nakedVisionResult);
-        // SE >= 0
         if (se.compareTo(new BigDecimal("0.00")) >= 0) {
-            return "裸眼远视力≥4.9，目前尚无近视高危因素。建议：1、6-12个月复查。2、6岁儿童SE≥+2.00D，请到医疗机构接受检查。";
+            return DoctorConclusion.NormalSEGreaterThan_0;
         } else {
-            // SE < 0
-            return "裸眼远视力≥4.9，可能存在近视高危因素。建议：1、严格注意用眼卫生。2、到医疗机构检查了解是否可能发展未近视。";
+            return DoctorConclusion.NormalSELessThan_0;
         }
     }
 
