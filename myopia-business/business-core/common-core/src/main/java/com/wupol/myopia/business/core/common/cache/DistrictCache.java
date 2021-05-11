46c3bb225c1843322a1f3e3e29e12b5102ee1a9d
lzh <zhihao.lao@vistel.cn>
2021-05-11 23:05:00 +0800

    upgrade logger lever to info

--- myopia-business/business-core/common-core/src/main/java/com/wupol/myopia/business/core/common/cache/DistrictCache.java
+++ myopia-business/business-core/common-core/src/main/java/com/wupol/myopia/business/core/common/cache/DistrictCache.java
@@ -34,32 +34,32 @@ public class DistrictCache implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        logger.debug("开始缓存district数据");
+        logger.info("开始缓存district数据");
         // 缓存全国行政区域-列表结构
         if (!redisUtil.hasKey(DistrictCacheKey.DISTRICT_ALL_LIST)) {
-            logger.debug("...缓存全国行政区域-列表结构");
+            logger.info("...缓存全国行政区域-列表结构");
             List<District> districtList = districtService.findByList(new District());
             Map<String, Object> districtMap = districtList.stream().collect(Collectors.toMap(x -> String.valueOf(x.getCode()), Function.identity()));
             redisUtil.hmset(DistrictCacheKey.DISTRICT_ALL_LIST, districtMap);
-            logger.debug("...完成缓存全国行政区域-列表结构");
+            logger.info("...完成缓存全国行政区域-列表结构");
         }
 
         // 缓存全国行政区域-树结构
         if (!redisUtil.hasKey(DistrictCacheKey.DISTRICT_ALL_TREE)) {
-            logger.debug("...缓存全国行政区域-树结构");
+            logger.info("...缓存全国行政区域-树结构");
             redisUtil.set(DistrictCacheKey.DISTRICT_ALL_TREE, districtService.getWholeCountryDistrictTree());
-            logger.debug("...完成全国行政区域-树结构");
+            logger.info("...完成全国行政区域-树结构");
         }
 
         // 缓存各省行政区域-树结构
         if (!redisUtil.hasKey(DistrictCacheKey.DISTRICT_ALL_PROVINCE_TREE)) {
-            logger.debug("...缓存各省行政区域-树结构");
+            logger.info("...缓存各省行政区域-树结构");
             Object cacheTree = redisUtil.get(DistrictCacheKey.DISTRICT_ALL_TREE);
             List<District> allDistrictTree = JSON.parseObject(JSON.toJSONString(cacheTree), new TypeReference<List<District>>() {});
             Map<String, Object> districtMap = allDistrictTree.stream().collect(Collectors.toMap(x -> String.valueOf(x.getCode()).substring(0, 2), Function.identity()));
-            redisUtil.hmset(DistrictCacheKey.DISTRICT_ALL_PROVINCE_TREE, districtMap);
-            logger.debug("...完成缓存各省行政区域-树结构");
+//            redisUtil.hmset(DistrictCacheKey.DISTRICT_ALL_PROVINCE_TREE, districtMap);
+            logger.info("...完成缓存各省行政区域-树结构");
         }
-        logger.debug("完成缓存district数据");
+        logger.info("完成缓存district数据");
     }
 }
