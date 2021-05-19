7b9c7aadaf1b862c79a8584b7159297fb0f96bc5
Simple4H <crescentcxm@gmail.com>
2021-05-19 10:13:52 +0800

    feat(parent): init OrgCooperationHospital

---
+++ myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/domain/mapper/OrgCooperationHospitalMapper.java
@@ -0,0 +1,15 @@
+package com.wupol.myopia.business.core.hospital.domain.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wupol.myopia.business.core.hospital.domain.model.OrgCooperationHospital;
+
+/**
+ * 筛查机构合作医院表Mapper接口
+ *
+ * @Author jacob
+ * @Date 2021-05-19
+ */
+public interface OrgCooperationHospitalMapper extends BaseMapper<OrgCooperationHospital> {
+
+}
---
+++ myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/service/OrgCooperationHospitalService.java
@@ -0,0 +1,15 @@
+package com.wupol.myopia.business.core.hospital.service;
+
+import com.wupol.myopia.base.service.BaseService;
+import com.wupol.myopia.business.core.hospital.domain.mapper.OrgCooperationHospitalMapper;
+import com.wupol.myopia.business.core.hospital.domain.model.OrgCooperationHospital;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author jacob
+ * @Date 2021-05-19
+ */
+@Service
+public class OrgCooperationHospitalService extends BaseService<OrgCooperationHospitalMapper, OrgCooperationHospital> {
+
+}
