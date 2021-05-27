2229819636ef43c86c3e7d211d43441d0767a3e5
lizhou <lizhou.wu@vistel.cn>
2021-05-27 17:57:47 +0800

    优化flyway sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20210518__V1.2.0-update-permission.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20210518__V1.2.0-update-permission.sql
@@ -1,20 +1,3 @@
--- 医院、学校、筛查机构 账号停用\启用，重置密码
-UPDATE `o_permission` SET `api_url` = 'put:/management/hospital', `name` = '医院编辑' WHERE `id` = '37';
-INSERT INTO `o_permission` (`name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`)
-VALUES ('停用/启用', 'hospitalStatusUpdate', 'put:/management/hospital/status', '0', '0', '1', '12', '1'),
-('重置密码', 'hospitalPasswordReset', 'post:/management/hospital/reset', '0', '0', '1', '12', '1');
-
-UPDATE `o_permission` SET `api_url` = 'put:/management/school', `name` = '学校编辑' WHERE `id` = '41';
-INSERT INTO `o_permission` (`name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`)
-VALUES ('停用/启用', 'schoolStatusUpdate', 'put:/management/school/status', '0', '0', '1', '7', '1'),
-('重置密码', 'schoolPasswordReset', 'post:/management/school/reset', '0', '0', '1', '7', '1');
-
-UPDATE `o_permission` SET `api_url` = 'put:/management/screeningOrganization', `name` = '筛查机构编辑' WHERE `id` = '61';
-INSERT INTO `o_permission` (`name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`)
-VALUES ('停用/启用', 'screeningOrganizationStatusUpdate', 'put:/management/screeningOrganization/status', '0', '0', '1', '2', '1'),
-('重置密码', 'screeningOrganizationPasswordReset', 'post:/management/screeningOrganization/reset', '0', '0', '1', '2', '1');
-
-
 -- 导出复测报告功能
 INSERT INTO `o_permission` (`name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`)
 VALUES ('导出复测报告功能', 'statResultSchoolRescreenExport', 'get:/management/stat/rescreen/export', '0', '0', '1', '30', '1'),
