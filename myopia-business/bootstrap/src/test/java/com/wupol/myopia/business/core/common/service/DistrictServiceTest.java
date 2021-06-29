fe90dfe58f75ae26da30fe198d09663d07ef552a
lizhou <lizhou.wu@vistel.cn>
2021-06-29 12:23:55 +0800

    测试用例增加断言

--- myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/core/common/service/DistrictServiceTest.java
+++ myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/core/common/service/DistrictServiceTest.java
@@ -1,13 +1,15 @@
 package com.wupol.myopia.business.core.common.service;
 
 import com.wupol.myopia.business.bootstrap.MyopiaBusinessApplication;
-import com.wupol.myopia.business.common.utils.util.JsonUtil;
 import com.wupol.myopia.business.core.common.domain.model.District;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * @Author wulizhou
@@ -22,8 +24,23 @@ public class DistrictServiceTest {
 
     @Test
     public void testGetProvinceDistrictTreePriorityCache() {
+        // 潮阳区所在省份
         District province = districtService.getProvinceDistrictTreePriorityCache(440582000L);
-        System.out.println(JsonUtil.objectToJsonString(province));
+        List<District> citys = province.getChild();
+        for (District city : citys) {
+            // 必有广州
+            if (city.getCode().equals(440100000L)) {
+                List<District> areas = city.getChild();
+                for (District area : areas) {
+                    // 必有天河区
+                    if (area.getCode().equals(440106000L)) {
+                        Assert.assertTrue(true);
+                        return ;
+                    }
+                }
+            }
+        }
+        Assert.assertTrue(false);
     }
 
 }
