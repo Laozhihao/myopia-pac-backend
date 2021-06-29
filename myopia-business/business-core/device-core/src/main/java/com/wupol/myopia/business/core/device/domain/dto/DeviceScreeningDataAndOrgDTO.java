3eb5f83dca1836d8d18961269dfc1d7820c0512d
lizhou <lizhou.wu@vistel.cn>
2021-06-29 12:04:56 +0800

    增加设备数据返回bean

---
+++ myopia-business/business-core/device-core/src/main/java/com/wupol/myopia/business/core/device/domain/dto/DeviceScreeningDataAndOrgDTO.java
@@ -0,0 +1,17 @@
+package com.wupol.myopia.business.core.device.domain.dto;
+
+import com.wupol.myopia.business.core.device.domain.model.DeviceScreeningData;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @Author wulizhou
+ * @Date 2021/6/29 11:49
+ */
+@Data
+@ToString(callSuper = true)
+public class DeviceScreeningDataAndOrgDTO extends DeviceScreeningData {
+
+    private String orgName;
+
+}
