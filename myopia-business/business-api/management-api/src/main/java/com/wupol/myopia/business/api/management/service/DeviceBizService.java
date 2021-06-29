be19df1e2c5e4d4deafe9fd6aed85b5a67297da0
Simple4H <crescentcxm@gmail.com>
2021-06-29 10:53:45 +0800

    feat(device): 报告打印

--- myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/DeviceBizService.java
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/DeviceBizService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 设备管理
@@ -41,5 +43,79 @@ public class DeviceBizService {
             r.setDoctorAdvice("");
         });
         return responseDTOS;
+    }
+
+    /**
+     * 判断是否近视
+     *
+     * @param leftPa  左眼等效球镜
+     * @param rightPa 右眼等效球镜
+     * @return 是否近视
+     */
+    private boolean checkIsMyopia(BigDecimal leftPa, BigDecimal rightPa) {
+        return checkSingleEyeIsMyopia(leftPa) || checkSingleEyeIsMyopia(rightPa);
+    }
+
+    /**
+     * 单眼是否近视
+     *
+     * @param pa 等效球镜
+     * @return 是否近视
+     */
+    private boolean checkSingleEyeIsMyopia(BigDecimal pa) {
+        if (Objects.isNull(pa)) {
+            return false;
+        }
+        return pa.compareTo(new BigDecimal("-0.5")) < 0;
+    }
+
+    /**
+     * 判断是否远视
+     *
+     * @param patientAge 患者年龄
+     * @param leftPa     左眼等效球镜
+     * @param rightPa    右眼等效球镜
+     * @return 是否远视
+     */
+    private boolean checkIsFarsightedness(Integer patientAge, BigDecimal leftPa, BigDecimal rightPa) {
+
+    }
+
+    /**
+     * 单眼是否远视
+     *
+     * @param patientAge 患者年龄
+     * @param pa         等效球镜
+     * @return 是否远视
+     */
+    private boolean checkSingleEyeIsFarsightedness(Integer patientAge, BigDecimal pa) {
+        switch (patientAge) {
+            case 0:
+                return pa.compareTo(new BigDecimal("3.5")) > 0;
+            case 1:
+                return pa.compareTo(new BigDecimal("3.5")) > 0;
+            case 2:
+                return pa.compareTo(new BigDecimal("3.5")) > 0;
+            case 3:
+                return pa.compareTo(new BigDecimal("3.5")) > 0;
+            case 4:
+                return pa.compareTo(new BigDecimal("2.5")) > 0;
+            case 5:
+                return pa.compareTo(new BigDecimal("2.5")) > 0;
+            case 6:
+                return pa.compareTo(new BigDecimal("2.0")) > 0;
+            case 7:
+                return pa.compareTo(new BigDecimal("2.0")) > 0;
+            case 8:
+                return pa.compareTo(new BigDecimal("1.5")) > 0;
+            case 9:
+                return pa.compareTo(new BigDecimal("1.25")) > 0;
+            case 10:
+                return pa.compareTo(new BigDecimal("1.0")) > 0;
+            case 11:
+                return pa.compareTo(new BigDecimal("0.75")) > 0;
+            default:
+                return pa.compareTo(new BigDecimal("0.50")) > 0;
+        }
     }
 }
