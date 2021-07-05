957b7bef389d7ff9a2e529f8cbd1fc6682115fd5
lizhou <lizhou.wu@vistel.cn>
2021-07-05 17:30:12 +0800

    优化测试用例

--- myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/api/management/service/DeviceScreeningDataBizServiceTest.java
+++ myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/api/management/service/DeviceScreeningDataBizServiceTest.java
@@ -1,9 +1,11 @@
 package com.wupol.myopia.business.api.management.service;
 
+import cn.hutool.core.lang.Assert;
 import com.wupol.myopia.business.bootstrap.MyopiaBusinessApplication;
 import com.wupol.myopia.business.common.utils.domain.query.PageRequest;
 import com.wupol.myopia.business.common.utils.util.JsonUtil;
 import com.wupol.myopia.business.core.device.domain.dto.DeviceScreeningDataQueryDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import java.util.Date;
  * @Author wulizhou
  * @Date 2021/6/29 18:28
  */
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MyopiaBusinessApplication.class)
 public class DeviceScreeningDataBizServiceTest {
@@ -26,17 +29,22 @@ public class DeviceScreeningDataBizServiceTest {
 
     @Test
     public void testGetPage() {
-        DeviceScreeningDataQueryDTO query = new DeviceScreeningDataQueryDTO();
-        PageRequest pageRequest = new PageRequest();
-        System.out.println(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
-        query.setScreeningTimeStart(new Date());
-        System.out.println(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
-        query.setScreeningOrgNameSearch("机构");
-        System.out.println(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
-        query.setSphEnd(new BigDecimal("5.0"));
-        System.out.println(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
-        query.setPatientGender(1);
-        System.out.println(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+        try {
+            DeviceScreeningDataQueryDTO query = new DeviceScreeningDataQueryDTO();
+            PageRequest pageRequest = new PageRequest();
+            log.info(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+            query.setScreeningTimeStart(new Date());
+            log.info(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+            query.setScreeningOrgNameSearch("机构");
+            log.info(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+            query.setSphEnd(new BigDecimal("5.0"));
+            log.info(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+            query.setPatientGender(1);
+            log.info(JsonUtil.objectToJsonString(deviceScreeningDataBizService.getPage(query, pageRequest)));
+        } catch (Exception e) {
+            Assert.isTrue(false);
+        }
+        Assert.isTrue(true);
     }
 
 }
