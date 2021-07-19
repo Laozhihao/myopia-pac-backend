c0629da49bbb1a378e6174e52244fcb874ac9ab2
lzh <zhihao.lao@vistel.cn>
2021-07-19 16:56:14 +0800

    fix: html convert to pdf bug

--- myopia-business/common-utils/src/main/java/com/wupol/myopia/business/common/utils/util/HtmlToPdfUtil.java
+++ myopia-business/common-utils/src/main/java/com/wupol/myopia/business/common/utils/util/HtmlToPdfUtil.java
@@ -46,8 +46,8 @@ public class HtmlToPdfUtil {
             parent.mkdirs();
         }
         // "--window-status 1" 允许js异步请求
-        ProcessBuilder processBuilder = new ProcessBuilder(HTML_TO_PDF_TOOL_COMMAND, "--load-media-error-handling", "ignore", "--load-error-handling", "ignore", "2000", "--window-status", "1", htmlSrcPath, pdfFilePath);
-        log.debug(processBuilder.command().toString());
+        ProcessBuilder processBuilder = new ProcessBuilder(HTML_TO_PDF_TOOL_COMMAND, "--load-media-error-handling", "ignore", "--load-error-handling", "ignore", "--javascript-delay", "2000", "--window-status", "1", htmlSrcPath, pdfFilePath);
+        log.info(processBuilder.command().toString());
         processBuilder.redirectErrorStream(true);
         BufferedReader br = null;
         InputStreamReader reader = null;
