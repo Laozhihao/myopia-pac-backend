b360af4ad7fc887a746da0a8166ee94cc176d30a
lzh <zhihao.lao@vistel.cn>
2021-07-27 18:35:34 +0800

    add screening api

--- myopia-business/business-api/screening-app-api/src/main/java/com/wupol/myopia/business/api/screening/app/domain/dto/FundusDataDTO.java
+++ myopia-business/business-api/screening-app-api/src/main/java/com/wupol/myopia/business/api/screening/app/domain/dto/FundusDataDTO.java
@@ -1,11 +1,8 @@
 package com.wupol.myopia.business.api.screening.app.domain.dto;
 
-import com.wupol.myopia.business.core.screening.flow.domain.dos.FundusDataDO;
-import com.wupol.myopia.business.core.screening.flow.domain.dto.ScreeningResultBasicData;
-import com.wupol.myopia.business.core.screening.flow.domain.model.VisionScreeningResult;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * 眼底数据
@@ -13,10 +10,8 @@ import lombok.experimental.Accessors;
  * @Author HaoHao
  * @Date 2021/7/27
  **/
-@Accessors(chain = true)
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class FundusDataDTO extends ScreeningResultBasicData {
+public class FundusDataDTO implements Serializable {
 
     /**
      * 眼底(左眼)：0-未见异常、1-异常
@@ -30,16 +25,4 @@ public class FundusDataDTO extends ScreeningResultBasicData {
      * 备注说明
      */
     private String remark;
-    /**
-     * 是否配合检查：0-配合、1-不配合
-     */
-    private Integer isCooperative;
-
-    @Override
-    public VisionScreeningResult buildScreeningResultData(VisionScreeningResult visionScreeningResult) {
-        FundusDataDO.FundusData leftFundusData = new FundusDataDO.FundusData().setLateriality(0).setHasAbnormal(leftHasAbnormal);
-        FundusDataDO.FundusData rightFundusData = new FundusDataDO.FundusData().setLateriality(1).setHasAbnormal(rightHasAbnormal);
-        FundusDataDO fundusDataDO = new FundusDataDO().setLeftEyeData(leftFundusData).setRightEyeData(rightFundusData).setIsCooperative(isCooperative).setRemark(remark);
-        return new VisionScreeningResult().setFundusData(fundusDataDO);
-    }
 }
--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dto/SlitLampDataDTO.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dto/SlitLampDataDTO.java
@@ -1,12 +1,9 @@
 package com.wupol.myopia.business.core.screening.flow.domain.dto;
 
-import com.wupol.myopia.business.core.screening.flow.domain.dos.SlitLampDataDO;
-import com.wupol.myopia.business.core.screening.flow.domain.model.VisionScreeningResult;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,10 +14,9 @@ import java.util.List;
  * @Author HaoHao
  * @Date 2021/7/27
  **/
-@Accessors(chain = true)
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class SlitLampDataDTO extends ScreeningResultBasicData {
+public class SlitLampDataDTO implements Serializable {
+
     /**
      * 病变异常组织(左眼)
      */
@@ -40,11 +36,6 @@ public class SlitLampDataDTO extends ScreeningResultBasicData {
      * 初步诊断结果（右眼）：0-正常、1-（疑似）异常
      */
     private Integer rightDiagnosis;
-
-    /**
-     * 是否配合检查：0-配合、1-不配合
-     */
-    private Integer isCooperative;
 
     /**
      * 获取右眼病变组织list
@@ -73,14 +64,6 @@ public class SlitLampDataDTO extends ScreeningResultBasicData {
         }
         String[] diseaseStringArray = pathologicalTissueStr.split(",");
         return Arrays.asList(diseaseStringArray);
-    }
-
-    @Override
-    public VisionScreeningResult buildScreeningResultData(VisionScreeningResult visionScreeningResult) {
-        SlitLampDataDO.SlitLampData rightSlitLampData = new SlitLampDataDO.SlitLampData().setLateriality(1).setDiagnosis(rightDiagnosis).setPathologicalTissues(getRightPathologicalTissueList());
-        SlitLampDataDO.SlitLampData leftSlitLampData = new SlitLampDataDO.SlitLampData().setLateriality(0).setDiagnosis(leftDiagnosis).setPathologicalTissues(getLeftPathologicalTissueList());
-        SlitLampDataDO slitLampDataDO = new SlitLampDataDO().setRightEyeData(rightSlitLampData).setLeftEyeData(leftSlitLampData).setIsCooperative(isCooperative);
-        return visionScreeningResult.setSlitLampData(slitLampDataDO);
     }
 
 }
