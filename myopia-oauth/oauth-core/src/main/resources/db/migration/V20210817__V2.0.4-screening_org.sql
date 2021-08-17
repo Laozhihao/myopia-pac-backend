64687eabb40a2a15e598b077ebda691661ce7a0c
Simple4H <crescentcxm@gmail.com>
2021-08-17 17:02:51 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20210817__V2.0.4-screening_org.sql
@@ -0,0 +1,12 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
+                          update_time)
+VALUES (359, '打印告知书', 'printNotification', 'get:/management/screeningPlan/export/notice', 0, 0, 17, 27, 1,
+        '2021-08-17 10:38:50', '2021-08-17 10:40:08');
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
+                          update_time)
+VALUES (360, '打印筛查二维码', 'printScreeningCode', 'get:/management/screeningPlan/export/QRCode', 0, 0, 18, 27, 1,
+        '2021-08-17 10:42:09', '2021-08-17 10:42:09');
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
+                          update_time)
+VALUES (361, '打印VS666设备专属二维码', 'printEquipmentCode', 'get:/management/screeningPlan/export/QRCode', 0, 0, 19, 27, 1,
+        '2021-08-17 10:44:38', '2021-08-17 10:44:38');
