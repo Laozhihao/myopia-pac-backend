e5434b5693bb885366cd9874ad57e808b319ede3
Simple4H <crescentcxm@gmail.com>
2021-08-19 10:47:26 +0800

    feat(oauth): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20210817__V2.0.4-screening_org.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20210817__V2.0.4-screening_org.sql
@@ -1,5 +1,5 @@
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`,
-                                                          pid, system_code, create_time, update_time)
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
+                          update_time)
 VALUES (360, '初始化筛查机构', 'managementScreeningOrganizationResetOrg', 'get:/management/screeningOrganization/resetOrg', 0,
         0, 1, 348, 1, '2021-08-19 10:39:13', '2021-08-19 10:39:13');
 
