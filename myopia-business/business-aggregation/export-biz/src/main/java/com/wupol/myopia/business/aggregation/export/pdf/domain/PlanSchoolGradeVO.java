b880756e965f5ca80195aacfa91022160c20ff22
Simple4H <crescentcxm@gmail.com>
2021-08-20 16:17:21 +0800

    feat(management): code review

--- myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/pdf/domain/PlanSchoolGradeVO.java
+++ myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/pdf/domain/PlanSchoolGradeVO.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import java.util.List;
 
 /**
- * TODO:
+ * 学校班级-年级
  *
  * @author Simple4H
  */
@@ -15,9 +15,18 @@ import java.util.List;
 @Setter
 public class PlanSchoolGradeVO {
 
+    /**
+     * 年级Id
+     */
     private Integer id;
 
+    /**
+     * 年级名称
+     */
     private String gradeName;
 
+    /**
+     * 班级信息
+     */
     private List<SchoolClass> classes;
 }
