6aafcb1908deeb201c1fd8f3ff17e6a7a6008872
Simple4H <crescentcxm@gmail.com>
2021-09-07 18:15:22 +0800

    feat(management): update sql

--- myopia-business/bootstrap/src/main/resources/db/migration/V20210907__V2.1.0-management.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20210907__V2.1.0-management.sql
@@ -2,7 +2,7 @@ alter table m_screening_plan_school_student
     add artificial int default 0 null comment '0-非人造的、1-人造的';
 
 alter table m_screening_plan_school_student
-    add screening_code long null comment '筛查编号';
+    add screening_code bigint null comment '筛查编号';
 
 alter table m_student
     modify id_card varchar(32) null comment '身份证号码';
@@ -15,3 +15,6 @@ alter table m_screening_plan_school_student
 
 alter table m_screening_plan_school_student
     modify student_name varchar(32) not null comment '筛查计划--参与筛查的学生名字';
+
+create unique index m_screening_plan_school_student_screening_code_uindex
+    on m_screening_plan_school_student (screening_code);
