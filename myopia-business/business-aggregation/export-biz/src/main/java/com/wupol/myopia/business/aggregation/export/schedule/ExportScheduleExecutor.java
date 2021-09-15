3889404e1214c3bde291d99d4145c9a3eab5b159
lzh <zhihao.lao@vistel.cn>
2021-09-15 20:52:17 +0800

    feat: code review

--- myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/schedule/ExportScheduleExecutor.java
+++ myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/schedule/ExportScheduleExecutor.java
@@ -3,15 +3,12 @@ package com.wupol.myopia.business.aggregation.export.schedule;
 import com.wupol.myopia.base.cache.RedisConstant;
 import com.wupol.myopia.base.cache.RedisUtil;
 import com.wupol.myopia.business.aggregation.export.ExportStrategy;
-import com.wupol.myopia.business.aggregation.export.interfaces.ExportFileService;
-import com.wupol.myopia.business.aggregation.export.pdf.domain.ExportCondition;
 import com.wupol.myopia.business.aggregation.export.pdf.domain.QueueInfo;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.Objects;
 
 /**
@@ -30,7 +27,7 @@ public class ExportScheduleExecutor {
     private RedisUtil redisUtil;
 
     @Scheduled(cron = "0/5 * * * * ?")
-    public void export() throws IOException {
+    public void export() {
 
         // 从队列中获取一个任务
         QueueInfo queueInfo = (QueueInfo) redisUtil.lGet(RedisConstant.FILE_EXPORT_LIST);
