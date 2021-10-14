2e1bfbcf2fc591666e4f23b585e3107014375d34
Simple4H <crescentcxm@gmail.com>
2021-10-14 11:34:32 +0800

    feat(management): 近视人数Bug fix

---
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20211012__V2.2.0-school.sql
@@ -0,0 +1,11 @@
+alter table m_school_vision_statistic
+    add myopia_level_light int null comment '轻度近视人数' after focus_targets_numbers;
+
+alter table m_school_vision_statistic
+    add myopia_level_middle int null comment '中度近视人数' after myopia_level_light;
+
+alter table m_school_vision_statistic
+    add myopia_level_high int null comment '高度近视人数' after myopia_level_middle;
+
+alter table m_school_vision_statistic
+    add myopia_level_insufficient int null comment '远视储备不足人数' after myopia_level_high;
