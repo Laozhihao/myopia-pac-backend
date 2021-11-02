08260b57ed409a86696ae7a4896bf7af5b7a3c7f
Simple4H <crescentcxm@gmail.com>
2021-11-02 12:00:38 +0800

    feat(management): update sql

--- myopia-business/bootstrap/src/main/resources/db/migration/V20211012__V2.2.0-school.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20211012__V2.2.0-school.sql
@@ -21,21 +21,6 @@ alter table m_stat_conclusion
 
 alter table m_stat_conclusion
     add astigmatism_level int null comment '散光等级';
-
--- 更新学校表
-UPDATE m_school SET district_detail = '[]' WHERE school_no = '1234567890';
-ALTER TABLE `m_school`
-  MODIFY COLUMN `district_detail` json NOT NULL COMMENT '行政区域json',
-  ADD COLUMN `district_area_code` bigint(20) DEFAULT NULL COMMENT '行政区域-区/县code（含省市）',
-  ADD COLUMN `area_type` tinyint(1) COMMENT '片区：1好片、2中片、3差片',
-  ADD COLUMN `monitor_type` tinyint(1) COMMENT '监测点：1城区、2郊县',
-  ADD INDEX `m_school_district_area_code_area_type_monitor_type_index`(`district_area_code`, `area_type`, `monitor_type`) USING BTREE;
-
--- 更新学生表
-ALTER TABLE `m_student`
-  ADD COLUMN `school_id` int(11) COMMENT '学校ID';
--- 处理学生表历史数据
-UPDATE m_student a, m_school b SET a.school_id = b.id WHERE a.school_no = b.school_no;
 -- m_student表新增视力情况等级相关字段
 ALTER TABLE `m_student`
   ADD COLUMN `myopia_level` tinyint(1) COMMENT '近视等级：0-正常、1-筛查性近视、2-近视前期、3-低度近视、4-中度近视、5-重度近视',
@@ -52,7 +37,7 @@ ALTER TABLE `m_school`
   ADD INDEX `m_school_district_area_code_area_type_monitor_type_index`(`district_area_code`, `area_type`, `monitor_type`) USING BTREE;
 
 -- 更新学生表
-ALTER TABLE .`m_student`
+ALTER TABLE `m_student`
   ADD COLUMN `school_id` int(11) COMMENT '学校ID';
 -- 处理学生表历史数据
 UPDATE m_student a, m_school b SET a.school_id = b.id WHERE a.school_no = b.school_no;
