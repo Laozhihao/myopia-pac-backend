f101434bbe6ebf326738b8b55ca81a75c700e714
Simple4H <crescentcxm@gmail.com>
2021-11-04 17:08:03 +0800

    feat(management): 获取结果统计分析

--- myopia-business/business-api/school-management-api/src/main/java/com/wupol/myopia/business/api/school/management/controller/VisionScreeningController.java
+++ myopia-business/business-api/school-management-api/src/main/java/com/wupol/myopia/business/api/school/management/controller/VisionScreeningController.java
@@ -5,10 +5,9 @@ import com.wupol.myopia.base.handler.ResponseResultBody;
 import com.wupol.myopia.business.api.school.management.service.VisionScreeningService;
 import com.wupol.myopia.business.common.utils.domain.query.PageRequest;
 import com.wupol.myopia.business.core.screening.flow.domain.dto.ScreeningListResponseDTO;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wupol.myopia.business.core.stat.domain.model.SchoolVisionStatistic;
+import com.wupol.myopia.business.core.stat.service.SchoolVisionStatisticService;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -26,6 +25,9 @@ public class VisionScreeningController {
     @Resource
     private VisionScreeningService visionScreeningService;
 
+    @Resource
+    private SchoolVisionStatisticService schoolVisionStatisticService;
+
     /**
      * 获取学校计划
      *
@@ -36,5 +38,16 @@ public class VisionScreeningController {
     @GetMapping("list")
     public IPage<ScreeningListResponseDTO> getList(PageRequest pageRequest, Integer schoolId) {
         return visionScreeningService.getList(pageRequest, schoolId);
+    }
+
+    /**
+     * 获取结果统计分析
+     *
+     * @param schoolStatisticId 结果统计
+     * @return SchoolVisionStatistic
+     */
+    @GetMapping("{schoolStatisticId}")
+    public SchoolVisionStatistic getSchoolStatistic(@PathVariable("schoolStatisticId") Integer schoolStatisticId) {
+        return schoolVisionStatisticService.getById(schoolStatisticId);
     }
 }
