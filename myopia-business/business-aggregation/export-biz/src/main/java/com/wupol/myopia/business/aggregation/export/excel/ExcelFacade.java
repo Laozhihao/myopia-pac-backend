9758f2de6c33967f0e3c0e7b49bd038060827656
Simple4H <crescentcxm@gmail.com>
2021-11-19 15:21:57 +0800

    feat(management): 导入Fix

--- myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/excel/ExcelFacade.java
+++ myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/excel/ExcelFacade.java
@@ -245,10 +245,14 @@ public class ExcelFacade {
     private void preCheckStudent(List<School> schools, List<String> idCards) {
         Assert.isTrue(!CollectionUtils.isEmpty(schools), "学校编号异常");
         Assert.isTrue(idCards.stream().distinct().count() == idCards.size(), "学生身份证号码重复");
+        List<String> duplicateElements = ListUtil.getDuplicateElements(idCards);
+        if (!CollectionUtils.isEmpty(duplicateElements)) {
+            throw new BusinessException("身份证" + StringUtils.join(duplicateElements, ",") + "重复");
+        }
 
         List<String> repeatIdCard = idCards.stream().filter(s -> StringUtils.isNotBlank(s) && !Pattern.matches(RegularUtils.REGULAR_ID_CARD, s)).collect(Collectors.toList());
         if (!CollectionUtils.isEmpty(repeatIdCard)) {
-            throw new BusinessException("身份证" + StringUtils.join(repeatIdCard, ",") + "重复");
+            throw new BusinessException("身份证" + StringUtils.join(repeatIdCard, ",") + "错误");
         }
     }
 
