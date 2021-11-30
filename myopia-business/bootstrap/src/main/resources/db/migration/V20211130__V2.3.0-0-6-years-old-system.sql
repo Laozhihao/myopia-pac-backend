ed65bd1d9ccd2296261b4122a564c73152d47399
lizhou <lizhou.wu@vistel.cn>
2021-11-30 14:08:07 +0800

    0-6岁系统基本分支

--- myopia-business/bootstrap/src/main/resources/db/migration/V20211130__V2.3.0-0-6-years-old-system.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20211130__V2.3.0-0-6-years-old-system.sql
@@ -1,4 +1,1 @@
 -- 0-6岁系统
-ALTER TABLE `h_doctor`
-ADD COLUMN `create_user_id`  int(11) NULL DEFAULT NULL COMMENT '创建人ID' AFTER `user_id`,
-ADD COLUMN `phone`  char(11) NULL COMMENT '手机号码' AFTER `create_user_id`;
