6ced2f23396073dcb6f7a02c12f16ce08359987d
Simple4H <crescentcxm@gmail.com>
2021-12-01 16:31:16 +0800

    feat(management): init

---
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/controller/HospitalWorkbenchPatientController.java
@@ -0,0 +1,24 @@
+package com.wupol.myopia.business.api.management.controller;
+
+import com.wupol.myopia.base.domain.CurrentUser;
+import com.wupol.myopia.base.handler.ResponseResultBody;
+import com.wupol.myopia.base.util.CurrentUserUtil;
+import com.wupol.myopia.business.core.hospital.domain.query.HospitalStudentQuery;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 工作台-患者
+ *
+ * @author Simple4H
+ */
+@ResponseResultBody
+@CrossOrigin
+@RestController
+@RequestMapping("/management/hospital/workbench/patient")
+public class HospitalWorkbenchPatientController {
+
+}
