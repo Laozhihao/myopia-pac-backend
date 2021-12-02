3a9d1b873a76320040e1ce799005ae7d84e20f17
lizhou <lizhou.wu@vistel.cn>
2021-12-02 17:37:42 +0800

    初始化版本号，增加0-6岁系统api模块

---
+++ myopia-business/business-api/preschool-app-api/src/main/java/com/wupol/myopia/business/api/preschool/app/controller/PreschoolInfoController.java
@@ -0,0 +1,22 @@
+package com.wupol.myopia.business.api.preschool.app.controller;
+
+import com.wupol.myopia.base.handler.ResponseResultBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 0-6岁APP接口
+ *
+ * @Author wulizhou
+ * @Date 2021/12/2 17:35
+ */
+@ResponseResultBody
+@CrossOrigin
+@RestController
+@RequestMapping("/preschool/app")
+public class PreschoolInfoController {
+
+
+
+}
