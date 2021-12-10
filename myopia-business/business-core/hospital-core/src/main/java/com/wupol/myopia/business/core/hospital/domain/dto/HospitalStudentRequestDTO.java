131e3d544ca0cd64489e1402aceaee7ecb358a50
Simple4H <crescentcxm@gmail.com>
2021-12-10 15:31:58 +0800

    feat(hospital): 医院列表

--- myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/domain/dto/HospitalStudentRequestDTO.java
+++ myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/domain/dto/HospitalStudentRequestDTO.java
@@ -3,8 +3,6 @@ package com.wupol.myopia.business.core.hospital.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * 医院-获取学生request
  *
@@ -17,7 +15,6 @@ public class HospitalStudentRequestDTO {
     /**
      * 医院Id
      */
-    @NotNull(message = "医院Id不能为空")
     private Integer hospitalId;
 
     /**
