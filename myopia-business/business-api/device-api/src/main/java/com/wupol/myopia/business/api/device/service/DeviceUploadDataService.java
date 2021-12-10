b48ce0239dea621be9d7188659083598a39704ef
Simple4H <crescentcxm@gmail.com>
2021-12-10 16:39:27 +0800

    feat(all): remove log info

--- myopia-business/business-api/device-api/src/main/java/com/wupol/myopia/business/api/device/service/DeviceUploadDataService.java
+++ myopia-business/business-api/device-api/src/main/java/com/wupol/myopia/business/api/device/service/DeviceUploadDataService.java
@@ -159,7 +159,7 @@ public class DeviceUploadDataService {
         Device device = deviceService.getDeviceByDeviceSn(deviceUploadDto.getImei());
         //如果不存在报错
         if (device == null) {
-            log.warn("无法找到设备,imei={}", deviceUploadDto.getImei());
+            log.debug("无法找到设备,imei={}", deviceUploadDto.getImei());
             return;
         }
         Integer bindingScreeningOrgId = device.getBindingScreeningOrgId();
