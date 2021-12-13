003cc71595ac2c7bd1ca90173e8102c8d3020481
Simple4H <crescentcxm@gmail.com>
2021-12-13 16:09:11 +0800

    feat(hospital): 视力低下

--- myopia-business/bootstrap/src/main/resources/db/migration/V20211209__V2.3.0-0-6-years-old-system.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20211209__V2.3.0-0-6-years-old-system.sql
@@ -26,3 +26,28 @@ ADD COLUMN `cooperation_type` tinyint(4) NULL DEFAULT NULL COMMENT '合作类型
 ADD COLUMN `cooperation_time_type` tinyint(4) NULL DEFAULT NULL COMMENT '合作期限类型 -1-自定义 0-30天 1-60天 2-180天 3-1年 4-2年 5-3年' AFTER `cooperation_type`,
 ADD COLUMN `cooperation_start_time` timestamp(3) NULL DEFAULT NULL COMMENT '合作开始时间' AFTER `cooperation_time_type`,
 ADD COLUMN `cooperation_end_time` timestamp(3) NULL DEFAULT NULL COMMENT '合作结束时间' AFTER `cooperation_start_time`;
+
+-- 0-6岁系统
+alter table m_student
+    add is_newborn_without_id_card tinyint(1) default false null comment '是否新生儿暂无身份证 false-否 true-是';
+
+alter table m_student
+    add family_info json null comment '家庭信息';
+
+alter table m_student
+    add committee_code bigint null comment '委会行政区域code';
+
+alter table m_student
+    add record_no bigint null comment '检查建档编码';
+
+alter table h_hospital_student
+    add is_newborn_without_id_card tinyint(1) default false null comment '是否新生儿暂无身份证 false-否 true-是';
+
+alter table h_hospital_student
+    add family_info json null comment '家庭信息';
+
+alter table h_hospital_student
+    add committee_code bigint null comment '委会行政区域code';
+
+alter table h_hospital_student
+    add record_no bigint null comment '检查建档编码';
