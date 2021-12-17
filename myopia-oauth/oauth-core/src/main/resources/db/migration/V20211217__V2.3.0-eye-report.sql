ccbffab4af3cc8f40509b6639ddb6f230e9f534c
Simple4H <crescentcxm@gmail.com>
2021-12-17 15:30:42 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20211217__V2.3.0-eye-report.sql
@@ -0,0 +1,4 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (553, '获取眼健康列表', 'getEyeHealthyList', 'get:/management/hospital/workbench/report/list', 0, 0, 1, 504, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (554, '眼健康就诊记录', 'patientReportList', 'get:/management/hospital/workbench/patient/report/list', 0, 0, 0, 505, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (555, '获取眼健康列表', 'tGetEyeHealthyList', 'get:/management/hospital/workbench/report/list', 0, 0, 1, 522, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (556, '眼健康就诊记录', 'tGatientReportList', 'get:/management/hospital/workbench/patient/report/list', 0, 0, 0, 523, 1);
