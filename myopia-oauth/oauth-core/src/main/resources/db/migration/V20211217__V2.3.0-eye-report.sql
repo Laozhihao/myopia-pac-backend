13a3f690aa9fe7804ee7128b451fd63526108421
Simple4H <crescentcxm@gmail.com>
2021-12-20 16:08:13 +0800

    feat(oAuth): add sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20211217__V2.3.0-eye-report.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20211217__V2.3.0-eye-report.sql
@@ -2,3 +2,5 @@ INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `o
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (554, '眼健康就诊记录', 'patientReportList', 'get:/management/hospital/workbench/patient/report/list', 0, 0, 0, 505, 1);
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (555, '获取眼健康列表', 'tGetEyeHealthyList', 'get:/management/hospital/workbench/report/list', 0, 0, 1, 522, 1);
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (556, '眼健康就诊记录', 'tGatientReportList', 'get:/management/hospital/workbench/patient/report/list', 0, 0, 0, 523, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (557, '就诊列表', 'reportList', 'get:/management/student/report/list', 0, 0, 0, 505, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (558, '学生信息', 'studentInfo', 'get:/management/student/**', 0, 0, 0, 505, 1);
