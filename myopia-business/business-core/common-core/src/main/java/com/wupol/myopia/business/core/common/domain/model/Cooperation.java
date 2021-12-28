f6a24510cbf22f30f38b77ff993e20cd08e70f5d
lizhou <lizhou.wu@vistel.cn>
2021-12-28 18:08:51 +0800

    增加合作类型

---
+++ myopia-business/business-core/common-core/src/main/java/com/wupol/myopia/business/core/common/domain/model/Cooperation.java
@@ -0,0 +1,14 @@
+package com.wupol.myopia.business.core.common.domain.model;
+
+import lombok.Data;
+
+/**
+ * @Author wulizhou
+ * @Date 2021/12/28 17:47
+ */
+@Data
+public class Cooperation {
+
+
+
+}
