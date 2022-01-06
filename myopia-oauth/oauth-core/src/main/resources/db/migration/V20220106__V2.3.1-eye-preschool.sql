d7adc1e4075abf5e9aed5ced2166edcc26c3a0e9
Simple4H <crescentcxm@gmail.com>
2022-01-06 17:26:50 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220106__V2.3.1-eye-preschool.sql
@@ -0,0 +1,3 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`,
+                                                          pid, system_code)
+VALUES (564, '0-6岁眼保健检查（按钮）', 'bChildEyeHealthy', null, 0, 0, 0, 12, 1);
