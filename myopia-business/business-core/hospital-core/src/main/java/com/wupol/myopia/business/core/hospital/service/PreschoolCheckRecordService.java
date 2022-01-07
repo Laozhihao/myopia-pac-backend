0a8858a7d3a47e6e6bcd3b94e0d77b054ab4192d
lizhou <lizhou.wu@vistel.cn>
2022-01-07 20:54:44 +0800

    增加0-6APP接口

--- myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/service/PreschoolCheckRecordService.java
+++ myopia-business/business-core/hospital-core/src/main/java/com/wupol/myopia/business/core/hospital/service/PreschoolCheckRecordService.java
@@ -2,13 +2,24 @@ package com.wupol.myopia.business.core.hospital.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.wupol.myopia.base.service.BaseService;
+import com.wupol.myopia.base.util.BusinessUtil;
 import com.wupol.myopia.base.util.DateUtil;
 import com.wupol.myopia.business.common.utils.domain.query.PageRequest;
+import com.wupol.myopia.base.constant.MonthAgeEnum;
+import com.wupol.myopia.business.core.hospital.constant.MonthAgeStatusEnum;
+import com.wupol.myopia.business.core.hospital.domain.dto.MonthAgeStatusDTO;
 import com.wupol.myopia.business.core.hospital.domain.dto.PreschoolCheckRecordDTO;
 import com.wupol.myopia.business.core.hospital.domain.mapper.PreschoolCheckRecordMapper;
 import com.wupol.myopia.business.core.hospital.domain.model.PreschoolCheckRecord;
 import com.wupol.myopia.business.core.hospital.domain.query.PreschoolCheckRecordQuery;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -45,6 +56,57 @@ public class PreschoolCheckRecordService extends BaseService<PreschoolCheckRecor
             record.setDoctorsName("");
         });
         return records;
+    }
+
+    /**
+     * 获取学生13次检查的检查状态
+     * @param records
+     * @return
+     */
+    private List<MonthAgeStatusDTO> getStudentCheckStatus(Date birthday, List<PreschoolCheckRecordDTO> records) {
+        Date now = new Date();
+        Map<Integer, Integer> monthAgeStatusDTOS = initMonthAgeStatusMap();
+        List<Integer> canCheckMonthAge = BusinessUtil.getCanCheckMonthAgeByDate(birthday);
+        // 设置当前可检查年龄段为AGE_STAGE_STATUS_NOT_DATA状态
+        canCheckMonthAge.forEach(monthAge -> {
+            monthAgeStatusDTOS.put(monthAge, MonthAgeStatusEnum.AGE_STAGE_STATUS_NOT_DATA.getStatus());
+        });
+        records.forEach(record -> {
+            // 检查大于3天，无法修改
+            if (DateUtil.betweenDay(record.getCreateTime(), now) > 3) {
+                monthAgeStatusDTOS.put(record.getMonthAge(), MonthAgeStatusEnum.AGE_STAGE_STATUS_CANNOT_UPDATE.getStatus());
+            } else {
+                // 已检查，可修改
+                monthAgeStatusDTOS.put(record.getMonthAge(), MonthAgeStatusEnum.AGE_STAGE_STATUS_CAN_UPDATE.getStatus());
+            }
+        });
+        // 设置最小可检查年龄段为AGE_STAGE_STATUS_CURRENT
+        List<Integer> collect = records.stream().map(record -> record.getMonthAge()).collect(Collectors.toList());
+        List<Integer> unCheckMonthAgeByCanCheck = canCheckMonthAge.stream().filter(x -> !collect.contains(x)).collect(Collectors.toList());
+        // 当前需检查的都已检查，选中最后的检查项
+        if (CollectionUtils.isEmpty(unCheckMonthAgeByCanCheck)) {
+            monthAgeStatusDTOS.put(canCheckMonthAge.stream().mapToInt(x -> x).max().getAsInt(), MonthAgeStatusEnum.AGE_STAGE_STATUS_CURRENT.getStatus());
+        } else {
+            // 若存在应检未检的，选中该年龄段
+            monthAgeStatusDTOS.put(canCheckMonthAge.stream().mapToInt(x -> x).min().getAsInt(), MonthAgeStatusEnum.AGE_STAGE_STATUS_CURRENT.getStatus());
+        }
+        return createMonthAgeStatusDTOByMap(monthAgeStatusDTOS);
+    }
+
+    /**
+     * 初始化所有月龄为不可点击
+     * @return
+     */
+    private Map<Integer, Integer> initMonthAgeStatusMap() {
+        Map monthAgeStatus = new LinkedHashMap();
+        for(MonthAgeEnum monAge : MonthAgeEnum.values()) {
+            monthAgeStatus.put(monAge.getId(), MonthAgeStatusEnum.AGE_STAGE_STATUS_DISABLE.getStatus());
+        }
+        return monthAgeStatus;
+    }
+
+    private List<MonthAgeStatusDTO> createMonthAgeStatusDTOByMap(Map<Integer, Integer> map) {
+        return map.keySet().stream().map(monthAge -> new MonthAgeStatusDTO(monthAge, map.get(monthAge))).collect(Collectors.toList());
     }
 
 }
--- service-common/src/main/java/com/wupol/myopia/base/util/BusinessUtil.java
+++ service-common/src/main/java/com/wupol/myopia/base/util/BusinessUtil.java
@@ -1,6 +1,12 @@
 package com.wupol.myopia.base.util;
 
+import com.wupol.myopia.base.constant.MonthAgeEnum;
+
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -27,6 +33,82 @@ public class BusinessUtil {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 获取用户可做的月龄检查
+     * @param birthday
+     * @return
+     */
+    public static List<Integer> getCanCheckMonthAgeByDate(Date birthday) {
+        Date now = new Date();
+        Period period = Period.between(birthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                now.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        int years = period.getYears();
+        int months = period.getMonths();
+        int days = period.getDays();
+
+        // 6岁<*【6岁】
+        if (years >= 6 && (months > 0 || days > 0)) {
+            return Arrays.asList(MonthAgeEnum.YEAR6.getId());
+        } else if (years == 5 && (months > 0 || days > 0)) {
+            // 5岁＜*≤6岁：【5岁】【6岁】
+            return Arrays.asList(MonthAgeEnum.YEAR5.getId(), MonthAgeEnum.YEAR6.getId());
+        } else if (years == 4 && (months > 0 || days > 0)) {
+            // 4岁＜*≤5岁：【4岁】【5岁】
+            return Arrays.asList(MonthAgeEnum.YEAR4.getId(), MonthAgeEnum.YEAR5.getId());
+        } else if (years == 3 && (months > 0 || days > 15)) {
+            // 36月龄15天＜*≤4岁：【36月龄】【4岁】
+            return Arrays.asList(MonthAgeEnum.MONTH36.getId(), MonthAgeEnum.YEAR4.getId());
+        } else if ((years * 12 + months > 35) || (years * 12 + months == 35 && days > 15)) {
+            // 35月龄15天＜*≤36月龄15天：【36月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH36.getId());
+        } else if (years  == 2 && (months > 0 || days > 15)) {
+            // 24月龄15天＜*≤35月龄15天：【24月龄】、【36月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH24.getId(), MonthAgeEnum.MONTH36.getId());
+        } else if ((years * 12 + months > 23) || (years * 12 + months == 23 && days > 15)) {
+            // 23月龄15天＜*≤24月龄15天：【24月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH24.getId());
+        } else if ((years * 12 + months > 18) || (years * 12 + months == 18 && days > 15)) {
+            // 18月龄15天＜*≤23月龄15天：【18月龄】、【24月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH18.getId(), MonthAgeEnum.MONTH24.getId());
+        } else if ((years * 12 + months > 17) || (years * 12 + months == 17 && days > 15)) {
+            // 17月龄15天＜*≤18月龄15天：【18月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH18.getId());
+        } else if ((years * 12 + months > 12) || (years * 12 + months == 12 && days > 15)) {
+            // 12月龄15天＜*≤17月龄15天：【12月龄】、【18月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH12.getId(), MonthAgeEnum.MONTH18.getId());
+        } else if ((years * 12 + months > 11) || (years * 12 + months == 11 && days > 15)) {
+            // 11月龄15天＜*≤12月龄15天：【12月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH12.getId());
+        } else if ((months > 8) || (months == 8 && days > 15)) {
+            // 8月龄15天＜*≤11月龄15天：【8月龄】、【12月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH8.getId(), MonthAgeEnum.MONTH12.getId());
+        } else if ((months > 7) || (months == 7 && days > 15)) {
+            // 7月龄15天＜*≤8月龄15天：【8月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH8.getId());
+        } else if ((months > 6) || (months == 6 && days > 15)) {
+            // 6月龄15天＜*≤7月龄15天：【6月龄】、【8月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH6.getId(), MonthAgeEnum.MONTH8.getId());
+        } else if ((months > 5) || (months == 5 && days > 15)) {
+            // 5月龄15天＜*≤6月龄15天：【6月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH6.getId());
+        } else if ((months > 3) || (months == 3 && days > 15)) {
+            // 3月龄15天＜*≤5月龄15天：【3月龄】、【6月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH3.getId(), MonthAgeEnum.MONTH6.getId());
+        } else if ((months > 2) || (months == 2 && days > 15)) {
+            // 2月龄15天＜*≤3月龄15天：【3月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH3.getId());
+        } else if (DateUtil.betweenDay(birthday, now) > 45) {
+            // 45天＜*≤2月龄15天：【满月】、【3月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH1.getId(), MonthAgeEnum.MONTH3.getId());
+        } else if (days > 15) {
+            // 45天＜*≤2月龄15天：【满月】、【3月龄】
+            return Arrays.asList(MonthAgeEnum.MONTH1.getId());
+        } else {
+            // 0＜*≤15天：【新生儿】
+            return Arrays.asList(MonthAgeEnum.NB.getId());
+        }
     }
 
 }
