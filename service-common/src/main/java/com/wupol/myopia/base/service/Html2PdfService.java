7c8fd197c98213998ae7aff24fb19bbba9b7217b
Simple4H <crescentcxm@gmail.com>
2022-01-10 11:57:10 +0800

    feat(management): bug fix

--- service-common/src/main/java/com/wupol/myopia/base/service/Html2PdfService.java
+++ service-common/src/main/java/com/wupol/myopia/base/service/Html2PdfService.java
@@ -1,7 +1,6 @@
 package com.wupol.myopia.base.service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.vistel.Interface.config.AWSConfig;
 import com.wupol.myopia.base.domain.PdfRequestDTO;
 import com.wupol.myopia.base.domain.PdfResponseDTO;
 import com.wupol.myopia.base.util.DateFormatUtil;
@@ -14,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.Date;
 
 /**
