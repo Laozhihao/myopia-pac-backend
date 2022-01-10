e906cc3355622b01797a5005531a3cd96d173454
Simple4H <crescentcxm@gmail.com>
2022-01-10 20:00:42 +0800

    feat(hospital): 居委会

--- myopia-business/business-api/hospital-app-api/src/main/java/com/wupol/myopia/business/api/hospital/app/facade/HospitalStudentFacade.java
+++ myopia-business/business-api/hospital-app-api/src/main/java/com/wupol/myopia/business/api/hospital/app/facade/HospitalStudentFacade.java
@@ -140,7 +140,7 @@ public class HospitalStudentFacade {
             studentVo.setTownId(studentVo.getTown().getId());
         }
         if (Objects.nonNull(studentVo.getCommittee())) {
-            studentVo.setCommitteeCode(studentVo.getCommittee().getCode());
+            studentVo.setCommitteeCode(districtService.getById(studentVo.getCommittee().getId()).getCode());
         }
 
         // 如果管理端没有该学生信息, 则先到管理端创建,再到医院端创建
@@ -163,6 +163,9 @@ public class HospitalStudentFacade {
             }
             if (Objects.nonNull(studentVo.getTownId())) {
                 tmpStudent.setTownCode(districtService.getById(studentVo.getTownId()).getCode());
+            }
+            if (Objects.nonNull(studentVo.getCommitteeCode())) {
+                tmpStudent.setCommitteeCode(studentVo.getCommitteeCode());
             }
             Integer studentId = studentService.saveStudent(tmpStudent);
             studentVo.setStudentId(studentId);
