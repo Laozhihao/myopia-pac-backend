b22a066d7ce7fbca780697855d55c3eb1e7b2ce7
Simple4H <crescentcxm@gmail.com>
2022-01-12 16:50:40 +0800

    feat(oauth): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20211230__V2.3.1-optimize.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20211230__V2.3.1-optimize.sql
@@ -1,14 +1,11 @@
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
-                          update_time)
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (564, '更新结果通知配置', 'updateResultNoticeConfig',
-        'put:/management/screeningOrganization/update/resultNoticeConfig/**', 0, 0, 1, 27, 1, '2021-12-29 17:39:35',
-        '2021-12-29 17:39:35');
+        'put:/management/screeningOrganization/update/resultNoticeConfig/**', 0, 0, 1, 27, 1);
 
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid,
-                          system_code)
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (568, '异步导出学生报告', 'asyncGeneratorPDF', 'get:/management/screeningPlan/screeningNoticeResult/asyncGeneratorPDF',
         0, 0, 0, 27, 1);
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid,
-                          system_code)
+
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (569, '同步导出学生报告', 'syncGeneratorPDF', 'get:/management/screeningPlan/screeningNoticeResult/syncGeneratorPDF', 0,
         0, 0, 27, 1);
