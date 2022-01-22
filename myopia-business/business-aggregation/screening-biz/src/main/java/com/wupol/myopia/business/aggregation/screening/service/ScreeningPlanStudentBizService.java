15869348a70e2ab80469b96c811a491de0ef358a
Simple4H <crescentcxm@gmail.com>
2022-01-22 18:36:46 +0800

    feat(management): 压缩测试
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试
    
    feat(management): 压缩测试

--- myopia-business/business-aggregation/screening-biz/src/main/java/com/wupol/myopia/business/aggregation/screening/service/ScreeningPlanStudentBizService.java
+++ myopia-business/business-aggregation/screening-biz/src/main/java/com/wupol/myopia/business/aggregation/screening/service/ScreeningPlanStudentBizService.java
@@ -1,5 +1,8 @@
 package com.wupol.myopia.business.aggregation.screening.service;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ZipUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.wupol.myopia.base.cache.RedisUtil;
 import com.wupol.myopia.base.domain.PdfResponseDTO;
 import com.wupol.myopia.base.domain.vo.PdfGeneratorVO;
@@ -9,29 +12,42 @@ import com.wupol.myopia.business.aggregation.screening.domain.dto.UpdatePlanStud
 import com.wupol.myopia.business.common.utils.domain.model.ResultNoticeConfig;
 import com.wupol.myopia.business.core.common.service.Html2PdfService;
 import com.wupol.myopia.business.core.common.service.ResourceFileService;
+import com.wupol.myopia.business.core.common.util.S3Utils;
 import com.wupol.myopia.business.core.school.domain.model.School;
+import com.wupol.myopia.business.core.school.domain.model.SchoolClass;
 import com.wupol.myopia.business.core.school.domain.model.SchoolGrade;
 import com.wupol.myopia.business.core.school.domain.model.Student;
 import com.wupol.myopia.business.core.school.management.domain.model.SchoolStudent;
 import com.wupol.myopia.business.core.school.management.service.SchoolStudentService;
+import com.wupol.myopia.business.core.school.service.SchoolClassService;
 import com.wupol.myopia.business.core.school.service.SchoolGradeService;
 import com.wupol.myopia.business.core.school.service.SchoolService;
 import com.wupol.myopia.business.core.school.service.StudentService;
+import com.wupol.myopia.business.core.screening.flow.domain.dos.StudentDO;
 import com.wupol.myopia.business.core.screening.flow.domain.dto.ScreeningStudentDTO;
+import com.wupol.myopia.business.core.screening.flow.domain.model.ScreeningPlan;
 import com.wupol.myopia.business.core.screening.flow.domain.model.ScreeningPlanSchoolStudent;
 import com.wupol.myopia.business.core.screening.flow.domain.model.VisionScreeningResult;
 import com.wupol.myopia.business.core.screening.flow.service.ScreeningPlanSchoolStudentService;
+import com.wupol.myopia.business.core.screening.flow.service.ScreeningPlanService;
 import com.wupol.myopia.business.core.screening.flow.service.VisionScreeningResultService;
 import com.wupol.myopia.business.core.screening.organization.service.ScreeningOrganizationService;
+import com.wupol.myopia.business.core.system.service.NoticeService;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
+
 import javax.annotation.Resource;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -66,6 +82,16 @@ public class ScreeningPlanStudentBizService {
     private ResourceFileService resourceFileService;
     @Resource
     private VisionScreeningResultService visionScreeningResultService;
+    @Value("${report.pdf.save-path}")
+    public String pdfSavePath;
+    @Resource
+    private NoticeService noticeService;
+    @Resource
+    private S3Utils s3Utils;
+    @Resource
+    private ScreeningPlanService screeningPlanService;
+    @Resource
+    private SchoolClassService schoolClassService;
 
     /**
      * 筛查通知结果页面地址
@@ -178,27 +204,85 @@ public class ScreeningPlanStudentBizService {
      * @param planStudentIdStr 筛查学生Ids
      * @param userId           用户Id
      */
+    @Async
     public void asyncGeneratorPDF(Integer planId, Integer schoolId, Integer gradeId, Integer classId,
                                   Integer orgId, String planStudentIdStr, Boolean isSchoolClient, Integer userId) {
-        String uuid = UUID.randomUUID().toString();
-        String fileName = getFileName(schoolId, gradeId);
-        cacheInfo(uuid, userId, fileName);
-        String screeningNoticeResultHtmlUrl = String.format(SCREENING_NOTICE_RESULT_HTML_URL,
-                htmlUrlHost,
-                planId,
-                Objects.nonNull(schoolId) ? schoolId : StringUtils.EMPTY,
-                Objects.nonNull(gradeId) ? gradeId : StringUtils.EMPTY,
-                Objects.nonNull(classId) ? classId : StringUtils.EMPTY,
-                Objects.nonNull(orgId) ? orgId : StringUtils.EMPTY,
-                Objects.nonNull(planStudentIdStr) ? planStudentIdStr : StringUtils.EMPTY,
-                isSchoolClient);
-        log.info("导出URL:{}", screeningNoticeResultHtmlUrl);
-        PdfResponseDTO responseDTO = html2PdfService.asyncGeneratorPDF(screeningNoticeResultHtmlUrl, fileName, uuid);
-        if (responseDTO.getStatus().equals(false)) {
-            // 错误删除时候删除缓存信息
-            redisUtil.del(uuid);
-            throw new BusinessException("异步导出学生报告异常");
+
+        List<ScreeningStudentDTO> screeningStudentDTOS = getScreeningNoticeResultStudent(planId, schoolId, gradeId, classId, orgId, planStudentIdStr, isSchoolClient, null);
+        if (CollectionUtils.isEmpty(screeningStudentDTOS)) {
+            return;
         }
+        String fileSaveParentPath = getFileSaveParentPath() + UUID.randomUUID() + "/";
+
+        ScreeningPlan plan = screeningPlanService.getById(planId);
+
+        List<Integer> schoolIds = screeningStudentDTOS.stream().map(ScreeningStudentDTO::getSchoolId).collect(Collectors.toList());
+        Map<Integer, String> schoolMap = schoolService.getByIds(schoolIds).stream().collect(Collectors.toMap(School::getId, School::getName));
+
+        List<Integer> gradeIds = screeningStudentDTOS.stream().map(ScreeningStudentDTO::getGradeId).collect(Collectors.toList());
+        Map<Integer, SchoolGrade> gradeMap = schoolGradeService.getGradeMapByIds(gradeIds);
+
+        List<Integer> classIds = screeningStudentDTOS.stream().map(ScreeningStudentDTO::getClassId).collect(Collectors.toList());
+        Map<Integer, SchoolClass> classMap = schoolClassService.getClassMapByIds(classIds);
+
+        Map<Integer, List<ScreeningStudentDTO>> planGroup = screeningStudentDTOS.stream().collect(Collectors.groupingBy(ScreeningStudentDTO::getPlanId));
+
+        for (Map.Entry<Integer, List<ScreeningStudentDTO>> planEntry : planGroup.entrySet()) {
+            List<ScreeningStudentDTO> planList = planEntry.getValue();
+            if (CollectionUtils.isEmpty(planList)) {
+                continue;
+            }
+            Map<Integer, List<ScreeningStudentDTO>> schoolGroup = planList.stream().collect(Collectors.groupingBy(ScreeningStudentDTO::getSchoolId));
+            for (Map.Entry<Integer, List<ScreeningStudentDTO>> schoolEntry : schoolGroup.entrySet()) {
+                List<ScreeningStudentDTO> schoolList = schoolEntry.getValue();
+                if (CollectionUtils.isEmpty(schoolList)) {
+                    continue;
+                }
+                Map<Integer, List<ScreeningStudentDTO>> gradeGroup = schoolList.stream().collect(Collectors.groupingBy(StudentDO::getGradeId));
+                for (Map.Entry<Integer, List<ScreeningStudentDTO>> gradeEntry : gradeGroup.entrySet()) {
+                    List<ScreeningStudentDTO> gradeList = gradeEntry.getValue();
+                    if (CollectionUtils.isEmpty(gradeList)) {
+                        continue;
+                    }
+                    Map<Integer, List<ScreeningStudentDTO>> classGroup = gradeList.stream().collect(Collectors.groupingBy(StudentDO::getClassId));
+                    if (CollectionUtils.isEmpty(classGroup)) {
+                        continue;
+                    }
+                    for (Map.Entry<Integer, List<ScreeningStudentDTO>> classEntry : classGroup.entrySet()) {
+                        List<ScreeningStudentDTO> classList = classEntry.getValue();
+                        if (CollectionUtils.isEmpty(classList)) {
+                            continue;
+                        }
+                        String screeningNoticeResultHtmlUrl = String.format(SCREENING_NOTICE_RESULT_HTML_URL,
+                                htmlUrlHost,
+                                planEntry.getKey(),
+                                Objects.nonNull(schoolEntry.getKey()) ? schoolEntry.getKey() : StringUtils.EMPTY,
+                                Objects.nonNull(gradeEntry.getKey()) ? gradeEntry.getKey() : StringUtils.EMPTY,
+                                Objects.nonNull(classEntry.getKey()) ? classEntry.getKey() : StringUtils.EMPTY,
+                                Objects.nonNull(orgId) ? orgId : StringUtils.EMPTY,
+                                Objects.nonNull(planStudentIdStr) ? planStudentIdStr : StringUtils.EMPTY,
+                                isSchoolClient);
+                        String uuid = UUID.randomUUID().toString();
+                        String fileName = "档案卡";
+                        PdfResponseDTO pdfResponseDTO = html2PdfService.syncGeneratorPDF(screeningNoticeResultHtmlUrl, fileName, uuid);
+                        log.info("response:{}", JSONObject.toJSONString(pdfResponseDTO));
+                        try {
+                            downloadFile(pdfResponseDTO.getUrl(),
+                                    fileSaveParentPath + plan.getTitle() + "/" +
+                                            schoolMap.get(schoolEntry.getKey()) + "/" +
+                                            gradeMap.get(gradeEntry.getKey()).getName() + "/" +
+                                            classMap.get(classEntry.getKey()).getName() + "/" +
+                                            fileName + ".pdf");
+                        } catch (Exception e) {
+                            log.error("Exception",e);
+                        }
+                    }
+                }
+            }
+        }
+        File zip = ZipUtil.zip(fileSaveParentPath);
+        log.info("zip file name:{}, path:{}",zip.getName(),zip.getPath());
+        noticeService.sendExportSuccessNotice(userId, userId, "fileName", s3Utils.uploadFileToS3(zip));
     }
 
     /**
@@ -284,5 +368,42 @@ public class ScreeningPlanStudentBizService {
     public List<ScreeningStudentDTO> getScreeningStudentDTOS(Integer planId, Integer schoolId, Integer gradeId, Integer classId, String planStudentIdStr, String planStudentName) {
         List<Integer> planStudentId = ListUtil.str2List(planStudentIdStr);
         return screeningPlanSchoolStudentService.getScreeningNoticeResultStudent(planId, schoolId, gradeId, classId, CollectionUtils.isEmpty(planStudentId) ? null : planStudentId, planStudentName);
+    }
+
+    /**
+     * 获取文件保存父目录路径
+     *
+     * @return java.lang.String
+     **/
+    public String getFileSaveParentPath() {
+        return Paths.get(pdfSavePath, UUID.randomUUID().toString()).toString();
+    }
+
+    /**
+     * 文件下载
+     * @param fileUrl 下载路径
+     * @param savePath 存放地址
+     */
+    public static void downloadFile(String fileUrl,String savePath) throws Exception {
+
+        File file = new File(savePath);
+        File parentFile = file.getParentFile();
+        if(!parentFile.exists()){
+            parentFile.mkdirs();
+        }
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        URL url = new URL(fileUrl);
+        URLConnection conn = url.openConnection();
+        InputStream inputStream = conn.getInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream(savePath);
+        int byteRead;
+        byte[] buffer = new byte[1024];
+        while ((byteRead = inputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, byteRead);
+        }
+        fileOutputStream.close();
     }
 }
