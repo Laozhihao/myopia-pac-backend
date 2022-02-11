98cfe7c1218d40fbb1ff19ed17650762a2f3f202
Simple4H <crescentcxm@gmail.com>
2022-02-11 18:30:51 +0800

    feat(management): fix

--- service-common/src/main/java/com/wupol/myopia/base/util/BusinessUtil.java
+++ service-common/src/main/java/com/wupol/myopia/base/util/BusinessUtil.java
@@ -6,10 +6,7 @@ import com.wupol.myopia.base.constant.MonthAgeEnum;
 
 import java.time.Period;
 import java.time.ZoneId;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @Author wulizhou
@@ -32,10 +29,7 @@ public class BusinessUtil {
                 || Objects.isNull(cooperationEndTime)) {
             return false;
         }
-        if (cooperationStartTime.getTime() > cooperationEndTime.getTime()) {
-            return false;
-        }
-        return true;
+        return cooperationStartTime.getTime() <= cooperationEndTime.getTime();
     }
 
     /**
@@ -54,7 +48,7 @@ public class BusinessUtil {
 
         // 6岁<*【6岁】
         if (years > 6 || (years == 6 && (months > 0 || days > 0))) {
-            return Arrays.asList(MonthAgeEnum.YEAR6.getId());
+            return Collections.singletonList(MonthAgeEnum.YEAR6.getId());
         } else if (years > 5 || (years == 5 && (months > 0 || days > 0))) {
             // 5岁＜*≤6岁：【5岁】【6岁】
             return Arrays.asList(MonthAgeEnum.YEAR5.getId(), MonthAgeEnum.YEAR6.getId());
@@ -66,52 +60,52 @@ public class BusinessUtil {
             return Arrays.asList(MonthAgeEnum.MONTH36.getId(), MonthAgeEnum.YEAR4.getId());
         } else if ((years * 12 + months > 35) || (years * 12 + months == 35 && days > 15)) {
             // 35月龄15天＜*≤36月龄15天：【36月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH36.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH36.getId());
         } else if (years == 2 && (months > 0 || days > 15)) {
             // 24月龄15天＜*≤35月龄15天：【24月龄】、【36月龄】
             return Arrays.asList(MonthAgeEnum.MONTH24.getId(), MonthAgeEnum.MONTH36.getId());
         } else if ((years * 12 + months > 23) || (years * 12 + months == 23 && days > 15)) {
             // 23月龄15天＜*≤24月龄15天：【24月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH24.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH24.getId());
         } else if ((years * 12 + months > 18) || (years * 12 + months == 18 && days > 15)) {
             // 18月龄15天＜*≤23月龄15天：【18月龄】、【24月龄】
             return Arrays.asList(MonthAgeEnum.MONTH18.getId(), MonthAgeEnum.MONTH24.getId());
         } else if ((years * 12 + months > 17) || (years * 12 + months == 17 && days > 15)) {
             // 17月龄15天＜*≤18月龄15天：【18月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH18.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH18.getId());
         } else if ((years * 12 + months > 12) || (years * 12 + months == 12 && days > 15)) {
             // 12月龄15天＜*≤17月龄15天：【12月龄】、【18月龄】
             return Arrays.asList(MonthAgeEnum.MONTH12.getId(), MonthAgeEnum.MONTH18.getId());
         } else if ((years * 12 + months > 11) || (years * 12 + months == 11 && days > 15)) {
             // 11月龄15天＜*≤12月龄15天：【12月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH12.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH12.getId());
         } else if ((months > 8) || (months == 8 && days > 15)) {
             // 8月龄15天＜*≤11月龄15天：【8月龄】、【12月龄】
             return Arrays.asList(MonthAgeEnum.MONTH8.getId(), MonthAgeEnum.MONTH12.getId());
         } else if ((months > 7) || (months == 7 && days > 15)) {
             // 7月龄15天＜*≤8月龄15天：【8月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH8.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH8.getId());
         } else if ((months > 6) || (months == 6 && days > 15)) {
             // 6月龄15天＜*≤7月龄15天：【6月龄】、【8月龄】
             return Arrays.asList(MonthAgeEnum.MONTH6.getId(), MonthAgeEnum.MONTH8.getId());
         } else if ((months > 5) || (months == 5 && days > 15)) {
             // 5月龄15天＜*≤6月龄15天：【6月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH6.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH6.getId());
         } else if ((months > 3) || (months == 3 && days > 15)) {
             // 3月龄15天＜*≤5月龄15天：【3月龄】、【6月龄】
             return Arrays.asList(MonthAgeEnum.MONTH3.getId(), MonthAgeEnum.MONTH6.getId());
         } else if ((months > 2) || (months == 2 && days > 15)) {
             // 2月龄15天＜*≤3月龄15天：【3月龄】
-            return Arrays.asList(MonthAgeEnum.MONTH3.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH3.getId());
         } else if (DateUtil.betweenDay(birthday, now) > 45) {
             // 45天＜*≤2月龄15天：【满月】、【3月龄】
             return Arrays.asList(MonthAgeEnum.MONTH1.getId(), MonthAgeEnum.MONTH3.getId());
         } else if (DateUtil.betweenDay(birthday, now) > 15) {
             // 15天＜*≤45天：【满月】
-            return Arrays.asList(MonthAgeEnum.MONTH1.getId());
+            return Collections.singletonList(MonthAgeEnum.MONTH1.getId());
         } else {
             // 0＜*≤15天：【新生儿】
-            return Arrays.asList(MonthAgeEnum.NB.getId());
+            return Collections.singletonList(MonthAgeEnum.NB.getId());
         }
     }
 
