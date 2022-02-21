d13378c8ba9c90d9375bc08b16fe445ba83db5f1
liangjinhao <jinhao.liang@vistel.cn>
2022-02-21 15:43:19 +0800

    增加机构二维码配置

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220216__V2.3.2-independent-screening.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220216__V2.3.2-independent-screening.sql
@@ -9,3 +9,6 @@ alter table h_hospital_student
 
 alter table m_screening_plan_school_student
     add passport varchar(32) null comment '护照';
+
+alter table m_screening_organization
+    add qr_code_config varchar(16) default '' not null comment '二维码配置, 英文逗号分隔, 1-普通二维码, 2-vs666, 3-虚拟二维码';
