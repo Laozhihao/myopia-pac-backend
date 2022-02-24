43328ac04768c3c6bb8af12f2a49992793885fd9
lizhou <lizhou.wu@vistel.cn>
2022-02-24 12:13:37 +0800

    修复bug 1176

--- myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/PreschoolCheckRecordBizService.java
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/PreschoolCheckRecordBizService.java
@@ -1,15 +1,20 @@
 package com.wupol.myopia.business.api.management.service;
 
+import com.wupol.myopia.business.aggregation.hospital.domain.vo.HospitalStudentVO;
 import com.wupol.myopia.business.api.management.domain.dto.StudentPreschoolCheckRecordDTO;
+import com.wupol.myopia.business.core.hospital.domain.dto.HospitalStudentResponseDTO;
 import com.wupol.myopia.business.core.hospital.domain.dto.MonthAgeStatusDTO;
 import com.wupol.myopia.business.core.hospital.domain.model.PreschoolCheckRecord;
+import com.wupol.myopia.business.core.hospital.service.HospitalStudentService;
 import com.wupol.myopia.business.core.hospital.service.PreschoolCheckRecordService;
 import com.wupol.myopia.business.core.school.domain.dto.StudentDTO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Author wulizhou
@@ -24,6 +29,9 @@ public class PreschoolCheckRecordBizService {
     @Autowired
     private PreschoolCheckRecordService preschoolCheckRecordService;
 
+    @Autowired
+    private HospitalStudentService hospitalStudentService;
+
     /**
      * 获取学生信息
      * @param hospitalId
@@ -32,11 +40,20 @@ public class PreschoolCheckRecordBizService {
      */
     public StudentPreschoolCheckRecordDTO getInit(Integer hospitalId, Integer studentId) {
         StudentPreschoolCheckRecordDTO record = new StudentPreschoolCheckRecordDTO();
-        StudentDTO student = studentBizService.getStudentById(studentId);
-        record.setStudent(student);
+        HospitalStudentVO studentVO = new HospitalStudentVO();
+        // 未区分医院时，取学生信息
+        if (Objects.isNull(hospitalId)) {
+            StudentDTO student = studentBizService.getStudentById(studentId);
+            BeanUtils.copyProperties(student, studentVO);
+        } else {
+            // 区分医院时，取医院下患者信息
+            HospitalStudentResponseDTO student = hospitalStudentService.getByHospitalIdAndStudentId(hospitalId, studentId);
+            BeanUtils.copyProperties(student, studentVO);
+        }
+        record.setStudent(studentVO);
         // 对应当前医院各年龄段状态
         List<PreschoolCheckRecord> records = preschoolCheckRecordService.getStudentRecord(hospitalId, studentId);
-        Map<Integer, MonthAgeStatusDTO> studentCheckStatus = preschoolCheckRecordService.getStudentCheckStatus(student.getBirthday(), records);
+        Map<Integer, MonthAgeStatusDTO> studentCheckStatus = preschoolCheckRecordService.getStudentCheckStatus(studentVO.getBirthday(), records);
         record.setAgeStageStatusList(preschoolCheckRecordService.createMonthAgeStatusDTOByMap(studentCheckStatus));
         return record;
     }
