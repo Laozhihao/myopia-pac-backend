9b0563309bae3297922e69db1a0360bcdeccee4e
Simple4H <crescentcxm@gmail.com>
2022-02-28 19:09:32 +0800

    feat(management): bug fix

--- myopia-business/business-aggregation/hospital-biz/src/main/java/com/wupol/myopia/business/aggregation/hospital/service/HospitalAggService.java
+++ myopia-business/business-aggregation/hospital-biz/src/main/java/com/wupol/myopia/business/aggregation/hospital/service/HospitalAggService.java
@@ -170,13 +170,13 @@ public class HospitalAggService {
                 && Boolean.TRUE.equals(oldStudent.getIsNewbornWithoutIdCard()))) {
             if ((Objects.nonNull(oldStudent) && isCheckNameAndIDCard)) {
                 if (StringUtils.isNotBlank(oldStudent.getIdCard())
-                        && !(idCard.equals(oldStudent.getIdCard())
-                        && oldStudent.getName().equals(studentVo.getName()))) {
+                        && !(StringUtils.equals(idCard, oldStudent.getIdCard())
+                        && StringUtils.equals(oldStudent.getName(), studentVo.getName()))) {
                     throw new BusinessException("学生的身份证与姓名不匹配");
                 }
                 if (StringUtils.isNotBlank(oldStudent.getPassport())
-                        && !(passport.equals(oldStudent.getPassport())
-                        && oldStudent.getName().equals(studentVo.getName()))) {
+                        && !(StringUtils.equals(passport, oldStudent.getPassport())
+                        && StringUtils.equals(oldStudent.getName(), studentVo.getName()))) {
                     throw new BusinessException("学生的护照与姓名不匹配");
                 }
             }
