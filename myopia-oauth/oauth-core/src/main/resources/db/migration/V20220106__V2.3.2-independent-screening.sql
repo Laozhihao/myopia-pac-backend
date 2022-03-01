778eb6b30119aef1293d3e6f802a6a31ffb6b545
Simple4H <crescentcxm@gmail.com>
2022-03-01 16:22:20 +0800

    feat(OAuth): update SQL

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220106__V2.3.2-independent-screening.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220106__V2.3.2-independent-screening.sql
@@ -1,1 +1,7 @@
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time, update_time) VALUES (682, '删除筛查计划学生', 'deletedPlanStudent', 'post:/management/screeningPlan/deleted/planStudent/**', 0, 0, 1, 27, 1, '2022-02-28 18:37:33', '2022-02-28 18:37:33');
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
+                          update_time)
+VALUES (682, '删除筛查计划学生', 'deletedPlanStudent', 'post:/management/screeningPlan/deleted/planStudent/**', 0, 0, 1, 27, 1,
+        '2022-02-28 18:37:33', '2022-02-28 18:37:33'),
+       (681, '虚拟学生列表', 'fictitiousStudentList', 'get:/management/mockPlanStudent/list', 0, 0, 1, 680, 1,
+        '2022-02-24 14:20:14', '2022-02-24 14:20:14'),
+       (680, '虚拟学生管理', 'fictitiousStudent', null, 1, 1, 7, 1, 1, '2022-02-15 11:57:49', '2022-02-15 11:58:51');
