54df685f734134bdb3ed99cbacaa66533c2e011d
yanjing <664333428@qq.com>
2022-03-04 19:00:50 +0800

    feat(parent):init wordOrderTable

---
+++ myopia-business/business-core/parent-core/src/main/java/com/wupol/myopia/business/core/parent/service/WorkOrderService.java
@@ -0,0 +1,16 @@
+package com.wupol.myopia.business.core.parent.service;
+
+import com.wupol.myopia.base.service.BaseService;
+import com.wupol.myopia.business.core.parent.domain.mapper.WorkOrderMapper;
+import com.wupol.myopia.business.core.parent.domain.model.WorkOrder;
+import org.springframework.stereotype.Service;
+
+/**
+ * 工单业务类
+ * @Author xjl
+ * @Date 2022-03-04
+ */
+@Service
+public class WorkOrderService extends BaseService<WorkOrderMapper, WorkOrder> {
+
+}
