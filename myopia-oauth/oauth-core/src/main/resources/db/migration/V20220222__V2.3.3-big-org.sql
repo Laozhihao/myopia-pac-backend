034172d80f95053c152aa775cff0b17d543ba226
Simple4H <crescentcxm@gmail.com>
2022-03-04 10:34:34 +0800

    feat(oauth): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220222__V2.3.3-big-org.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code, create_time,
-                          update_time)
-VALUES (763, '筛查二维码', 'reportScreeningOrgQrcode', 'get:/management/report/screeningOrg/qrcode', 0, 0, 1, 27, 1,
-        '2022-03-04 10:24:17', '2022-03-04 10:24:17');
