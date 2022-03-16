e5bab1e7997ad83c6ef62e684dd42d21bc3ecd66
yanjing <664333428@qq.com>
2022-03-16 14:44:00 +0800

    feat(workOrder): permission fix

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
@@ -3,9 +3,9 @@ INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `o
                           pid, system_code)
 VALUES
        (810, '工单管理', 'workOrderManagement', NULL, 1, 1, 1, 14, 5),
-       (811, '工单处理', 'workOrderProcessing', NULL, 1, 1, 1, 810, 1),
-       (812, '查看工单', 'workOrderDetail', NULL, 1, 1, 1, 810, 1),
+       (811, '工单处理', 'workOrderProcessing', NULL, 0, 1, 1, 810, 1),
+       (812, '查看工单', 'workOrderDetail', NULL, 0, 1, 1, 810, 1),
        (820, '查看工单列表', 'workOrderList', 'get:/management/workOrder/list', 0, 0, 1, 810, 1),
        (821, '处理工单', 'workOrderDispose', 'get:/management/workOrder/dispose', 0, 0, 1, 811, 1);
-       (821, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1);
-       (821, '工单查询学生筛查记录', 'workOrderStudentList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1);
+       (822, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1);
+       (823, '工单查询学生筛查记录', 'workOrderStudentList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1);
