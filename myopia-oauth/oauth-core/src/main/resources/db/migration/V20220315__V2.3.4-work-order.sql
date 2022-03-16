5cae9209e7b582eab2e31fe40f5814258ac4b3bf
yanjing <664333428@qq.com>
2022-03-16 14:50:58 +0800

    feat(workOrder): permission fix

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
@@ -6,6 +6,6 @@ VALUES
        (811, '工单处理', 'workOrderProcessing', NULL, 0, 1, 1, 810, 1),
        (812, '查看工单', 'workOrderDetail', NULL, 0, 1, 1, 810, 1),
        (820, '查看工单列表', 'workOrderList', 'get:/management/workOrder/list', 0, 0, 1, 810, 1),
-       (821, '处理工单', 'workOrderDispose', 'get:/management/workOrder/dispose', 0, 0, 1, 811, 1);
-       (822, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1);
+       (821, '处理工单', 'workOrderDispose', 'get:/management/workOrder/dispose', 0, 0, 1, 811, 1),
+       (822, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1),
        (823, '工单查询学生筛查记录', 'workOrderStudentList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1);
