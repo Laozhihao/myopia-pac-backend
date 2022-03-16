58c20ec3bb0d2457cb52c8247ff87375b9ce881d
yanjing <664333428@qq.com>
2022-03-16 11:19:56 +0800

    feat(parent): permission update

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
@@ -2,8 +2,8 @@
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`,
                           pid, system_code)
 VALUES
-       (810, '工单管理', 'workOrderManagement', NULL, 1, 1, 1, 14, 1),
-       (811, '工单处理', 'workOrderProcessing', NULL, 0, 1, 1, 810, 1),
-       (812, '查看工单', 'workOrderDetail', NULL, 0, 1, 1, 810, 1),
+       (810, '工单管理', 'workOrderManagement', NULL, 1, 1, 1, 14, 5),
+       (811, '工单处理', 'workOrderProcessing', NULL, 1, 1, 1, 810, 1),
+       (812, '查看工单', 'workOrderDetail', NULL, 1, 1, 1, 810, 1),
        (820, '查看工单列表', 'workOrderList', 'get:/management/workTable/list', 0, 0, 1, 810, 1),
        (821, '处理工单', 'workOrderDispose', 'get:/management/workTable/dispose', 0, 0, 2, 810, 1);
