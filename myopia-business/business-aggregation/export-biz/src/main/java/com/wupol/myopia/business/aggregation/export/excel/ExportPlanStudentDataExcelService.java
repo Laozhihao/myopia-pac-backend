47d7987b45698ccbd9f6ff45b9283a4028bca06d
陶帅 <542564264@qq.com>
2022-03-17 17:09:18 +0800

    导入账号数量上限限制

--- myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/excel/ExportPlanStudentDataExcelService.java
+++ myopia-business/business-aggregation/export-biz/src/main/java/com/wupol/myopia/business/aggregation/export/excel/ExportPlanStudentDataExcelService.java
@@ -161,7 +161,7 @@ public class ExportPlanStudentDataExcelService extends BaseExportExcelFileServic
                 collect.forEach((classKey,classValue) ->{
                     SchoolClass schoolClass = schoolClassService.getById(classKey);
                     try {
-                        ExcelUtil.exportListToExcelWithFolder(folder.toString()+ gradeFolder, String.format(PLAN_STUDENT_FILE_NAME,school.getName()+grade.getName()+schoolClass.getName()), excelFacade.genVisionScreeningResultExportVos(value), mergeStrategy, getHeadClass());
+                        ExcelUtil.exportListToExcelWithFolder(folder.toString()+ gradeFolder, String.format(PLAN_STUDENT_FILE_NAME,school.getName()+grade.getName()+schoolClass.getName()), excelFacade.genVisionScreeningResultExportVos(classValue), mergeStrategy, getHeadClass());
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -189,7 +189,7 @@ public class ExportPlanStudentDataExcelService extends BaseExportExcelFileServic
                 collect.forEach((classKey,classValue) ->{
                     SchoolClass schoolClass = schoolClassService.getById(key);
                     try {
-                        ExcelUtil.exportListToExcelWithFolder(folder.toString()+ gradeFolder, String.format(PLAN_STUDENT_FILE_NAME,school.getName()+grade.getName()+schoolClass.getName()), excelFacade.genVisionScreeningResultExportVos(value), mergeStrategy, getHeadClass());
+                        ExcelUtil.exportListToExcelWithFolder(folder.toString()+ gradeFolder, String.format(PLAN_STUDENT_FILE_NAME,school.getName()+grade.getName()+schoolClass.getName()), excelFacade.genVisionScreeningResultExportVos(classValue), mergeStrategy, getHeadClass());
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
