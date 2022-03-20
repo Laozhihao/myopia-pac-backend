86c2459bfe22aa3a19410d8add4f3c15489ca0f6
钓猫的小鱼 <shuailong.wu@vistel.cn>
2022-03-20 18:07:36 +0800

    feat（school） 学生档案卡路径

--- myopia-business/business-api/school-management-api/src/main/java/com/wupol/myopia/business/api/school/management/controller/SchoolReportController.java
+++ myopia-business/business-api/school-management-api/src/main/java/com/wupol/myopia/business/api/school/management/controller/SchoolReportController.java
@@ -1,28 +1,14 @@
 package com.wupol.myopia.business.api.school.management.controller;
 
-import com.alibaba.csp.sentinel.util.StringUtil;
 import com.wupol.myopia.base.domain.ApiResult;
-import com.wupol.myopia.base.exception.BusinessException;
 import com.wupol.myopia.base.handler.ResponseResultBody;
-import com.wupol.myopia.base.util.CurrentUserUtil;
-import com.wupol.myopia.business.aggregation.export.ExportStrategy;
 import com.wupol.myopia.business.aggregation.export.pdf.archives.SyncExportStudentScreeningArchivesService;
-import com.wupol.myopia.business.aggregation.export.pdf.constant.ExportReportServiceNameConstant;
-import com.wupol.myopia.business.aggregation.export.pdf.domain.ExportCondition;
-import com.wupol.myopia.business.core.screening.flow.service.VisionScreeningResultService;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * @Author 钓猫的小鱼
@@ -35,7 +21,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/school/report")
 @Log4j2
-public class ReportController {
+public class SchoolReportController {
 
     @Autowired
     private SyncExportStudentScreeningArchivesService syncExportStudentScreeningArchivesService;
