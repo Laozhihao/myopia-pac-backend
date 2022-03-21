e00507f9056c55d1610b1ff6c76c4c2013dd4103
钓猫的小鱼 <2292229154@qq.com>
2022-03-21 11:48:53 +0800

    feat(screening) 权限配置

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`,
+                          pid, system_code)
+VALUES
+       (810, '工单管理', 'workOrderManagement', NULL, 1, 1, 1, 14, 5),
+       (811, '工单处理', 'workOrderProcessing', NULL, 0, 1, 1, 810, 1),
+       (812, '查看工单', 'workOrderDetail', NULL, 0, 1, 1, 810, 1),
+       (813, '查看工单列表', 'workOrderList', 'get:/management/workOrder/list', 0, 0, 1, 810, 1),
+       (814, '处理工单', 'workOrderDispose', 'put:/management/workOrder/dispose', 0, 0, 1, 811, 1),
+       (815, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1),
+       (816, '工单查询学生筛查记录', 'workOrderStudentScreeningList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1);
+       (817, '学生档案卡路径', 'managementArchivesUrl', 'get:/management/report/student/archivesUrl', 0, 0, 3, 11, 1);
