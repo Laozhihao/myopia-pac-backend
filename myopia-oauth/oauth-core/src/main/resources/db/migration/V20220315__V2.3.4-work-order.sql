74a40050daabca897ff1826443d94362bc8737a3
钓猫的小鱼 <2292229154@qq.com>
2022-03-22 11:57:58 +0800

    feat (V20220315__V2.3.4-work-order) 修改逗号

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220315__V2.3.4-work-order.sql
@@ -8,6 +8,5 @@ VALUES
        (813, '查看工单列表', 'workOrderList', 'get:/management/workOrder/list', 0, 0, 1, 810, 1),
        (814, '处理工单', 'workOrderDispose', 'put:/management/workOrder/dispose', 0, 0, 1, 811, 1),
        (815, '工单查询学生列表', 'workOrderStudentList', 'get:/management/student/list', 0, 0, 1, 811, 1),
-       (816, '工单查询学生筛查记录', 'workOrderStudentScreeningList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1);
-
+       (816, '工单查询学生筛查记录', 'workOrderStudentScreeningList', 'get:/management/student/screening/**', 0, 0, 1, 811, 1),
        (817, '学生档案卡路径', 'managementArchivesUrl', 'get:/management/report/student/archivesUrl', 0, 0, 3, 11, 1);
