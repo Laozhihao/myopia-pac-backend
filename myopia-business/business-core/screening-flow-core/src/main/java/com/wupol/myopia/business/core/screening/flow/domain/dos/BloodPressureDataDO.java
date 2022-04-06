76912bec9c95bcbdf06a45d92556af0292aa7a31
zhe.xu@vistel.cn <1>
2022-04-06 17:15:05 +0800

    app筛查，数据结构新增

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/BloodPressureDataDO.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/BloodPressureDataDO.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 /**
  * 血压
+ * @Description
+ * @Date 2021/4/06 16:50
+ * @Author xz
  */
 @Data
 public class BloodPressureDataDO {
--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/PrivacyDataDO.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/PrivacyDataDO.java
@@ -3,13 +3,19 @@ package com.wupol.myopia.business.core.screening.flow.domain.dos;
 import lombok.Data;
 
 /**
- * 个人隐私项
+ * @Description 个人隐私项
+ * @Date 2021/4/06 16:50
+ * @Author by xz
  */
 @Data
 public class PrivacyDataDO {
-    // 是否隐私项
+    /**
+     * 是否隐私项
+     */
     private Boolean hasIncident;
 
-    // 出现的年龄
+    /**
+     * 出现的年龄
+     */
     private Integer age;
 }
