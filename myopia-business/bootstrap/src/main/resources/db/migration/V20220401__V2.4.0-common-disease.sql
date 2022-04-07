99ac0f4c99cc71b9e114c068e79c9da663d529ef
zhe.xu@vistel.cn <1>
2022-04-07 12:11:01 +0800

    app筛查，数据结构新增

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -6,5 +6,5 @@ ALTER TABLE m_vision_screening_result ADD blood_pressure_data json NULL COMMENT 
 ALTER TABLE m_vision_screening_result ADD diseases_history_data  json NULL COMMENT '疾病史';
 ALTER TABLE m_vision_screening_result ADD privacy_data json NULL COMMENT '个人隐私';
 ALTER TABLE m_screening_plan_school_student ADD state tinyint(1) NOT NULL default 0 COMMENT '未做检查说明【0:无；1：请假；2：转学;3:其他】';
-ALTER TABLE m_screening_plan_school_student ADD screening_type tinyint(1) NOT NULL default 0 COMMENT '筛查类型--来自筛查计划，筛查计划强一致 （0：视力筛查，1；常见病）';
+ALTER TABLE m_vision_screening_result ADD screening_type tinyint(1) NOT NULL default 0 COMMENT '筛查类型--来自筛查计划，筛查计划强一致 （0：视力筛查，1；常见病）';
 ALTER TABLE m_screening_plan ADD screening_type tinyint(1) NOT NULL default 0 COMMENT '筛查类型（0：视力筛查，1；常见病）';
