4e7efa83eafb943f4948bcd4e09c28a638e5caf7
Simple4H <crescentcxm@gmail.com>
2022-04-07 11:16:21 +0800

    feat(management): add sql

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -1,1 +1,9 @@
 -- sql 加上注释说明描述
+alter table m_stat_rescreen
+    add type int default 0 null comment '0-视力筛查 1-常见病' after school_id;
+
+alter table m_stat_rescreen
+    add physique_rescreen_num int null comment '体格复查人数';
+
+alter table m_stat_rescreen
+    add physique_index_num int null comment '体格复查指数';
