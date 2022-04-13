2a83539b84cefbb4d166256db056b8207f033168
zhe.xu@vistel.cn <1>
2022-04-13 11:13:02 +0800

    筛查不准确说明

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/DeviationDO.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/DeviationDO.java
@@ -16,7 +16,7 @@ public class DeviationDO {
     /**
      * 视光检查误差
      */
-    private EyesightDeviation eyesightDeviation;
+    private EyesightDeviationEnum eyesightDeviation;
 
     /**
      * 视光检查误差 说明
@@ -26,7 +26,7 @@ public class DeviationDO {
     /**
      * 身高体重误差
      */
-    private HeightWeightDeviation heightWeightDeviation;
+    private HeightWeightDeviationEnum heightWeightDeviation;
 
     /**
      * 身高体重误差 说明
@@ -35,32 +35,30 @@ public class DeviationDO {
 
     @Getter
     @AllArgsConstructor
-    public enum EyesightDeviation {
+    public enum EyesightDeviationEnum {
         ONE(1, "测量仪器问题"),
         TWO(2, "学生配合问题"),
         THREE(3, "学生眼睛疲劳影像"),
-        FOUR(4, "其他"),
-        ;
+        FOUR(4, "其他");
         private final Integer code;
         private final String name;
 
-        public static EyesightDeviation getByCode(Integer code) {
+        public static EyesightDeviationEnum getByCode(Integer code) {
             return code == null ? null : Arrays.stream(values()).filter((item) -> code.equals(item.code)).findFirst().orElse(null);
         }
     }
 
     @Getter
     @AllArgsConstructor
-    public enum HeightWeightDeviation {
+    public enum HeightWeightDeviationEnum {
         ONE(1, "测量仪器问题"),
         TWO(2, "穿着袜子或鞋子测量"),
         THREE(3, "衣着重量问题"),
-        FOUR(4, "其他"),
-        ;
+        FOUR(4, "其他");
         private final Integer code;
         private final String name;
 
-        public static HeightWeightDeviation getByCode(Integer code) {
+        public static HeightWeightDeviationEnum getByCode(Integer code) {
             return code == null ? null : Arrays.stream(values()).filter((item) -> code.equals(item.code)).findFirst().orElse(null);
         }
     }
