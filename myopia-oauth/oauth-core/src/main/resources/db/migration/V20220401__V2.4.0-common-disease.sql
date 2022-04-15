d21d33d9ab9ed7ced9d44ddd38fed58aacce46f9
Simple4H <crescentcxm@gmail.com>
2022-04-15 12:15:52 +0800

    feat(oauth): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -1,1 +1,4 @@
 -- ID 从830开始
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (830, '复查告知书-导出复查告知书', 'reviewExport', 'get:/management/screeningPlan/review/export', 0, 0, 1, 27, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (831, '复查告知书-获取班级年级信息', 'reviewGetGrades', 'get:/management/screeningPlan/review/getGrades/**/**/**', 0, 0, 1, 27, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (832, '复查告知书-获取学校列表', 'reviewGetSchool', 'get:/management/screeningPlan/review/getSchool/**/**', 0, 0, 1, 27, 1);
