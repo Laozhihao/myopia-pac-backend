9124af3e45784563881889aecbdc788535fdcbb8
zhe.xu@vistel.cn <1>
2022-04-15 21:41:46 +0800

    公共数据结构，夜戴角膜度数

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/VisionDataDO.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dos/VisionDataDO.java
@@ -57,6 +57,12 @@ public class VisionDataDO extends AbstractDiagnosisResult implements ScreeningRe
         private BigDecimal nakedVision;
 
         /**
+         *
+         * 等效球镜（夜戴角膜的度数）
+         */
+        private BigDecimal sphericalEquivalent;
+
+        /**
          * 判断是否有效数据
          *
          * @return
