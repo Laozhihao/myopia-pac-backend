23e4091a5cbead1c80eb2f1ac9b7015508dd87fd
yuanhang <295043131@qq.com>
2022-04-18 13:23:02 +0800

    测试类修改，参考sonarqube

--- myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/util/StatUtilTest.java
+++ myopia-business/bootstrap/src/test/java/com/wupol/myopia/business/util/StatUtilTest.java
@@ -23,107 +23,107 @@ public class StatUtilTest {
 
     @Test
     public void isLowVisionTest(){
-        Assert.assertEquals(StatUtil.isLowVision("4.5", 2),true);
-        Assert.assertEquals(StatUtil.isLowVision("4.9", 7),true);
-        Assert.assertEquals(StatUtil.isLowVision("5.0", 5),false);
+        Assert.assertEquals(true,StatUtil.isLowVision("4.5", 2));
+        Assert.assertEquals(true,StatUtil.isLowVision("4.9", 7));
+        Assert.assertEquals(false,StatUtil.isLowVision("5.0", 5));
     }
 
     @Test
     public void getNakedVisionWarningLevelTest(){
-        Assert.assertEquals(StatUtil.getNakedVisionWarningLevel("4.5", 5),null);
-        Assert.assertEquals(StatUtil.getNakedVisionWarningLevel("4.9", 7), WarningLevel.ONE);
-        Assert.assertEquals(StatUtil.getNakedVisionWarningLevel("4.7", 7),WarningLevel.TWO);
-        Assert.assertEquals(StatUtil.getNakedVisionWarningLevel("4.4", 7),WarningLevel.THREE);
+        Assert.assertEquals(null,StatUtil.getNakedVisionWarningLevel("4.5", 5));
+        Assert.assertEquals(WarningLevel.ONE,StatUtil.getNakedVisionWarningLevel("4.9", 7));
+        Assert.assertEquals(WarningLevel.TWO,StatUtil.getNakedVisionWarningLevel("4.7", 7));
+        Assert.assertEquals(WarningLevel.THREE,StatUtil.getNakedVisionWarningLevel("4.4", 7));
     }
 
     @Test
     public void isMyopiaTest(){
-        Assert.assertEquals(StatUtil.isMyopia(4.6f,4.6f, 4,4.8f),true);
-        Assert.assertEquals(StatUtil.isMyopia(4.6f,4.6f, 7,4.9f),true);
-        Assert.assertEquals(StatUtil.isMyopia(0), false);
-        Assert.assertEquals(StatUtil.isMyopia(3), true);
-        Assert.assertEquals(StatUtil.isMyopia(MyopiaLevelEnum.MYOPIA_LEVEL_EARLY),false);
+        Assert.assertEquals(true,StatUtil.isMyopia(4.6f,4.6f, 4,4.8f));
+        Assert.assertEquals(true,StatUtil.isMyopia(4.6f,4.6f, 7,4.9f));
+        Assert.assertEquals(false,StatUtil.isMyopia(0));
+        Assert.assertEquals(true,StatUtil.isMyopia(3));
+        Assert.assertEquals(false,StatUtil.isMyopia(MyopiaLevelEnum.MYOPIA_LEVEL_EARLY));
     }
 
     @Test
     public void isRefractiveErrorTest(){
-        Assert.assertEquals(StatUtil.isRefractiveError("2.0","3.0",2),true);
-        Assert.assertEquals(StatUtil.isRefractiveError("2.0","4.0",4),true);
-        Assert.assertEquals(StatUtil.isRefractiveError("3.0","4.0",6), true);
+        Assert.assertEquals(true,StatUtil.isRefractiveError("2.0","3.0",2));
+        Assert.assertEquals(true,StatUtil.isRefractiveError("2.0","4.0",4));
+        Assert.assertEquals( true,StatUtil.isRefractiveError("3.0","4.0",6));
     }
 
     @Test
     public void isAnisometropiaTest(){
-        Assert.assertEquals(StatUtil.isAnisometropiaVision("2.0","3.0"),false);
-        Assert.assertEquals(StatUtil.isAnisometropiaVision("2.0","4.0"),true);
-        Assert.assertEquals(StatUtil.isAnisometropiaAstigmatism("2.0","3.5"),true);
-        Assert.assertEquals(StatUtil.isAnisometropiaAstigmatism("2.0","3.0"),false);
+        Assert.assertEquals(false,StatUtil.isAnisometropiaVision("2.0","3.0"));
+        Assert.assertEquals(true,StatUtil.isAnisometropiaVision("2.0","4.0"));
+        Assert.assertEquals(true,StatUtil.isAnisometropiaAstigmatism("2.0","3.5"));
+        Assert.assertEquals(false,StatUtil.isAnisometropiaAstigmatism("2.0","3.0"));
     }
 
     @Test
     public void isAstigmatismTest(){
-        Assert.assertEquals(StatUtil.isAstigmatism("-1.0"),true);
-        Assert.assertEquals(StatUtil.isAstigmatism("-0.3"),false);
-        Assert.assertEquals(StatUtil.isAstigmatism(AstigmatismLevelEnum.ASTIGMATISM_LEVEL_LIGHT),true);
-        Assert.assertEquals(StatUtil.getAstigmatismWarningLevel("-0.3"),AstigmatismLevelEnum.ZERO);
-        Assert.assertEquals(StatUtil.getAstigmatismWarningLevel("1.0"),AstigmatismLevelEnum.ASTIGMATISM_LEVEL_LIGHT);
-        Assert.assertEquals(StatUtil.getAstigmatismWarningLevel("-3.0"),AstigmatismLevelEnum.ASTIGMATISM_LEVEL_MIDDLE);
-        Assert.assertEquals(StatUtil.getAstigmatismWarningLevel("-5.0"),AstigmatismLevelEnum.ASTIGMATISM_LEVEL_HIGH);
+        Assert.assertEquals(true,StatUtil.isAstigmatism("-1.0"));
+        Assert.assertEquals(false,StatUtil.isAstigmatism("-0.3"));
+        Assert.assertEquals(true,StatUtil.isAstigmatism(AstigmatismLevelEnum.ASTIGMATISM_LEVEL_LIGHT));
+        Assert.assertEquals(AstigmatismLevelEnum.ZERO,StatUtil.getAstigmatismWarningLevel("-0.3"));
+        Assert.assertEquals(AstigmatismLevelEnum.ASTIGMATISM_LEVEL_LIGHT,StatUtil.getAstigmatismWarningLevel("1.0"));
+        Assert.assertEquals(AstigmatismLevelEnum.ASTIGMATISM_LEVEL_MIDDLE,StatUtil.getAstigmatismWarningLevel("-3.0"));
+        Assert.assertEquals(AstigmatismLevelEnum.ASTIGMATISM_LEVEL_HIGH,StatUtil.getAstigmatismWarningLevel("-5.0"));
     }
 
     @Test
     public void isHyperopiaTest(){
-        Assert.assertEquals(StatUtil.isHyperopia(HyperopiaLevelEnum.HYPEROPIA_LEVEL_HIGH),true);
-        Assert.assertEquals(StatUtil.isHyperopia("2.0","2.0",3),false);
-        Assert.assertEquals(StatUtil.isHyperopia("2.0","2.0",5),true);
-        Assert.assertEquals(StatUtil.isHyperopia("2.0","2.0",7),true);
-        Assert.assertEquals(StatUtil.isHyperopia("2.0","2.0",9),true);
+        Assert.assertEquals(true,StatUtil.isHyperopia(HyperopiaLevelEnum.HYPEROPIA_LEVEL_HIGH));
+        Assert.assertEquals(false,StatUtil.isHyperopia("2.0","2.0",3));
+        Assert.assertEquals(true,StatUtil.isHyperopia("2.0","2.0",5));
+        Assert.assertEquals(true,StatUtil.isHyperopia("2.0","2.0",7));
+        Assert.assertEquals(true,StatUtil.isHyperopia("2.0","2.0",9));
     }
 
     @Test
     public void getSphericalEquivalentTest(){
-        Assert.assertEquals(StatUtil.getSphericalEquivalent("2.0","2.0"),new BigDecimal("3.0"));
+        Assert.assertEquals(new BigDecimal("3.0"),StatUtil.getSphericalEquivalent("2.0","2.0"));
     }
 
     @Test
     public void nakedVisionTest(){
-        Assert.assertEquals(StatUtil.nakedVision("4.6",3),WarningLevel.ONE);
-        Assert.assertEquals(StatUtil.nakedVision("4.6",4),WarningLevel.TWO);
-        Assert.assertEquals(StatUtil.nakedVision("4.6",6),WarningLevel.THREE);
+        Assert.assertEquals(WarningLevel.ONE,StatUtil.nakedVision("4.6",3));
+        Assert.assertEquals(WarningLevel.TWO,StatUtil.nakedVision("4.6",4));
+        Assert.assertEquals(WarningLevel.THREE,StatUtil.nakedVision("4.6",6));
     }
 
     @Test
     public void refractiveDataTest(){
-        Assert.assertEquals(StatUtil.refractiveData(new BigDecimal("-2.00"),null,null,0),WarningLevel.ONE);
-        Assert.assertEquals(StatUtil.refractiveData(null,new BigDecimal("5.00"),null,1),WarningLevel.THREE);
-        Assert.assertEquals(StatUtil.refractiveData(new BigDecimal("2.00"),null,3,2),WarningLevel.NORMAL);
-        Assert.assertEquals(StatUtil.refractiveData(new BigDecimal("3.00"),null,4,2),WarningLevel.ONE);
+        Assert.assertEquals(WarningLevel.ONE,StatUtil.refractiveData(new BigDecimal("-2.00"),null,null,0));
+        Assert.assertEquals(WarningLevel.THREE,StatUtil.refractiveData(null,new BigDecimal("5.00"),null,1));
+        Assert.assertEquals(WarningLevel.NORMAL,StatUtil.refractiveData(new BigDecimal("2.00"),null,3,2));
+        Assert.assertEquals(WarningLevel.ONE,StatUtil.refractiveData(new BigDecimal("3.00"),null,4,2));
     }
 
 
     @Test
     public void isOverweightAndObesityTest(){
-        Assert.assertEquals(StatUtil.isOverweightAndObesity("58","1.4","10.0",0).getFirst(),false);
-        Assert.assertEquals(StatUtil.isOverweightAndObesity("58","1.4","10.0",0).getSecond(),true);
+        Assert.assertEquals(false,StatUtil.isOverweightAndObesity("58","1.4","10.0",0).getFirst());
+        Assert.assertEquals(true,StatUtil.isOverweightAndObesity("58","1.4","10.0",0).getSecond());
     }
 
     @Test
     public void malnutritionTest(){
-        Assert.assertEquals(StatUtil.isStunting(0,"7.0","130"),false);
-        Assert.assertEquals(StatUtil.isWasting("58","1.4","10.0",0),false);
+        Assert.assertEquals(false,StatUtil.isStunting(0,"7.0","130"));
+        Assert.assertEquals(false,StatUtil.isWasting("58","1.4","10.0",0));
     }
 
     @Test
     public void isHighBloodPressureTest(){
-        Assert.assertEquals(StatUtil.isHighBloodPressure(100,80,0,8),false);
-        Assert.assertEquals(StatUtil.isHighBloodPressure(100,80,1,8),false);
-        Assert.assertEquals(StatUtil.isHighBloodPressure(100,80,0,18),false);
-        Assert.assertEquals(StatUtil.isHighBloodPressure(100,80,1,18),false);
+        Assert.assertEquals(false,StatUtil.isHighBloodPressure(100,80,0,8));
+        Assert.assertEquals(false,StatUtil.isHighBloodPressure(100,80,1,8));
+        Assert.assertEquals(false,StatUtil.isHighBloodPressure(100,80,0,18));
+        Assert.assertEquals(false,StatUtil.isHighBloodPressure(100,80,1,18));
     }
 
     @Test
     public void getAgeTest(){
-        Assert.assertEquals(StatUtil.getAge(DateUtil.toDate(LocalDate.of(2020,4,13))).getFirst().intValue(),2);
-        Assert.assertEquals(StatUtil.getAge(DateUtil.toDate(LocalDate.of(2020,4,13))).getSecond(),"2.0");
+        Assert.assertEquals(2,StatUtil.getAge(DateUtil.toDate(LocalDate.of(2020,4,13))).getFirst().intValue());
+        Assert.assertEquals("2.0",StatUtil.getAge(DateUtil.toDate(LocalDate.of(2020,4,13))).getSecond());
     }
 }
