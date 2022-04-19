9d61ab5f6f8a78b06fb665c38aa0c43d7c6fd84f
yuanhang <295043131@qq.com>
2022-04-19 17:23:24 +0800

    加索引

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -47,8 +47,8 @@ CREATE TABLE `m_screening_result_statistic` (
   `screening_task_id` int NOT NULL COMMENT '关联的任务id（is_total情况下，可能为0）',
   `screening_plan_id` int NOT NULL COMMENT '筛查计划id',
   `screening_type` tinyint NOT NULL COMMENT '筛查类型 （0-视力筛查、1-常见病筛查）',
-  `screening_org_id` int DEFAULT NULL COMMENT '筛查机构id',
-  `school_id` int DEFAULT NULL COMMENT '学校ID',
+  `screening_org_id` int NOT NULL COMMENT '筛查机构id',
+  `school_id` int NOT NULL COMMENT '学校ID',
   `school_type` tinyint NOT NULL COMMENT '学校类型 0-小学,1-初级中学,2-高级中学,3-完全中学,4-九年一贯制学校,5-十二年一贯制学校,6-职业高中,7-其他,8-幼儿园',
   `school_num` int DEFAULT NULL COMMENT '学校数',
   `district_id` int unsigned NOT NULL COMMENT '筛查范围、所属的地区id',
@@ -66,5 +66,7 @@ CREATE TABLE `m_screening_result_statistic` (
   `questionnaire` json DEFAULT NULL COMMENT '问卷情况',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `screening_result_statistic_district_unique` (`screening_notice_id`,`district_id`,`screening_type`,`is_total`) USING BTREE,
+  KEY `screening_result_statistic_school_unique` (`screening_plan_id`,`screening_type`,`screening_org_id`,`school_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='筛查结果统计表';
