99d4cc00bc1ff1e6a8d26162a2d7e1e6204d9d66
钓猫的小鱼 <2292229154@qq.com>
2022-04-20 09:48:52 +0800

    feat (screening) 补全注释

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -4,6 +4,4 @@ INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `o
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (832, '复查告知书-获取学校列表', 'reviewGetSchool', 'get:/management/screeningPlan/review/getSchool/**/**', 0, 0, 1, 27, 1);
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (823, '复测报告（二级）获取学校日期', 'sRescreenSchoolDate', 'get:/management/stat/rescreen/schoolDate', 0, 0, 0, 2, 1);
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (822, '复查报告（三级）获取学校日期', 'rescreenSchoolDate', 'get:/management/stat/rescreen/schoolDate', 0, 0, 0, 3, 1);
-
-VALUES
-    (833, '学生档案卡路径', 'workOrderStudentScreeningList', 'get:/management/student/screeningResult/**', 0, 0, 1, 811, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code) VALUES (833, '学生档案卡路径', 'workOrderStudentScreeningList', 'get:/management/student/screeningResult/**', 0, 0, 1, 811, 1);
