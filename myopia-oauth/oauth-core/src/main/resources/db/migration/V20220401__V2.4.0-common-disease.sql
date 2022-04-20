7704f31e8c40490ab459d95c92efa17855a7e71c
钓猫的小鱼 <2292229154@qq.com>
2022-04-20 10:43:26 +0800

    feat (screening) 修改SQLID

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220401__V2.4.0-common-disease.sql
@@ -1,3 +1,3 @@
 -- ID 从830开始
 VALUES
-    (833, '学生档案卡路径', 'workOrderStudentScreeningList', 'get:/management/student/screeningResult/**', 0, 0, 1, 811, 1);
+    (835, '学生档案卡路径', 'workOrderStudentScreeningList', 'get:/management/student/screeningResult/**', 0, 0, 1, 811, 1);
