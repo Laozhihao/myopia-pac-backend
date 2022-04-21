e0cc777c15e15a4b2cb5b05b5a7929d62f3b6d77
赵太斌 <1542471734@qq.com>
2022-04-21 21:52:25 +0800

    筛查计划查看学生详情

---
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dto/SaprodontiaStat.java
@@ -0,0 +1,20 @@
+package com.wupol.myopia.business.core.screening.flow.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class SaprodontiaStat {
+
+    /**
+     * 为龋（d/D）的牙齿总数
+     */
+    private Integer dCount;
+    /**
+     * 为失（m/M）的牙齿总数
+     */
+    private Integer mCount;
+    /**
+     * 为补（f/F）的牙齿总数
+     */
+    private Integer fCount;
+}
