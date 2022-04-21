f568a6937a7d1b5c1892ba0e4bdd9c07fc7ae4ee
Simple4H <crescentcxm@gmail.com>
2022-04-21 10:49:46 +0800

    feat(management): 复测报告bug fix

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/service/StatRescreenService.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/service/StatRescreenService.java
@@ -2,6 +2,7 @@ package com.wupol.myopia.business.core.screening.flow.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.wupol.myopia.base.service.BaseService;
+import com.wupol.myopia.base.util.DateUtil;
 import com.wupol.myopia.business.core.screening.flow.domain.mapper.StatRescreenMapper;
 import com.wupol.myopia.business.core.screening.flow.domain.model.StatRescreen;
 import io.jsonwebtoken.lang.Assert;
@@ -56,7 +57,7 @@ public class StatRescreenService extends BaseService<StatRescreenMapper, StatRes
     }
 
     public List<StatRescreen> getByPlanAndSchool(Integer planId, Integer schoolId, Date screeningTime) {
-        return baseMapper.getByPlanAndSchool(planId, schoolId, screeningTime, );
+        return baseMapper.getByPlanAndSchool(planId, schoolId, screeningTime, DateUtil.getYesterdayEndTime());
     }
 
 }
