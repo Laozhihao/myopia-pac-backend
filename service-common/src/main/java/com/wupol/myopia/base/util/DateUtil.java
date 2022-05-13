033517fedd96a781d89d51b4991f67a8be5e2dc0
赵太斌 <1542471734@qq.com>
2022-05-13 16:22:50 +0800

    结束时间格式化时分秒

--- service-common/src/main/java/com/wupol/myopia/base/util/DateUtil.java
+++ service-common/src/main/java/com/wupol/myopia/base/util/DateUtil.java
@@ -478,6 +478,6 @@ public class DateUtil extends cn.hutool.core.date.DateUtil {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.DATE,days);
-        return calendar.getTime();
+        return getEndTime(calendar.getTime());
     }
 }
