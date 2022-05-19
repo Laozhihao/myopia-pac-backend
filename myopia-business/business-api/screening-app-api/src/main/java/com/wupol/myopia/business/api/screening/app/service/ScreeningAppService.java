efc79cfa3e3d69208898983ec3ea720680fda845
zhe.xu@vistel.cn <1>
2022-05-19 10:31:43 +0800

    重载

--- myopia-business/business-api/screening-app-api/src/main/java/com/wupol/myopia/business/api/screening/app/service/ScreeningAppService.java
+++ myopia-business/business-api/screening-app-api/src/main/java/com/wupol/myopia/business/api/screening/app/service/ScreeningAppService.java
@@ -669,7 +669,7 @@ public class ScreeningAppService {
         screeningProgressState.setErrorItemCount(screeningProgressState.getRetestStudents().stream().mapToInt(RetestStudentVO::getErrorItemCount).sum());
         screeningProgressState.setRetestItemCount(screeningProgressState.getRetestStudents().stream().mapToInt(RetestStudentVO::getRetestItemCount).sum());
         screeningProgressState.setErrorRatio(screeningProgressState.getRetestItemCount() == 0 ? BigDecimal.ZERO : new BigDecimal(screeningProgressState.getErrorItemCount()).divide(new BigDecimal(screeningProgressState.getRetestItemCount()), 4, BigDecimal.ROUND_HALF_UP));
-        screeningProgressState.setRetestRatio(new BigDecimal(screeningProgressState.getRetestStudents().size()).divide(new BigDecimal(screeningProgressState.getScreeningCount()), 4, BigDecimal.ROUND_HALF_UP));
+        screeningProgressState.setRetestRatio(screeningProgressState.getScreeningCount() == 0? BigDecimal.ZERO : new BigDecimal(screeningProgressState.getRetestStudents().size()).divide(new BigDecimal(screeningProgressState.getScreeningCount()), 4, BigDecimal.ROUND_HALF_UP));
         screeningProgressState.setReScreeningCount(screeningProgressState.getRetestStudents().size());
         return screeningProgressState;
     }
