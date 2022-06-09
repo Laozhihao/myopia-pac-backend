da5fec2789ca92765dc32e43a5b548c313f00f56
yuanhang <295043131@qq.com>
2022-06-09 09:49:13 +0800

    不复用实体合并

--- myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/domain/vo/report/HeightAndWeightMonitorTable.java
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/domain/vo/report/HeightAndWeightMonitorTable.java
@@ -1,7 +1,10 @@
 package com.wupol.myopia.business.api.management.domain.vo.report;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
 
 /**
  * 身高体重监测-表格数据
@@ -9,9 +12,8 @@ import lombok.EqualsAndHashCode;
  * @author hang.yuan
  * @date 2022/6/6
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class HeightAndWeightMonitorTable extends HeightAndWeightVO {
+public class HeightAndWeightMonitorTable {
 
     /**
      * 项目 （性别、学龄段、年龄段）
@@ -22,5 +24,47 @@ public class HeightAndWeightMonitorTable extends HeightAndWeightVO {
      * 筛查人数(有效数据)
      */
     private Integer validScreeningNum;
+
+    /**
+     * 超重人数
+     */
+    private Integer overweightNum;
+
+    /**
+     * 肥胖人数
+     */
+    private Integer obeseNum;
+
+    /**
+     * 营养不良人数
+     */
+    private Integer malnourishedNum;
+    /**
+     * 生长迟缓人数
+     */
+    private Integer stuntingNum;
+
+    /**
+     * 超重率
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal overweightRatio;
+    /**
+     * 肥胖率
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal obeseRatio;
+
+    /**
+     * 营养不良率
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal malnourishedRatio;
+
+    /**
+     * 生长迟缓率
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal stuntingRatio;
 
 }
