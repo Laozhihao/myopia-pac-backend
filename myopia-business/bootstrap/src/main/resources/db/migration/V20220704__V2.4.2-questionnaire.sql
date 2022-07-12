8dc0bcf34088af25238631a8633ea617c043e0d1
Simple4H <crescentcxm@gmail.com>
2022-07-12 12:07:03 +0800

    feat(questionnaire): init

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220704__V2.4.2-questionnaire.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220704__V2.4.2-questionnaire.sql
@@ -19,15 +19,16 @@ CREATE TABLE `q_questionnaire`
 
 CREATE TABLE `q_question`
 (
-    `id`            int          not null auto_increment comment '主键',
-    `type`          varchar(30)  not null comment '问题类型，如：radio（单选）、checkbox（多选）、input（填空）',
-    `title`         varchar(255) not null comment '问题题目',
-    `attribute`     json         default null comment '问题属性',
-    `options`       json         not null comment '问题的答案选项',
-    `serial_number` varchar(15)  default null comment '问题的序号',
-    `pid`           int          not null comment '父ID，没有上级为-1',
-    `create_time`   timestamp    not null default CURRENT_TIMESTAMP comment '创建时间',
-    `update_time`   timestamp    not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
+    `id`                     int          not null auto_increment comment '主键',
+    `type`                   varchar(30)  not null comment '问题类型，如：radio（单选）、checkbox（多选）、input（填空）',
+    `title`                  varchar(255) not null comment '问题题目',
+    `attribute`              json                  default null comment '问题属性',
+    `options`                json         not null comment '问题的答案选项',
+    `same_question_group_id` varchar(128) null comment '相同问题uuid',
+    `serial_number`          varchar(15)           default null comment '问题的序号',
+    `pid`                    int          not null comment '父ID，没有上级为-1',
+    `create_time`            timestamp    not null default CURRENT_TIMESTAMP comment '创建时间',
+    `update_time`            timestamp    not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   default CHARSET = utf8mb4
@@ -68,10 +69,12 @@ CREATE TABLE `q_user_answer`
     `user_id`          int       not null comment '用户ID',
     `questionnaire_id` int       not null comment '问卷ID',
     `question_id`      int       not null comment '问题ID',
-    `answer`           json      default null comment '用户答案',
+    `answer`           json               default null comment '用户答案',
     `create_time`      timestamp not null default CURRENT_TIMESTAMP comment '创建时间',
     `update_time`      timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    constraint q_user_answer_user_id_questionnaire_id_question_id_uindex
+        unique (user_id, questionnaire_id, question_id)
 ) ENGINE = InnoDB
   default CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci
@@ -79,22 +82,22 @@ CREATE TABLE `q_user_answer`
 
 create table q_user_question_record
 (
-    id               int auto_increment comment '主键'
+    id                 int auto_increment comment '主键'
         primary key,
-    user_id          int                                 not null comment '用户端Id',
-    questionnaire_id int                                 not null comment '问卷Id',
-    plan_id          int                                 null comment '计划Id',
-    task_id          int                                 null comment '任务Id',
-    notice_id        int                                 null comment '通知Id',
-    gov_id           int                                 null comment '政府Id',
-    school_id        int                                 null comment '学校Id',
-    student_id       int                                 null comment '学生Id',
-    type             int                                 null comment '问卷类型',
-    status           tinyint                             null comment '状态 0-未开始 1-进行中 2-结束',
-    create_time      timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time      timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
+    user_id            int                                 not null comment '用户端Id',
+    questionnaire_id   int                                 not null comment '问卷Id',
+    plan_id            int                                 null comment '计划Id',
+    task_id            int                                 null comment '任务Id',
+    notice_id          int                                 null comment '通知Id',
+    gov_id             int                                 null comment '政府Id',
+    school_id          int                                 null comment '学校Id',
+    student_id         int                                 null comment '学生Id',
+    questionnaire_type int                                 null comment '问卷类型',
+    status             tinyint                             null comment '状态 0-未开始 1-进行中 2-结束',
+    create_time        timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time        timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 )
     comment '用户答问卷记录表';
 
 alter table q_question
-    add same_question_group_id varchar(128) null comment '相同问题uuid' after serial_number;
+    add
