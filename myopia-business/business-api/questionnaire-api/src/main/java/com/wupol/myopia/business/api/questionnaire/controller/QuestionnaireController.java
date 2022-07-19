67378db7ace3f9531087648bc4776702ed2ce5a0
Simple4H <crescentcxm@gmail.com>
2022-07-19 17:33:47 +0800

    feat(questionnaire): 获取标题

--- myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QuestionnaireController.java
+++ myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QuestionnaireController.java
@@ -3,12 +3,9 @@ package com.wupol.myopia.business.api.questionnaire.controller;
 import com.wupol.myopia.base.handler.ResponseResultBody;
 import com.wupol.myopia.business.api.questionnaire.service.QuestionnaireBizService;
 import com.wupol.myopia.business.core.questionnaire.domain.dto.QuestionnaireInfoDTO;
-import com.wupol.myopia.business.core.questionnaire.domain.model.Questionnaire;
+import com.wupol.myopia.business.core.questionnaire.domain.dto.UserQuestionnaireResponseDTO;
 import com.wupol.myopia.business.core.questionnaire.service.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,7 +31,7 @@ public class QuestionnaireController {
     }
 
     @GetMapping("/getUserQuestionnaire")
-    public Object getUserQuestionnaire() {
+    public List<UserQuestionnaireResponseDTO> getUserQuestionnaire() {
         return questionnaireBizService.getUserQuestionnaire();
     }
 }
