936f202a32a6aadb41deed8ec07e8253f61b902a
zhe.xu@vistel.cn <1>
2022-07-27 11:20:05 +0800

    管理后台的问卷接口

--- myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dto/GradeQuestionnaireInfo.java
+++ myopia-business/business-core/screening-flow-core/src/main/java/com/wupol/myopia/business/core/screening/flow/domain/dto/GradeQuestionnaireInfo.java
@@ -1,11 +1,13 @@
 package com.wupol.myopia.business.core.screening.flow.domain.dto;
 
+import com.wupol.myopia.business.core.school.constant.GradeCodeEnum;
 import com.wupol.myopia.business.core.school.domain.dto.SchoolGradeExportDTO;
 import com.wupol.myopia.business.core.school.domain.model.Student;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -35,12 +37,12 @@ public class GradeQuestionnaireInfo {
     private Integer studentCount;
 
     public static List<GradeQuestionnaireInfo> buildGradeInfo(Integer schoolId, Map<Integer, List<SchoolGradeExportDTO>> finalGradeIdMap, Map<Integer, List<Student>> finalUserGradeIdMap){
-        return finalGradeIdMap.get(schoolId).stream().map(grade->{
+        return finalGradeIdMap.get(schoolId).stream().map(grade -> {
             GradeQuestionnaireInfo questionnaireInfo = new GradeQuestionnaireInfo();
             questionnaireInfo.setGradeName(grade.getName());
             questionnaireInfo.setGradeId(grade.getId());
             questionnaireInfo.setStudentCount(CollectionUtils.isEmpty(finalUserGradeIdMap.get(grade.getId())) ? 0 : finalUserGradeIdMap.get(grade.getId()).size());
             return questionnaireInfo;
-        }).collect(Collectors.toList());
+        }).sorted(Comparator.comparing((GradeQuestionnaireInfo gradeInfo) -> Integer.valueOf(GradeCodeEnum.getByName(gradeInfo.getGradeName()).getCode()))).collect(Collectors.toList());
     }
 }
