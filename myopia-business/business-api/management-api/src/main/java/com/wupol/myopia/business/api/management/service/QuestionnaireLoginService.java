d78d25437996a7648699bebe636b3b8aeba67348
李勉颖 <mianying.li@vistel.cn>
2022-07-28 13:01:44 +0800

    修改类名冲突以及去除逗号

--- myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/QuestionnaireLoginService.java
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/service/QuestionnaireLoginService.java
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
  */
 @Service
 @Slf4j
-public class ScreeningPlanBizService {
+public class QuestionnaireLoginService {
 
     @Autowired
     private ScreeningPlanSchoolService screeningPlanSchoolService;
