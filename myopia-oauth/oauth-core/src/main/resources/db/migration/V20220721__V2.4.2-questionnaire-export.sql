af5446bb581cfa7f430058ce776b55a10a520089
yuanhang <295043131@qq.com>
2022-07-29 15:59:44 +0800

    导出问卷数据权限修改

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220721__V2.4.2-questionnaire-export.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220721__V2.4.2-questionnaire-export.sql
@@ -5,12 +5,21 @@
 -- 管理后台问卷管理权限
 
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES ('930', '导出问卷数据', 'exportQuestionnaire', 'post:/management/questionnaire/export', '0', '0', '30', '910', '1'),
-       ('931', '获取有问卷数据的学校', 'questionnaireDataSchool', 'get:/management/questionnaire/dataSchool', '0', '0', '31', '910','1'),
-       ('932', '获取问卷类型', 'questionnaireType', 'get:/management/questionnaire/type', '0', '0', '32', '910', '1'),
-       ('933', '下载问卷数据(页面)', 'questionnairePageData', NULL, '1', '1', '33', '910', '1'),
-       ('934', '下载问卷数据(学校列表)', 'questionnaireSchoolData', NULL, '1', '1', '34', '910', '1'),
-       ('935', '问卷数据(机构筛查记录)', 'organizationScreeningRecord', NULL, '1', '1', '35', '910', '1'),
-       ('936', '问卷数据(按区域统计)', 'districtStatistics', NULL, '1', '1', '36', '910', '1'),
-       ('937', '问卷数据(按学校统计)', 'schoolStatistics', NULL, '1', '1', '37', '910', '1'),
-       ('938', '问卷数据(学校筛查记录)', 'schoolScreeningRecord', NULL, '1', '1', '38', '910', '1');
+VALUES
+      ('930', '筛查机构管理-问卷数据', 'exportOrgQuestionnaire', 'post:/management/questionnaire/export', '0', '0', '2', '3', '1'),
+      ('931', '获取有问卷数据的学校', 'questionnaireDataSchool', 'get:/management/questionnaire/dataSchool', '0', '0', '3', '3', '1'),
+
+      ('932', '问卷管理-下载问卷数据', 'exportPageQuestionnaire', 'post:/management/questionnaire/export', '0', '0', '6', '882', '1'),
+
+      ('933', '学校环境-下载问卷数据', 'exportSchoolEnvironment', 'post:/management/questionnaire/export', '0', '0', '1', '888', '1'),
+
+      ('934', '中小学开展学校卫生工作-下载问卷数据', 'exportPrimarySecondarySchools', 'post:/management/questionnaire/export', '0', '0', '1', '887', '1'),
+      ('935', '学生视力不良及脊柱弯曲异常-下载问卷数据', 'exportVisionSpine', 'post:/management/questionnaire/export', '0', '0', '2', '887', '1'),
+      ('936', '学生健康状况-下载问卷数据', 'exportHealthStatus', 'post:/management/questionnaire/export', '0', '0', '3', '887', '1'),
+
+      ('937', '按区域-问卷数据', 'exportDistrictQuestionnaire', 'post:/management/questionnaire/export', '0', '0', '1', '29', '1'),
+      ('938', '按区域-获取问卷类型', 'districtQuestionnaireType', 'get:/management/questionnaire/type', '0', '0', '1', '29', '1'),
+      ('939', '按学校-问卷数据', 'exportSchoolQuestionnaire', 'post:/management/questionnaire/export', '0', '0', '1', '30', '1'),
+      ('940', '按学校-获取问卷类型', 'schoolQuestionnaireType', 'get:/management/questionnaire/type', '0', '0', '1', '30', '1'),
+
+      ('941', '多端学校管理筛查记录-问卷数据', 'exportMultiTerminalSchool', 'post:/management/questionnaire/export', '0', '0', '1', '9', '1');
