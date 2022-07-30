7db92b23ab3ec7edb1406e7f7bdc2591a5840f5e
yuanhang <295043131@qq.com>
2022-07-30 13:58:28 +0800

    移除@CrossOrigin

--- myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QesFieldMappingController.java
+++ myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QesFieldMappingController.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
  * @author Simple4H
  */
 @ResponseResultBody
-@CrossOrigin
 @RestController
 @RequestMapping("/qesFieldMapping")
 public class QesFieldMappingController  {
