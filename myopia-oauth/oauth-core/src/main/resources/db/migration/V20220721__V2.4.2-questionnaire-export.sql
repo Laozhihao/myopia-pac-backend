6a6ec0d005f4a962e577f6191526a5a85d06fe17
yuanhang <295043131@qq.com>
2022-08-01 14:59:45 +0800

    问卷数据SQL权限修改

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220721__V2.4.2-questionnaire-export.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220721__V2.4.2-questionnaire-export.sql
@@ -22,16 +22,4 @@ VALUES
         (939, '按学校-问卷数据', 'exportSchoolQuestionnaire', 'post:/management/questionnaire/export', 0, 0, 1, 30, 1),
         (940, '按学校-获取问卷类型', 'schoolQuestionnaireType', 'get:/management/questionnaire/type', 0, 0, 1, 30, 1),
 
-        (941, '多端学校管理筛查记录-问卷数据', 'exportMultiTerminalSchool', 'post:/management/questionnaire/export', 0, 0, 1, 9, 1)
-
-        (943, '问卷管理-下载问卷数据按钮', 'exportPageQuestionnaireButton', NULL, 0, 1, 6, 882, 1),
-        (944, '学校环境-下载问卷数据按钮', 'exportSchoolEnvironmentButton', NULL, 0, 1, 1, 888, 1),
-        (945, '中小学开展学校卫生工作-下载问卷数据按钮', 'exportPrimarySecondarySchoolsButton', NULL, 0, 1, 1, 887, 1),
-        (946, '学生视力不良及脊柱弯曲异常-下载问卷数据按钮', 'exportVisionSpineButton', NULL, 0, 1, 2, 887, 1),
-        (947, '学生健康状况-下载问卷数据按钮', 'exportHealthStatusButton', NULL, 0, 1, 3, 887, 1),
-        (948, '筛查机构管理-问卷数据按钮', 'exportOrgQuestionnaireButton', NULL, 0, 1, 2, 3, 1),
-        (949, '多端学校管理筛查记录-问卷数据', 'exportMultiTerminalSchoolButton', NULL, 0, 1, 1, 9, 1),
-        (950, '按区域-问卷数据按钮', 'exportDistrictQuestionnaireButton', NULL, 0, 1, 1, 29, 1),
-        (951, '按学校-问卷数据按钮', 'exportSchoolQuestionnaireButton', NULL, 0, 1, 1, 30, 1)
-
-      ;
+        (941, '多端学校管理筛查记录-问卷数据', 'exportMultiTerminalSchool', 'post:/management/questionnaire/export', 0, 0, 1, 9, 1);
