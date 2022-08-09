0a3a534e358d5739b1e4fa47305d36f64adbb787
Simple4H <crescentcxm@gmail.com>
2022-08-09 15:07:19 +0800

    feat(questionnaire): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220809__V2.4.3-questionnaire-gov-school.sql
@@ -0,0 +1,2 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
+VALUES (960, '通过key获取下拉值', 'getDropSelectKey', 'get:/questionnaire/getDropSelect/**', 0, 0, 0, 911, 1);
