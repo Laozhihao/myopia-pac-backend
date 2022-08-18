9c89fcde511d1ea52ac90a4f86244be217c7e291
Simple4H <crescentcxm@gmail.com>
2022-08-18 19:13:36 +0800

    feat(questionnaire): 问卷提交判断

--- myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QuestionnaireController.java
+++ myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/controller/QuestionnaireController.java
@@ -10,7 +10,10 @@ import com.wupol.myopia.business.core.questionnaire.domain.dto.QuestionnaireInfo
 import com.wupol.myopia.business.core.questionnaire.domain.dto.UserQuestionnaireResponseDTO;
 import com.wupol.myopia.business.core.questionnaire.service.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/service/impl/GovUserAnswerImpl.java
+++ myopia-business/business-api/questionnaire-api/src/main/java/com/wupol/myopia/business/api/questionnaire/service/impl/GovUserAnswerImpl.java
@@ -110,7 +110,7 @@ public class GovUserAnswerImpl implements IUserAnswerService {
     public List<UserQuestionnaireResponseDTO> getUserQuestionnaire(Integer userId) {
         ScreeningTask task = screeningTaskService.getOneByOrgId(userId);
         if (Objects.isNull(task)) {
-            throw new BusinessException("别搞，你没有问卷需要填写");
+            throw new BusinessException("你没有问卷需要填写");
         }
         List<QuestionnaireTypeEnum> typeList = QuestionnaireTypeEnum.getGovQuestionnaireType();
         return commonUserAnswer.getUserQuestionnaire(typeList);
