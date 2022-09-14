b9acc2fe59735064e132bbf48f12bc9fcdd7a282
Simple4H <crescentcxm@gmail.com>
2022-09-14 15:33:00 +0800

    feat(management): update sql

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220915__V2.4.5-school-independent-screening.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220915__V2.4.5-school-independent-screening.sql
@@ -1,9 +1,3 @@
-alter table m_school_admin
-    add role_type int default 0 null comment '0-管理员 1-校医' after gov_dept_id;
-
-alter table m_school_admin
-    add user_ids varchar(128) null comment '用户Ids，逗号隔开' after role_type;
-
 alter table m_school
     add vision_team_count int null comment '视力小分队人数';
 
