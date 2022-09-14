95b4021fb4a3870434fedf32b8a1845fce7033ef
Simple4H <crescentcxm@gmail.com>
2022-09-14 11:57:50 +0800

    feat(management): 添加新字段

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220913__V2.4.5-school-independent-screening.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220913__V2.4.5-school-independent-screening.sql
@@ -3,3 +3,12 @@ alter table m_school_admin
 
 alter table m_school_admin
     add user_ids varchar(128) null comment '用户Ids，逗号隔开' after role_type;
+
+alter table m_school
+    add vision_team_count int null comment '视力小分队人数';
+
+alter table m_school
+    add screening_config json null comment '筛查类型的配置';
+
+alter table m_school
+    add screening_type_config varchar(16) null comment '筛查类型配置, 英文逗号分隔, 0-视力筛查，1-常见病';
