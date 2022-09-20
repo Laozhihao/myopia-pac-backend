b576978645f11492c2bdd31ab86d9066f691a076
Simple4H <crescentcxm@gmail.com>
2022-09-20 17:18:41 +0800

    feat(management): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220913__V2.4.5-school-independent-screening.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220913__V2.4.5-school-independent-screening.sql
@@ -3,7 +3,8 @@ VALUES (1010, '视力小队', 'visionTeam', null, 1, 1, 1, 7, 1),
        (1011, '获取学校员工列表', 'schoolStaffList', 'get:/management/school/staff/list/**', 0, 0, 0, 1010, 1),
        (1012, '保存员工', 'schoolStaffSave', 'post:/management/school/staff/save/**', 0, 0, 0, 1010, 1),
        (1013, '启用/停用', 'schoolStaffEditStatus', 'post:/management/school/staff/editStatus/**/**', 0, 0, 0, 1010, 1),
-       (1014, '重置密码', 'schoolStaffReset', 'post:/management/school/staff/resetPassword/**', 0, 0, 0, 1010, 1);
+       (1014, '重置密码', 'schoolStaffReset', 'post:/management/school/staff/resetPassword/**', 0, 0, 0, 1010, 1),
+       (1015, '是否超过人数配置', 'isMoreThanConfig', 'get:/management/school/staff/checkTeamCount/**', 0, 0, 0, 1010,1);
 
 -- 将筛查机构的筛查人员的user_type改为0
 update o_user
