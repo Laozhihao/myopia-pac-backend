d43dab44bdefd4ad0b91925cbd739dba5b4634ee
yuanhang <295043131@qq.com>
2022-09-27 19:55:50 +0800

    SQL缺少分号

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20220927__V2.4.4-hot-fix.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20220927__V2.4.4-hot-fix.sql
@@ -1,2 +1,2 @@
 -- 名称有表情
-ALTER TABLE `o_user` MODIFY COLUMN `real_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '真实姓名'
+ALTER TABLE `o_user` MODIFY COLUMN `real_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '真实姓名';
