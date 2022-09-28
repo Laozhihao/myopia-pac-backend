686da27b256d9909b47f585c9abd888dd1548e90
Simple4H <crescentcxm@gmail.com>
2022-09-28 11:34:23 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
@@ -0,0 +1,2 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
+VALUES (1030, '模糊查询指定省份下学校', 'schoolProvinceList', 'get:/management/school/province/list', 0, 0, 0, 750, 1);
