aae599e5f840861bd9dc4ad41b8f9bab0ca01937
Simple4H <crescentcxm@gmail.com>
2022-09-28 11:54:44 +0800

    feat(management): code review

--- myopia-business/bootstrap/src/main/resources/db/migration/V20220929__V2.4.5-school-independent-screening.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20220929__V2.4.5-school-independent-screening.sql
@@ -1,11 +1,11 @@
 alter table m_school
-    add vision_team_count int not null  default 5 comment '视力小分队人数';
+    add vision_team_count int not null default 5 comment '视力小分队人数';
 
 alter table m_school
     add screening_config json null comment '筛查类型的配置';
 
 alter table m_school
-    add screening_type_config varchar(16) null comment '筛查类型配置, 英文逗号分隔, 0-视力筛查，1-常见病';
+    add screening_type_config varchar(16) null default '0' comment '筛查类型配置, 英文逗号分隔, 0-视力筛查，1-常见病';
 
 update m_school set screening_config = '{"channel":"Official","screeningTypeList":[0],"medicalProjectList":["vision","computer_optometry","other_eye_diseases"]}' where id is not null;
 
