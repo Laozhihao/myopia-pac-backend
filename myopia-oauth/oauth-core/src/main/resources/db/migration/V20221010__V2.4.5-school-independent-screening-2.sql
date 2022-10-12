f3713bcb9ca7878e89e49bb407a1e9e478a8a7d5
yuanhang <295043131@qq.com>
2022-10-12 13:40:46 +0800

    权限SQL

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
@@ -5,4 +5,6 @@ VALUES (1030, '模糊查询指定省份下学校', 'schoolProvinceList', 'get:/m
 -- 1000-1009
 INSERT INTO `o_permission`(`id`, `name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`)
 VALUES (1000, '创建筛查任务时-获取筛查学校列表', 'getSchoolList', 'get:/management/school/getSchoolList', 0, 0, 1, 25, 1),
-       (1047, '处理学校端学生的学龄段数据', 'gradeTypeData', 'get:/management/test/gradeTypeData', 0, 0, 0, 384, 1);
+       (1001, '处理学校端学生的学龄段数据', 'gradeTypeData', 'get:/management/test/gradeTypeData', 0, 0, 0, 384, 1),
+       (1002, '学生列表', 'schoolStudentList', 'get:/management/student/schoolList', 0, 0, 0, 10, 1),
+       (1003, '学生列表搜索下拉框', 'selectValue', 'get:/management/student/selectValue', 0, 0, 0, 10, 1);
