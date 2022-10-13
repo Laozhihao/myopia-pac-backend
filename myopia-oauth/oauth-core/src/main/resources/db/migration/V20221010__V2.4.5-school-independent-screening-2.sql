31a225a3822cf78e6ef244d7230a3ca62994d693
yuanhang <295043131@qq.com>
2022-10-13 15:47:24 +0800

    接口权限SQL

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
@@ -8,4 +8,6 @@ VALUES (1000, '创建筛查任务时-获取筛查学校列表', 'getSchoolList',
        (1001, '处理学校端学生的学龄段数据', 'gradeTypeData', 'get:/management/test/gradeTypeData', 0, 0, 0, 384, 1),
        (1002, '学生列表', 'schoolStudentList', 'get:/management/student/schoolList', 0, 0, 0, 10, 1),
        (1003, '学生列表搜索下拉框', 'selectValue', 'get:/management/student/selectValue', 0, 0, 0, 10, 1),
-       (1004, '学校学生的筛查记录', 'schoolStudentScreeningList', 'get:/management/student/screening/list/**', 0, 0, 0, 11, 1);
+       (1004, '学校学生的筛查记录', 'schoolStudentScreeningList', 'get:/management/student/screening/list/**', 0, 0, 0, 11, 1),
+       (1005, '学校学生详情', 'getSchoolStudentDetail', 'get:/management/student/school/**', 0, 0, 0, 11, 1),
+       (1006, '更新学校学生信息', 'saveSchoolStudent', 'post:/management/student/school/save', 0, 0, 0, 11, 1);
