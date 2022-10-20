eebf0ec72b00e9ca9cc0b23b0304b59599e77259
Simple4H <crescentcxm@gmail.com>
2022-10-20 17:45:00 +0800

    feat(school): 系统通知

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221010__V2.4.5-school-independent-screening-2.sql
@@ -1,2 +1,7 @@
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (1030, '模糊查询指定省份下学校', 'schoolProvinceList', 'get:/management/school/province/list', 0, 0, 0, 750, 1);
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
+VALUES (1050, '系统更新提醒', 'systemUpdateNotice', null, 1, 1, 0, 14, 1),
+       (1051, '系统更新通知下线通知', 'systemNoticeStatus', 'post:/management/systemNotice/status/**', 0, 0, 0, 1050,1),
+       (1052, '系统更新通知保存通知', 'systemNoticeSave', 'post:/management/systemNotice/save', 0, 0, 0, 1050, 1),
+       (1053, '系统更新通知获取列表', 'systemNoticeList', 'get:/management/systemNotice/list', 0, 0, 0, 1050, 1);
--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221019__V2.4.5-system-update-notice.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES (1050, '系统更新提醒', 'systemUpdateNotice', null, 1, 1, 0, 14, 1),
-       (1051, '系统更新通知下线通知', 'systemNoticeStatus', 'post:/management/systemNotice/status/**', 0, 0, 0, 1050,1),
-       (1052, '系统更新通知保存通知', 'systemNoticeSave', 'post:/management/systemNotice/save', 0, 0, 0, 1050, 1),
-       (1053, '系统更新通知获取列表', 'systemNoticeList', 'get:/management/systemNotice/list', 0, 0, 0, 1050, 1);
