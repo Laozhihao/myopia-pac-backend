d240ede37961b0309eb813aa3c51ab35a600e64b
lzh <zhihao.lao@vistel.cn>
2022-10-20 22:03:22 +0800

    feat: update flyway sql name

--- myopia-business/bootstrap/src/main/resources/db/migration/V20221020__V2.4.5-school-independent-screening-gov.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20221020__V2.4.5-school-independent-screening-gov.sql
@@ -53,7 +53,7 @@ create table m_system_update_notice
     id             int auto_increment comment 'id' primary key,
     create_user_id int                                 null comment '创建人ID',
     comment        varchar(256)                        not null comment '内容',
-    status         tinyint(0)                          not null default 0 comment '状态 0-上线 1-下线',
+    status         tinyint(1)                          not null default 0 comment '状态 0-上线 1-下线',
     system_code    json                                not null comment '系统编码',
     create_time    timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
     update_time    timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
