2deee27c5f720b1dbf25ba8d308aa5783fec6763
Simple4H <crescentcxm@gmail.com>
2022-10-25 16:04:47 +0800

    feat(management): bug fix

--- myopia-business/business-aggregation/student-biz/src/main/java/com/wupol/myopia/business/aggregation/student/domain/builder/StudentInfoBuilder.java
+++ myopia-business/business-aggregation/student-biz/src/main/java/com/wupol/myopia/business/aggregation/student/domain/builder/StudentInfoBuilder.java
@@ -38,15 +38,15 @@ public class StudentInfoBuilder {
         student.setHyperopiaLevel(statConclusion.getHyperopiaLevel());
         String schoolGradeCode = statConclusion.getSchoolGradeCode();
         GradeCodeEnum gradeCodeEnum = GradeCodeEnum.getByCode(schoolGradeCode);
+        if (Objects.equals(statConclusion.getIsLowVision(), Boolean.TRUE)) {
+            student.setLowVision(LowVisionLevelEnum.LOW_VISION.code);
+        } else {
+            student.setLowVision(null);
+        }
         if (!Objects.equals(SchoolAge.KINDERGARTEN.code,gradeCodeEnum.getType())){
             //小学及以上的数据同步
             student.setMyopiaLevel(statConclusion.getMyopiaLevel());
             student.setScreeningMyopia(statConclusion.getScreeningMyopia());
-            if (Objects.nonNull(statConclusion.getIsLowVision()) && Objects.equals(statConclusion.getIsLowVision(),Boolean.TRUE)) {
-                student.setLowVision(LowVisionLevelEnum.LOW_VISION.code);
-            }else {
-                student.setLowVision(null);
-            }
         }
     }
 }
