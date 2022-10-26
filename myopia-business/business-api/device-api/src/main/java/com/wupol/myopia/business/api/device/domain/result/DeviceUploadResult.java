5285aa2326d11ecc3648b8998d3236e52f58e68b
Simple4H <crescentcxm@gmail.com>
2022-10-26 11:16:30 +0800

    feat(management): VS666数据上传hotfix

--- myopia-business/business-api/device-api/src/main/java/com/wupol/myopia/business/api/device/domain/result/DeviceUploadResult.java
+++ myopia-business/business-api/device-api/src/main/java/com/wupol/myopia/business/api/device/domain/result/DeviceUploadResult.java
@@ -43,7 +43,7 @@ public class DeviceUploadResult {
      * @return
      */
     public static DeviceUploadResult error(String message) {
-        return new DeviceUploadResult(false,message,null);
+        return new DeviceUploadResult(false, message, null);
     }
 
 }
