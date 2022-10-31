6eb6a9296e4bebdafe9ca02d91079240e74a8c7e
Simple4H <crescentcxm@gmail.com>
2022-10-31 18:22:19 +0800

    feat(management): 异步导出PDF

--- myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/controller/PdfCallbackController.java
+++ myopia-business/business-api/management-api/src/main/java/com/wupol/myopia/business/api/management/controller/PdfCallbackController.java
@@ -65,6 +65,7 @@ public class PdfCallbackController {
             try {
                 File file = FileUtil.rename(ZipUtil.zip(srcPath), zipFileName, true, true);
                 noticeService.sendExportSuccessNotice(pdfGeneratorVO.getUserId(), pdfGeneratorVO.getUserId(), file.getName(), s3Utils.uploadFileToS3(file));
+                FileUtil.del(file);
             } catch (UtilException e) {
                 log.error("PDF请求回调异常, 请求参数:{}", JSON.toJSONString(responseDTO), e);
                 noticeService.sendExportFailNotice(pdfGeneratorVO.getUserId(), pdfGeneratorVO.getUserId(), "【导出失败】，" + zipFileName + "请稍后重试");
