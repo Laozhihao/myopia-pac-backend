7cce7769c4206c0f8e1fb0141908c6a7ea37b338
Simple4H <crescentcxm@gmail.com>
2022-11-03 15:24:47 +0800

    feat(management): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221103__V2.4.5-school-notification-config.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221103__V2.4.5-school-notification-config.sql
@@ -1,3 +1,3 @@
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES (1060, '学校告知书配置', 'schoolNotificationConfig', 'get:/management/school/update/notificationConfig/**', 0, 0, 0,
+VALUES (1060, '学校告知书配置', 'schoolResultNoticeConfig', 'get:/management/school/update/resultNoticeConfig/**', 0, 0, 0,
         7, 1);
