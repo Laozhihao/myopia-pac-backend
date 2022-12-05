170b29853c5538aa3dbe8748b9142383b1897789
Simple4H <crescentcxm@gmail.com>
2022-12-05 11:10:29 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221205__V2.4.5-excel-template.sql
@@ -0,0 +1,4 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`,
+                          pid, system_code)
+VALUES (1065, '导出学校筛查模板', 'schoolExportTemplate', 'get:/management/screeningResult/school/template/export', 0,
+        0, 0, 27, 1);
