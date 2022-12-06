6677adff9c423ed6d11b5f1cef4a18a3a0a941fd
Simple4H <crescentcxm@gmail.com>
2022-12-06 14:20:56 +0800

    feat(oauth): add sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20221205__V2.4.5-excel-template.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20221205__V2.4.5-excel-template.sql
@@ -3,4 +3,5 @@ INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `o
 VALUES (1065, '导出学校筛查模板', 'schoolExportTemplate', 'get:/management/screeningResult/school/template/export', 0,
         0, 0, 27, 1),
        (1066, '导入学校筛查模板', 'schoolTemplateImport', 'post:/management/screeningResult/school/template/import', 0,
-        0, 0, 27, 1);
+        0, 0, 27, 1),
+       (1067, '导出筛查数据', 'importScreeningData', null, 0, 0, 0, 27, 1);
