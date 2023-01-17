8ba9b30892156911a7fac92f4b8e14f59d1fbb17
lzh <zhihao.lao@vistel.cn>
2023-01-17 16:51:39 +0800

    optimize：添加索引

--- myopia-business/bootstrap/src/main/resources/db/migration/V20230117__V2.5.1-performance-optimization.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20230117__V2.5.1-performance-optimization.sql
@@ -24,3 +24,16 @@ ALTER TABLE `m_vision_screening_result`
 ALTER TABLE `m_screening_plan_school_student`
     DROP INDEX `m_screening_plan_school_student_screening_plan_id_index`,
     ADD INDEX `m_spss_student_id_index`(`student_id`) USING BTREE;
+
+-- 表 m_screening_plan_school_student 加索引
+ALTER TABLE `m_screening_plan_school_student`
+    ADD INDEX `m_spss_plan_id_class_id`(`screening_plan_id`, `class_id`) USING BTREE;
+
+-- 表 m_stat_conclusion 优化索引顺序
+ALTER TABLE `m_stat_conclusion`
+    DROP INDEX `district_INDEX`,
+    ADD INDEX `m_sc_create_time_rescreen_valid_cooperative_district_index`(`create_time`, `is_valid`, `is_rescreen`, `is_cooperative`, `warning_level`, `district_id`) USING BTREE;
+
+-- 表 m_school_student 加索引
+ALTER TABLE `m_school_student`
+    ADD INDEX `m_school_id_student_id_index`(`school_id`, `student_id`) USING BTREE;
--- myopia-business/bootstrap/src/main/resources/db/migration/V20230118__V2.5.1-performance-optimization.sql
+++
@@ -1,3 +0,0 @@
--- 表 m_school_student 加索引
-ALTER TABLE `m_school_student`
-    ADD INDEX `m_school_id_student_id_index`(`school_id`, `student_id`) USING BTREE;
