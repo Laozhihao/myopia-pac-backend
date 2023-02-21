d687282812b7bbd40f14103d37db4ec685283e3f
钓猫的小鱼 <2292229154@qq.com>
2023-02-21 11:08:27 +0800

    feat(V20230222__V2.5.1-reported-data): VS550 需求调整
    技术评审前提交

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230111__V2.5.0-school-vision-report-refactor.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230111__V2.5.0-school-vision-report-refactor.sql
@@ -1,9 +1,8 @@
--- ID从1070开始，新谋：1070 ~ 1074，立周：1075~1079
+-- ID从1070开始，新谋：1070 ~ 1074，立周：1075~1079 帅龙：1080~1085
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (1070, '幼儿园报告', 'refactorSchoolKindergarten', 'get:/management/report/refactor/school/kindergarten', 0, 0,0, 31, 1),
        (1071, '获取学生类型', 'refactorSchoolStudentType', 'get:/management/report/refactor/school/studentType', 0, 0,0, 31, 1),
        (1072, '中小学报告', 'refactorSchoolPrimary', 'get:/management/report/refactor/school/primary', 0, 0, 0, 31, 1),
        (1073, '幼儿园报告', 'refactorSchoolKindergarten', 'get:/management/report/refactor/school/kindergarten', 0, 0,0, 27, 1),
        (1074, '获取学生类型', 'refactorSchoolStudentType', 'get:/management/report/refactor/school/studentType', 0, 0,0, 27, 1)
-       (1075, '中小学报告', 'refactorSchoolPrimary', 'get:/management/report/refactor/school/primary', 0, 0, 0, 27, 1),
-       (1080, '获取设备对应模板信息（VS550）', 'templateBasicList', 'get:/management/device/report/template/basic/list', 0, 0, 0, 2, 1);
+       (1075, '中小学报告', 'refactorSchoolPrimary', 'get:/management/report/refactor/school/primary', 0, 0, 0, 27, 1);
