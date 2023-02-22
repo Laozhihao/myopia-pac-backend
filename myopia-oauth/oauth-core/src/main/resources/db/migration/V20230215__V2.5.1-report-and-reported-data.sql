93e91c78079ceae07b94218e35b88d9b9ec9f924
钓猫的小鱼 <2292229154@qq.com>
2023-02-22 09:47:46 +0800

    feat(V20230222__V2.5.1-reported-data): VS550 需求调整
    技术评审前提交

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230215__V2.5.1-report-and-reported-data.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230215__V2.5.1-report-and-reported-data.sql
@@ -1,3 +1,3 @@
--- ID从1070开始， 帅龙：1080~1085 佳亮：1086~1089
+-- ID从1080开始， 帅龙：1080~1085 佳亮：1086~1089
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
 VALUES (1080, '获取设备对应模板信息（VS550）', 'templateBasicList', 'get:/management/device/report/template/basic/list', 0, 0, 0, 2, 1);
