cac3823c91ddfccf5fdc98c6d7fd9d8f76423163
Simple4H <crescentcxm@gmail.com>
2023-03-10 10:32:59 +0800

    feat(management): update sql

--- myopia-business/bootstrap/src/main/resources/db/migration/V20230309__V2.5.1-feature-data-submit-template.sql
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20230309__V2.5.1-feature-data-submit-template.sql
@@ -6,4 +6,7 @@ update m_school set data_submit_config = '[0]' where data_submit_config is null 
 alter table m_screening_organization
     add data_submit_config json null comment '数据上报模版配置';
 
-update m_screening_organization set data_submit_config = '[0]' where data_submit_config is null ;
+update m_screening_organization set data_submit_config = '[0]' where data_submit_config is null;
+
+create index m_plan_student_id_is_rescreen_index
+    on m_stat_conclusion (screening_plan_school_student_id, is_rescreen);
