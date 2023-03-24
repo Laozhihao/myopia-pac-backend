33c8c9bd47ef182a728f7a2fe5f154f0e10176b5
Simple4H <crescentcxm@gmail.com>
2023-03-24 16:18:10 +0800

    feat(oauth): add sql

---
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230324__V2.5.1-plan-link.sql
@@ -0,0 +1,3 @@
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
+VALUES (1114, '获取可关联的通知', 'planLinkNoticeList', 'get:/management/screeningPlan/planLinkNotice/list', 0, 0, 0, 27, 1),
+       (1115, '计划关联通知', 'planLinkNotice', 'post:/management/screeningPlan/linkNotice/link', 0, 0, 0, 27, 1);
