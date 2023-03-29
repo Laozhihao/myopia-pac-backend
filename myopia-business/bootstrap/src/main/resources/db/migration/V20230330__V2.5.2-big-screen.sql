5e3f3071d4ce583c892b8fac66b0a123755e73fb
lzh <zhihao.lao@vistel.cn>
2023-03-29 21:05:31 +0800

    fix(数据大屏)：更新时update_time字段值没更新

---
+++ myopia-business/bootstrap/src/main/resources/db/migration/V20230330__V2.5.2-big-screen.sql
@@ -0,0 +1,2 @@
+-- 更新m_district_big_screen_statistic表的更新时间字段值为跟随数据的变动而更新
+ALTER TABLE `m_district_big_screen_statistic` MODIFY COLUMN `update_time` TIMESTAMP ( 0 ) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ( 0 ) COMMENT '大屏展示--更新时间' AFTER `avg_vision`;
