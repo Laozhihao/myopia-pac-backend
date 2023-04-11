7ff873aa9e620f9fc58f42e02c5b5279c3732705
Simple4H <crescentcxm@gmail.com>
2023-04-11 15:52:40 +0800

    feat(management): 优化提醒

--- myopia-business/business-aggregation/screening-biz/src/main/java/com/wupol/myopia/business/aggregation/screening/service/ScreeningNoticeBizFacadeService.java
+++ myopia-business/business-aggregation/screening-biz/src/main/java/com/wupol/myopia/business/aggregation/screening/service/ScreeningNoticeBizFacadeService.java
@@ -21,7 +21,6 @@ import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
