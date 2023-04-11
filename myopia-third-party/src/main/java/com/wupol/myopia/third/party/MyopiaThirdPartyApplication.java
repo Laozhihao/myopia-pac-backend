609bd66a725b47cdbbe6a09aaba350390d3e5811
lzh <zhihao.lao@vistel.cn>
2023-04-11 19:25:29 +0800

    feat：add new service "myopia-third-party"

---
+++ myopia-third-party/src/main/java/com/wupol/myopia/third/party/MyopiaThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package com.wupol.myopia.third.party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyopiaThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyopiaThirdPartyApplication.class, args);
+    }
+
+}
