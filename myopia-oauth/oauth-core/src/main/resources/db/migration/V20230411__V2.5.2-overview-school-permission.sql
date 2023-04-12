63d57ca81afeb3d544faadf785cec2806767a073
lzh <zhihao.lao@vistel.cn>
2023-04-12 15:42:16 +0800

    fix：add permission

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230411__V2.5.2-overview-school-permission.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230411__V2.5.2-overview-school-permission.sql
@@ -1,5 +1,8 @@
 -- “多端管理-学校管理-筛查记录-学生管理”下新增权限
 INSERT INTO `o_permission`(`id`, `name`, `menu_btn_name`, `api_url`, `is_menu`, `is_page`, `order`, `pid`, `system_code`) VALUES
+(1125, '导出学校筛查数据模板（带学生信息）', 'schoolExportTemplate', 'get:/management/screeningResult/school/template/export', 0, 0, 0, 211, 1),
+(1126, '导入学校筛查数据', 'schoolTemplateImport', 'post:/management/screeningResult/school/template/import', 0, 0, 0, 211, 1),
+(1127, '导出筛查数据（按钮）', 'importScreeningData', null, 0, 0, 0, 211, 1),
 (1128, '筛查学生列表（按钮）', 'screeningStudentListBtn', null, 0, 0, 1, 9, 1),
 (1129, '按学校-查看筛查结果详情', 'schoolScreeningDetail', 'get:/management/screening-statistic/school/schoolStatisticDetail', 0, 0, 1, 9, 1),
 (1130, '多端获取学校年级', 'manageSchoolGrade', 'get:/management/screeningPlan/grades/**/**', 0, 0, 1, 211, 1),
