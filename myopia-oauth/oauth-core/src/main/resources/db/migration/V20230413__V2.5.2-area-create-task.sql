074748b0e0f6c1393a5c81ca0ed8d46bb5c7adb0
Simple4H <crescentcxm@gmail.com>
2023-04-14 16:58:31 +0800

    feat(management): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230413__V2.5.2-area-create-task.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230413__V2.5.2-area-create-task.sql
@@ -3,4 +3,5 @@ VALUES (1140, '市区创建任务按钮', 'areaCreateTaskBtn', '', 0, 0, 0, 26, 
        (1141, '市区创建任务', 'areaCreateTask', 'post:/management/screeningTask/urbanArea/createTask', 0, 0, 0, 26, 1),
        (1142, '复查告知书-获取学校列表', 'reviewSchoolList', 'get:/management/screeningPlan/review/getSchool/**/**', 0, 0, 1, 9, 1),
        (1143, '复查告知书-获取班级年级信息', 'reviewSchoolGrades', 'get:/management/screeningPlan/review/getGrades/**/**/**', 0, 0, 1, 9, 1),
-       (1144, '复查告知书-导出复查告知书', 'reviewExport', 'get:/management/screeningPlan/review/export', 0, 0, 1, 9, 1);
+       (1144, '复查告知书-导出复查告知书', 'reviewExport', 'get:/management/screeningPlan/review/export', 0, 0, 1, 9, 1),
+       (1145, '获取学生民族列表', 'studentNation', 'get:/management/student/nation', 0, 0, 1, 3, 1);
