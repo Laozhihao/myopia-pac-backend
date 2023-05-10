5ef71a980f00f4cbbbf7b7a97e39d639204c71f4
Simple4H <crescentcxm@gmail.com>
2023-05-10 10:51:07 +0800

    feat(management): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230508__V2.5.3-js2-02-20.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230508__V2.5.3-js2-02-20.sql
@@ -1,4 +1,4 @@
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES (1150, '获取学生类型', 'refactorSchoolStudentType', 'get:/management/report/refactor/school/studentType', 0, 0, 0, 9, 1),
-       (1151, '获取档案卡', 'archiveExport', 'get:/management/archive/export', 0, 0, 0, 9, 1),
-       (1152, '学校告知书配置', 'schoolResultNoticeConfig', 'put:/management/school/update/resultNoticeConfig/**', 0, 0, 0, 9, 1);
+VALUES (1160, '获取学生类型', 'refactorSchoolStudentType', 'get:/management/report/refactor/school/studentType', 0, 0, 0, 9, 1),
+       (1161, '获取档案卡', 'archiveExport', 'get:/management/archive/export', 0, 0, 0, 9, 1),
+       (1162, '学校告知书配置', 'schoolResultNoticeConfig', 'put:/management/school/update/resultNoticeConfig/**', 0, 0, 0, 9, 1);
