ea77f6500508506c48cafbb592581e756d38798f
Simple4H <crescentcxm@gmail.com>
2023-05-18 16:15:34 +0800

    feat(management): add sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
@@ -1,5 +1,5 @@
-delete from o_permission where id in (703,706);
-delete from o_role_permission where permission_id in (703,706);
-delete from o_district_permission where permission_id in (703,706);
+delete from o_permission where id in (703,706,708,763);
+delete from o_role_permission where permission_id in (703,706,708,763);
+delete from o_district_permission where permission_id in (703,706,708,763);
 
-UPDATE o_permission t SET t.name = '筛查二维码' WHERE t.id = 705;
+UPDATE o_permission t SET t.name = '筛查二维码', t.api_url = 'get:/management/report/screeningOrg/qrcode' WHERE t.id = 705
