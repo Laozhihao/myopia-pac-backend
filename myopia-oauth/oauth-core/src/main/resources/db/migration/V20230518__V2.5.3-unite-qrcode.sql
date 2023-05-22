1570a39ac76bcbb6838429344e90f353b25af769
Simple4H <crescentcxm@gmail.com>
2023-05-22 11:40:00 +0800

    feat(management): 二维码修改

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
@@ -1,5 +1,7 @@
-delete from o_permission where id in (703,706,708,763);
-delete from o_role_permission where permission_id in (703,706,708,763);
-delete from o_district_permission where permission_id in (703,706,708,763);
+delete from o_permission where id in (703,705,706,708,763);
+delete from o_role_permission where permission_id in (703,705,706,708,763);
+delete from o_district_permission where permission_id in (703,705,706,708,763);
 
-UPDATE o_permission t SET t.name = '筛查二维码', t.api_url = 'get:/management/report/screeningOrg/qrcode' WHERE t.id = 705
+INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
+VALUES (1170, '筛查二维码', 'vsScreeningQrcode', 'get:/management/report/screeningOrg/qrcode', 1, 1, 1, 23, 1),
+       (1171, '虚拟二维码', 'virtualScreeningQrcode', null, 1, 1, 0, 23, 1);
