fc6a1d9c45794f72cb590a2384e5be3921ab8cd7
Simple4H <crescentcxm@gmail.com>
2023-05-23 11:48:30 +0800

    feat(management): update sql

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230518__V2.5.3-unite-qrcode.sql
@@ -3,6 +3,6 @@ delete from o_role_permission where permission_id in (703,705,706,708,763);
 delete from o_district_permission where permission_id in (703,705,706,708,763);
 
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES (1170, '筛查二维码', 'vsScreeningQrcode', 'get:/management/report/screeningOrg/qrcode', 1, 1, 1, 27, 1),
-       (1171, '虚拟二维码', 'virtualScreeningQrcode', null, 1, 1, 0, 27, 1),
+VALUES (1170, '筛查二维码', 'vsScreeningQrcode', 'get:/management/report/screeningOrg/qrcode', 1, 1, 1, 2, 1),
+       (1171, '虚拟二维码', 'virtualScreeningQrcode', null, 1, 1, 0, 2, 1),
        (1172, '导出问卷账号密码', 'questionnaireAccount', 'get:/management/screeningPlan/export/planStudent/questionnaire/account/**/**', 0, 0, 0, 27, 1);
