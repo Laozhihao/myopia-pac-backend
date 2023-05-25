87a438513e845e2986a9785f37bfec06a6edb1a7
lzh <zhihao.lao@vistel.cn>
2023-05-25 18:25:57 +0800

    feat(oauth): add xinjiang manual operation api permission

--- myopia-oauth/oauth-core/src/main/resources/db/migration/V20230420__V2.5.3-xinjiang.sql
+++ myopia-oauth/oauth-core/src/main/resources/db/migration/V20230420__V2.5.3-xinjiang.sql
@@ -1,2 +1,4 @@
 INSERT INTO o_permission (id, name, menu_btn_name, api_url, is_menu, is_page, `order`, pid, system_code)
-VALUES (1148, '判断是否为新疆地区机构', 'isXinJiangDistrictOrg', 'get:/management/screeningPlan/isXinJiangDistrict', 0, 0, 1, 27, 1);
+VALUES (1148, '判断是否为新疆地区机构', 'isXinJiangDistrictOrg', 'get:/management/screeningPlan/isXinJiangDistrict', 0, 0, 1, 27, 1),
+(1149, '手动同步筛查数据到新疆', 'syncDataToXinJiang', 'get:/management/screeningPlan/isXinJiangDistrict', 0, 0, 1, 294, 1),
+(1166, '更新筛查计划信息', 'updateScreeningPlanInfo', 'get:/management/test/syncDataToXinJiang', 0, 0, 1, 294, 1);
